2b584c028277f52ca17875cba75fc0e7cb11ed95
Gurden Batra <gurden.batra@gmail.com>
2022-02-24 13:50:46 +0200

    Make top filters working, and small changes

--- pages/laws/index.js
+++ pages/laws/index.js
@@ -5,7 +5,6 @@ import useSWR from "swr";
 import {
   wettelijk_bevoegdheidsniveau,
   rechtsgebied,
-  fasen,
   plaberum,
   r_ladder,
 } from "../../dataFilterExample";
@@ -39,8 +38,10 @@ export default function Laws() {
     plaberum: [],
     r_ladder: [],
   });
+  const [voorbeelden, setVoorbeelden] = useState(false);
+  const [huidige, setHuidige] = useState(false);
+
   const [numberOfLaws, setNumberOfLaws] = useState(67);
-  const { query } = useRouter();
 
   const handleFilters = (checkboxState, key) => {
     const newFilters = { ...selected };
@@ -48,38 +49,62 @@ export default function Laws() {
     setSelected(newFilters);
   };
 
+  const reset = () => {
+    setSelected({
+      wettelijk_bevoegdheidsniveau: [],
+      rechtsgebied: [],
+      plaberum: [],
+      r_ladder: [],
+    });
+
+    setVoorbeelden(false);
+    setHuidige(false);
+  }
+
   useEffect(() => {
     //added check for data to have been retrieved here
     if (data) {
       const filteredLaws = data;
-      if(selected.wettelijk_bevoegdheidsniveau.length > 0 && !selected.wettelijk_bevoegdheidsniveau.includes("all")){
+      if(selected.wettelijk_bevoegdheidsniveau.length > 0){
         filteredLaws = filteredLaws.filter(element => {
-          return selected.wettelijk_bevoegdheidsniveau.includes(element.bevoegdheidsniveau)
+          return (selected.wettelijk_bevoegdheidsniveau.includes(element.bevoegdheidsniveau) || element.bevoegdheidsniveau === "Alle bevoegdheidsniveaus")
         })
       } 
 
-      if(selected.r_ladder.length > 0 && !selected.r_ladder.includes("all")){
+      if(selected.r_ladder.length > 0){
         filteredLaws = filteredLaws.filter(element => {
           return element.r_ladder.split(', ').some(i => selected.r_ladder.includes(i))
         })
       }
 
-      if(selected.rechtsgebied.length > 0 && !selected.rechtsgebied.includes("all")){
+      if(selected.rechtsgebied.length > 0){
         filteredLaws = filteredLaws.filter(element => {
           return selected.rechtsgebied.includes(element.relatie)
         })
       }
 
-      if(selected.plaberum.length > 0 && !selected.plaberum.includes("all")){
+      if(selected.plaberum.length > 0){
         filteredLaws = filteredLaws.filter(element => {
           return selected.plaberum.includes(element.fasen)
+        })
+      }
+
+      if(voorbeelden){
+        filteredLaws = filteredLaws.filter(element => {
+          return element.opmerkingen_type_norm_valt_hier_ook_onder !== ""
+        })
+      }
+
+      if(huidige){
+        filteredLaws = filteredLaws.filter(element => {
+          return element.ingang_van_wet !== "Nog niet inwerking getreden"
         })
       }
 
       setLaws(filteredLaws);
       setNumberOfLaws(filteredLaws.length);
     }
-  }, [data, selected]);
+  }, [data, selected, voorbeelden, huidige]);
 
   return (
     <Layout>
@@ -88,7 +113,7 @@ export default function Laws() {
           <h2 className='block text-4xl mb-20'>HOUTBOUW</h2>
           <div className=' flex justify-between pb-3 border-b border-black mb-3'>
             <span className='text-lg'>Verfijnen</span>{" "}
-            <span className='underline'>Reset</span>
+            <span onClick={reset} className='underline'>Reset</span>
           </div>
           <SearchFilter
             title='Wettelijk bevoegdheidsniveau'
@@ -130,13 +155,19 @@ export default function Laws() {
           </div>
 
           <div>
-            <input type='checkbox' />
+            <input type='checkbox' 
+            checked={voorbeelden}
+            onChange={e => setVoorbeelden(e.target.checked)}
+            />
             <span className='pl-3 text-gray-400	'>
               Maatregelen met voorbeelden van succesvolle toepassingen
             </span>
           </div>
           <div className='pb-3 border-b border-black'>
-            <input type='checkbox' />
+            <input type='checkbox' 
+             checked={huidige}
+             onChange={e => setHuidige(e.target.checked)}
+            />
             <span className='pl-3 text-gray-400	'>
               Alleen maatregelen in HUIDIGE wet- en regelgeving
             </span>
