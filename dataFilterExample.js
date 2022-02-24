d0b3710e3fe99ae6edaf51d94ace8187b2a0a624
Gurden Batra <gurden.batra@gmail.com>
2022-02-24 15:47:39 +0200

    Make ids into strings

--- dataFilterExample.js
+++ dataFilterExample.js
@@ -1,20 +1,20 @@
 export const wettelijk_bevoegdheidsniveau = [
   {
-    id: 0,
+    id: "0",
     name: "Nationaal",
     label: "Nationaal",
     value: "Nationaal",
     number: "13",
   },
   {
-    id: 1,
+    id: "1",
     name: "Provinciaal",
     label: "Provinciaal",
     value: "Provinciaal",
     number: "12",
   },
   {
-    id: 2,
+    id: "2",
     name: "Gemeentelijk",
     label: "Gemeentelijk",
     value: "Gemeentelijk",
@@ -24,21 +24,21 @@ export const wettelijk_bevoegdheidsniveau = [
 
 export const rechtsgebied = [
   {
-    id: 0,
+    id: "0",
     name: "Publiek",
     label: "Publiek",
     value: "Publiekrecht",
     number: "49",
   },
   {
-    id: 1,
+    id: "1",
     name: "Privaat",
     label: "Privaat",
     value: "Privaatrecht",
     number: "18",
   },
   {
-    id: 2,
+    id: "2",
     name: "Fiscaal",
     label: "Fiscaal",
     value: "Fiscaalrecht",
@@ -48,28 +48,28 @@ export const rechtsgebied = [
 
 export const plaberum = [
   {
-    id: 0,
+    id: "0",
     name: "Verkenning",
     label: "Verkenning",
     value: "Verkenning",
     number: "44",
   },
   {
-    id: 1,
+    id: "1",
     name: "Haalbaarheid",
     label: "Haalbaarheid",
     value: "Haalbaarheid",
     number: "1",
   },
   {
-    id: 2,
+    id: "2",
     name: "Ontwerp",
     label: "Ontwerp",
     value: "Ontwerp",
     number: "4",
   },
   {
-    id: 3,
+    id: "3",
     name: "Uitvoerbaarheid",
     label: "Uitvoerbaarheid",
     value: "Uitvoerbaarheid",
@@ -79,42 +79,42 @@ export const plaberum = [
 
 export const r_ladder = [
   {
-    id: 0,
+    id: "0",
     name: "R1 Refuse & rethink",
     label: "R1",
     value: "R1",
     number: "48",
   },
   {
-    id: 1,
+    id: "1",
     name: "R2 Reduce",
     label: "R2",
     value: "R2",
     number: "42",
   },
   {
-    id: 2,
+    id: "2",
     name: "R3 Re-use",
     label: "R3",
     value: "R3",
     number: "11",
   },
   {
-    id: 3,
+    id: "3",
     name: "R4 Repair & remanufacture",
     label: "R4",
     value: "R4",
     number: "6",
   },
   {
-    id: 4,
+    id: "4",
     name: "R5 Recyclingk",
     label: "R5",
     value: "R5",
     number: "13",
   },
   {
-    id: 5,
+    id: "5",
     name: "R6 Recover",
     label: "R6",
     value: "R6",
