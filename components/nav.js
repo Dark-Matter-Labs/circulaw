2bb8a123108154c334a69214c39876d839a58c53
William Felker <gndclouds@icloud.com>
2022-03-14 22:09:25 +0000

    Dropdowns Added

--- components/nav.js
+++ components/nav.js
@@ -27,6 +27,10 @@ const juridischeThema = [
 
 const voorbeelden = [{ name: "name", description: "description", href: "#" }];
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export default function Nav() {
   const [session, loading] = useSession();
 
@@ -40,15 +44,104 @@ export default function Nav() {
           <Image src={logo} alt="Picture of the author" />
         </div>
         <div className="content right-0">
-          <Link href="/laws">
-            <a className="px-3">Waardeketens</a>
-          </Link>
-          <Link href="/laws">
-            <a className="px-3">Juridische Thema</a>
-          </Link>
-          <Link href="/laws">
-            <a className="px-3">Voorbeelden</a>
-          </Link>
+          <Popover className="inline-block relative px-3">
+            {({ open }) => (
+              <>
+                <Popover.Button
+                  className={classNames(
+                    open ? "text-gray-900" : "text-gray-500",
+                    "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  )}
+                >
+                  <span>Waardeketens</span>
+                  <ChevronDownIcon
+                    className={classNames(
+                      open ? "text-gray-600" : "text-gray-400",
+                      "ml-2 h-5 w-5 group-hover:text-gray-500"
+                    )}
+                    aria-hidden="true"
+                  />
+                </Popover.Button>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                      <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                        {waardeketens.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                          >
+                            <p className="text-base font-medium text-gray-900">
+                              {item.name}
+                            </p>
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </>
+            )}
+          </Popover>
+          <Popover className="inline-block relative px-3">
+            {({ open }) => (
+              <>
+                <Popover.Button
+                  className={classNames(
+                    open ? "text-gray-900" : "text-gray-500",
+                    "group bg-white rounded-md inline-flex  text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  )}
+                >
+                  <span>Juridische Thema</span>
+                  <ChevronDownIcon
+                    className={classNames(
+                      open ? "text-gray-600" : "text-gray-400",
+                      "ml-2 h-5 w-5 group-hover:text-gray-500"
+                    )}
+                    aria-hidden="true"
+                  />
+                </Popover.Button>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                      <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                        {juridischeThema.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                          >
+                            <p className="text-base font-medium text-gray-900">
+                              {item.name}
+                            </p>
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </>
+            )}
+          </Popover>
         </div>
       </div>
       <div className="flex-1 right-0">
