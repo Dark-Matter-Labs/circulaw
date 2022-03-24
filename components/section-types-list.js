2b2d5b5e7a5cbfc6327a61a3a66f7d7b23769a18
Gurden Batra <gurden.batra@gmail.com>
2022-03-24 14:34:52 +0000

    Adding link, fixing image issue and link issue

--- components/section-types-list.js
+++ components/section-types-list.js
@@ -9,29 +9,35 @@ import OverigeHero from "../public/overige-hero.png";
 
 const types = [
   {
-    heroImage: "/$`{HoutbouwHero}`",
+    heroImage: HoutbouwHero,
     icon: "",
     title: "Houtbouw",
     description:
       "Ontdek de juridische kansen en maatregelen die de transitie naar houtbouw kunnen versnellen",
+    buttonText: "Meer over houtbouw",
+    href: "/houtbouw",
     url: "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
     active: "true",
   },
   {
-    heroImage: "/$`{WindmolensHero}`",
+    heroImage: WindmolensHero,
     icon: "",
     title: "Circulaire windmolens",
     description:
       "Onze juristen brengen op dit moment de kansen en maatregelen in de wetgeving rond circulaire windmolens in kaart. Binnenkort",
+    buttonText: "Bekijk de maatregelen",
+    href: "#",
     url: "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
     active: "false",
   },
   {
-    heroImage: "/$`{OverigeHero}`",
+    heroImage: OverigeHero,
     icon: "",
     title: "Overige maatregelen circulaire bouweconomie",
     description:
       "Deze maatregelen zijn ook in de de circulaire bouweconomie toepasbaar",
+    buttonText: "Bekijk de maatregelen",
+    href: "#",
     url: "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
     active: "true",
   },
@@ -40,7 +46,7 @@ const types = [
 export default function SectionTypes({ data, qty, title, description }) {
   if ({ data } != null) {
     return (
-      <>
+      <div className="pb-20">
         <div className="text-2xl bold py-6">{title}</div>
         <ul
           role="list"
@@ -54,36 +60,27 @@ export default function SectionTypes({ data, qty, title, description }) {
               >
                 <Image
                   src={file.heroImage}
-                  alt="Picture of the author"
-                  width={500}
-                  height={500}
+                  alt="Picture of the case"
+                  width={382}
+                  height={248}
                 />
               </div>
               <div className="group block w-full p-4">
-                <div className="">
-                  <button
-                    type="button"
-                    className="absolute inset-0 focus:outline-none"
-                  >
-                    <span className="sr-only">
-                      View details for {file.title}
-                    </span>
-                  </button>
-                </div>
+                <div className=""></div>
                 <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
                   {file.title}
                 </p>
                 <p className="block text-sm font-medium text-gray-500 pointer-events-none">
                   {file.description}
                 </p>
-                <Link href="#">
-                  <a>Bekijk de maatregelen →</a>
+                <Link href={file.href}>
+                  <a>{file.buttonText} →</a>
                 </Link>
               </div>
             </li>
           ))}
         </ul>
-      </>
+      </div>
     );
   }
   if ({ data } === null) {
