a2eddfcbf3bdcb01369c09d0d99abdfdb59ba156
William Felker <gndclouds@icloud.com>
2022-04-26 02:38:41 +0000

    Updates to colum

--- components/footer.js
+++ components/footer.js
@@ -1,54 +1,56 @@
 const navigation = {
   WAARDEKETENS: [
-    { name: "Gebouwde omgeving", href: "#" },
-    { name: "Houtbouw", href: "#" },
-    { name: "Circuulaire windmolens", href: "#" },
-    { name: "Overige maatregelen", href: "#" },
-    { name: "Consumptiegoederen", href: "#" },
-    { name: "Biomassa en voedsel", href: "#" },
-    { name: "Maakindustrie", href: "#" },
-    { name: "Kunststoffen", href: "#" },
+    { name: "Gebouwde omgeving", href: "#", className: "" },
+    { name: "Houtbouw", href: "#", className: "pl-4" },
+    { name: "Circuulaire windmolens", href: "#", className: "pl-4" },
+    { name: "Overige maatregelen", href: "#", className: "pl-4" },
+    { name: "Consumptiegoederen", href: "#", className: "pl-4" },
+    { name: "Biomassa en voedsel", href: "#", className: "pl-4" },
+    { name: "Maakindustrie", href: "#", className: "pl-4" },
+    { name: "Kunststoffen", href: "#", className: "pl-4" },
   ],
 
   JURIDISCHE: [
-    { name: "Fysieke leefomgeving", href: "#" },
-    { name: "Aanbestedingen", href: "#" },
-    { name: "Bouwen", href: "#" },
-    { name: "Gronduitgifte", href: "#" },
+    { name: "Fysieke leefomgeving", href: "#", className: "" },
+    { name: "Aanbestedingen", href: "#", className: "" },
+    { name: "Bouwen", href: "#", className: "" },
+    { name: "Gronduitgifte", href: "#", className: "" },
   ],
   other: [
-    { name: "OVER ONS", href: "#" },
-    { name: "BLOG", href: "#" },
-    { name: "CONTACT", href: "#" },
+    { name: "OVER ONS", href: "#", className: "" },
+    { name: "BLOG", href: "#", className: "" },
+    { name: "CONTACT", href: "#", className: "" },
   ],
 };
 
 export default function Example() {
   return (
-    <footer className="bg-[#707A85]" aria-labelledby="footer-heading">
+    <footer className="bg-[#707A85] mt-20" aria-labelledby="footer-heading">
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
-          <div className="grid grid-cols-2 gap-8 xl:col-span-4">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm  text-white tracking-wider uppercase">
+        <div className="pb-8">
+          <div className="grid grid-cols-1 gap-8 sm:col-span-3">
+            <div className="sm:grid sm:grid-cols-4 md:gap-8">
+              <div className="col-span-2">
+                <h3 className="text-sm text-white tracking-wider uppercase">
                   WAARDEKETENS
                 </h3>
-                <ul role="list" className="mt-4 space-y-4">
-                  {navigation.WAARDEKETENS.map((item) => (
-                    <li key={item.name}>
-                      <a
-                        href={item.href}
-                        className="text-base text-gray-500 hover:text-gray-900"
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
+                <div className="grid grid-cols-1 gap-8">
+                  <ul role="list" className="mt-4 space-y-4">
+                    {navigation.WAARDEKETENS.map((item) => (
+                      <li key={item.name}>
+                        <a
+                          href={item.href}
+                          className={`text-base text-white hover:text-[#4099DA] ${item.className}`}
+                        >
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
               <div className="mt-12 md:mt-0">
                 <h3 className="text-sm text-white tracking-wider uppercase">
@@ -59,7 +61,7 @@ export default function Example() {
                     <li key={item.name}>
                       <a
                         href={item.href}
-                        className="text-base text-gray-500 hover:text-gray-900"
+                        className="text-base text-white hover:text-[#4099DA]"
                       >
                         {item.name}
                       </a>
@@ -73,7 +75,7 @@ export default function Example() {
                     <li key={item.name}>
                       <a
                         href={item.href}
-                        className="text-base text-gray-500 hover:text-gray-900"
+                        className="text-base text-white hover:text-[#4099DA]"
                       >
                         {item.name}
                       </a>
