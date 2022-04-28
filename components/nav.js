4bf099c6907246eabf75ac3dd5906a14a74f883c
William Felker <gndclouds@icloud.com>
2022-04-28 14:17:56 +0000

    menu restored

--- components/nav.js
+++ components/nav.js
@@ -68,6 +68,17 @@ export default function Example() {
         <>
           <div className="bg-[#4099DA] sm:bg-white max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
               <div className=" flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   <div className="text-white sm:text-gray-900 font-medium">
@@ -113,8 +124,8 @@ export default function Example() {
               </div>
             </div>
           </div>
-          <div className="flex  inset-x-0 top-0 p-5 sm:p-9">
-            <div className="flex-1">
+          <div className="flex inset-x-0 top-0 p-5 sm:p-9 invisible sm:visible hidden sm:inline">
+            <div className="flex-1 ">
               <div className="content right-0">
                 <Popover className="inline-block relative ">
                   {({ open }) => (
