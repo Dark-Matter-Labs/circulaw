e6d4b2a02c0f7c59cc43c7840d8df97031108bb5
William Felker <gndclouds@icloud.com>
2022-04-28 15:05:29 +0000

    mobile is back

--- components/nav.js
+++ components/nav.js
@@ -70,7 +70,7 @@ export default function Example() {
             <div className="relative flex justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-white ">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
