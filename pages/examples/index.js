08167d96b481f4a8f0b4fde58814a593f887c55a
William Felker <gndclouds@icloud.com>
2022-05-12 16:51:08 +0000

    old files

--- pages/examples/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../../components/layout";
-
-export default function Examples() {
-  return (
-    <Layout>
-      <div className="flex ">Examples Place Holder Page</div>
-    </Layout>
-  );
-}
