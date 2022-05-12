c13ea9459de20b7e206f6d1480f3bde6263f6481
William Felker <gndclouds@icloud.com>
2022-05-12 16:21:13 +0000

    cleaning + Removed nextauth

--- components/layout.js
+++ components/layout.js
@@ -1,25 +1,15 @@
 import Head from "next/head";
 import Footer from "/components/footer";
 import Nav from "/components/nav";
-import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Layout({ children }) {
-  const [session, loading] = useSession();
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <>
       <Nav />
       <Head>
         <title>Digitale tool Juridisch Landschap</title>
       </Head>
-      <main className=" max-w-screen-xl mx-4 xl:mx-auto">
-        {!session && <>{children}</>}
-        {session && <>{children}</>}
-      </main>
+      <main className=" max-w-screen-xl mx-4 xl:mx-auto">{children}</main>
       <Footer />
     </>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,9 @@ import "tailwindcss/tailwind.css";
 import "next-pagination/dist/index.css";
 import "../global.css";
 
-import { Provider } from "next-auth/client";
-
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return getLayout(<Component {...pageProps} />);
 }
