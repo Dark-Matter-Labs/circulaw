c109bf56ca01df11f2220c91f84c977221a95af1
William Felker <gndclouds@icloud.com>
2022-05-20 00:38:50 +0000

    Copy Test

--- pages/meer-over-de-voordelen-van-houtbouw/index.js
+++ pages/meer-over-de-voordelen-van-houtbouw/index.js
@@ -5,96 +5,68 @@ import Layout from "../../components/layout";
 import PolicyList from "../../components/policy-list";
 import ActionPanel from "../../components/section-action-panel";
 
+{
+  /* import ExampleImage from ""; */
+}
+
 export default function Houtbouw() {
   return (
     <Layout>
-      <div className="relative py-16 bg-white overflow-hidden">
-        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full"></div>
-        <div className="relative px-4 sm:px-6 lg:px-8">
-          <div className="text-lg max-w-prose mx-auto">
-            <h1>
-              <span className="block text-base text-indigo-600 font-semibold tracking-wide uppercase">
-                Introducing
-              </span>
-              <span className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                JavaScript for Beginners
-              </span>
-            </h1>
-            <p className="mt-8 text-xl text-gray-500 leading-8">
-              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem.
-              At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at
-              vitae feugiat egestas ac. Diam nulla orci at in viverra
-              scelerisque eget. Eleifend egestas fringilla sapien.
-            </p>
-          </div>
-          <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-            <p>
-              Faucibus commodo massa rhoncus, volutpat.{" "}
-              <strong>Dignissim</strong> sed <strong>eget risus enim</strong>.
-              Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit. Faucibus commodo massa rhoncus, volutpat.
-              Dignissim sed eget risus enim.{" "}
-              <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
-            </p>
-            <ul role="list">
-              <li>Quis elit egestas venenatis mattis dignissim.</li>
-              <li>
-                Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-              </li>
-              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-            </ul>
-            <p>
-              Quis semper vulputate aliquam venenatis egestas sagittis quisque
-              orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
-              eu. Tincidunt leo, elementum mattis elementum ut nisl, justo,
-              amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet,
-              duis sed elit interdum dignissim.
-            </p>
-            <h2>From beginner to expert in 30 days</h2>
-            <p>
-              Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
-              consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
-              vitae interdum mauris enim, consequat vulputate nibh. Maecenas
-              pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim
-              cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
-              ipsum eu a sed convallis diam.
-            </p>
-            <blockquote>
-              <p>
-                Sagittis scelerisque nulla cursus in enim consectetur quam.
-                Dictum urna sed consectetur neque tristique pellentesque.
-                Blandit amet, sed aenean erat arcu morbi.
-              </p>
-            </blockquote>
-            <p>
-              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit.
-            </p>
-            <figure>
-              <figcaption>
-                Sagittis scelerisque nulla cursus in enim consectetur quam.
-              </figcaption>
-            </figure>
-            <h2>Everything you need to get up and running</h2>
-            <p>
-              Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>.
-              Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-              ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-              viverra ridiculus non molestie. Gravida quis fringilla amet eget
-              dui tempor dignissim. Facilisis auctor venenatis varius nunc,
-              congue erat ac. Cras fermentum convallis quam.
-            </p>
-            <p>
-              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit.
-            </p>
-          </div>
-        </div>
+      <div className="">Hoe CircuLaw werkt</div>
+      <div className="">
+        De klimaatcrisis is een van de grootste bedreigingen van onze tijd. Om
+        (verdere) opwarming van de aarde tegen te gaan, is drastische actie
+        nodig. In 2050 wil Amsterdam een volledig circulaire stad zijn.
+        Juridische instrumenten spelen hierin een grote rol. Maar wet- en
+        regelgeving is complexe materie. Het kost beleidsmakers veel tijd en
+        energie om uit te vinden welke fiscaal-juridische instrumenten ze kunnen
+        inzetten.
+      </div>
+      <div className="">
+        CircuLaw lat beleidsmakers zien welke regelgeving mogeliikheden biedt om
+        nieuw. ander circulair beleid te maken.
+      </div>
+      <div className="">
+        Geanalyseerd en geinventariseerd door juridische experts
+      </div>
+      <div className="">
+        {/* <Image
+          src={ExampleImage}
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        /> */}
+      </div>
+      <div className="">
+        In CircuLaw vind je maatregelen die beleidsmakers helpen de circulaire
+        transitie te versnellen. Dat kunnen nieuwe maatregelen zijn, maar ok
+        bestaande maatregelen die nog niet, of weinig worden toegepast.
+        Juridische experts hebben deze maatregelen met een circulaire bril via
+        een uniek proces geinventariseerd vanuit de wet- en regelgeving en
+        leesbaar gemaakt voor beleidsmakers. Op dit moment vind je maatregelen
+        met kansen om 'Houtbouw' en 'Circulaire windturbines' te bevorderen. In
+        de toekomst volgen meer thema's.
+      </div>
+      <div className="">Verken de maatregelen </div>
+      <div className="">Image</div>
+      <div className="">
+        Fen maatregel is een iuridisch instrument waarmee overheden een
+        beleidsdoel kunnen. vormgeven en behalen. Voorbeelden van maatregelen
+        ziin subsidies, vergunningen of belastingen of ...? Fen maatregel in
+        Circul aw beschriift kort:
+        <ul>
+          <li>Het onderwerp waarop de maatregel betrekking heeft</li>
+          <li>
+            Wat de maatregel inhoudt in zo begrijpelijk mogelijke juridische
+            taal
+          </li>
+          <li>Hoe de maatregel kansen biedt om circulariteit te bevorderen</li>
+          <li>Fisen en beperkingen aan het inzetten van de maatregel</li>
+          <li>
+            De iuridische bron, een inschatting van de juridische houdbaarheid y
+            en mate van circulaire strategie
+          </li>
+        </ul>
       </div>
     </Layout>
   );
