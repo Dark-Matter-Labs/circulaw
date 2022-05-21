179cd519a1c27bb990b62a1b90f5ba5d8bb87a6c
William Felker <gndclouds@icloud.com>
2022-05-21 21:59:09 +0000

    cleaning styling

--- components/policy-list.js
+++ components/policy-list.js
@@ -20,13 +20,13 @@ export default function PolicyList(props) {
   return (
     <>
       <div className="py-5 relative max-w-lg">
-        <input
+        {/* <input
           aria-label="Search"
           type="text"
           onChange={(e) => setSearchValue(e.target.value)}
           placeholder="Search"
           className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-800 dark:text-gray-100"
-        />
+        /> */}
         <svg
           className="py-5 absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
           xmlns="http://www.w3.org/2000/svg"
