3ab729157d5b1d53888cd59a87e7142492a3f713
William Felker <gndclouds@icloud.com>
2022-05-21 22:10:49 +0000

    init

---
+++ pages/hoe-het-werkt/index.js
@@ -0,0 +1,125 @@
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "../../components/layout";
+
+export default function Kunststoffen() {
+  return (
+    <Layout>
+      <div className="">
+        <div className="">Hoe CircuLaw werkt</div>
+
+        <div className="">
+          De klimaatcrisis is een van de grootste bedreigingen van onze tijd. Om
+          (verdere) opwarming van de aarde tegen te gaan, is drastische actie
+          nodig._ In 2050 wil Amsterdam een volledig circulaire stad zijin.
+          Juridische instrumenten spelen hierin een grote rol. Maar wet- en
+          regelgeving is complexe materie. Het kost beleidsmakers veel tijd en
+          energie om uit te vinden. welke fiscaal-iuridische instrumenten ze
+          kunnen inzetten.
+        </div>
+        <div className="italic">
+          CircuLaw laat beleidsmakers zien welke regelgeving mogelijkheden biedt
+          om nieuw, ander circulair beleid te maken.
+        </div>
+        <div className="">
+          Geanalyseerd en geinventariseerd door juridische experts
+        </div>
+
+        <div className="">
+          Image
+          {/* <Image
+            src="me.png"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          /> */}
+        </div>
+        <div className=""></div>
+
+        <div className="">
+          In Circul aw vind je maatregelen die beleidsmakers helpen de
+          circulaire transitie te versnellen_ Dat kunnen nieuwe maatregelen
+          zijn, maar ook bestaande maatregelen die nog niet,_of weinig. worden
+          toegepast. Juridische experts hebben deze maatregelen met een
+          circulaire bril via een uniek proces. geinventariseerd vanuit de wet-
+          en regelgeving en leesbaar gemaakt voor. beleidsmakers. Op dit moment
+          vind je maatregelen met kansen om '
+          <Link href="/houtbouw">
+            <a>Houtbouw</a>
+          </Link>
+          ' en '
+          <Link href="/windturbines">
+            <a>Circulaire windturbines</a>
+          </Link>
+          ' te bevorderen. In de toekomst volgen meer thema's.
+        </div>
+        <div className="">Verken de maatregelen</div>
+        <div className="">IMAEG</div>
+        <div className="">
+          Fen maatregel is en juridisch instrument waarmee.
+          overheden.een_beleidsdoelkunnen vormgeven en behalen. Voorbeelden van
+          maatregelen ziin subsidies, vergunningen of belastingen of..? Fen
+          maatregel in Circulaw beschriift kort:
+          <ul>
+            <li> Het onderwerp waarop de maatregel betrekking heeft</li>
+            <li>
+              Wat de maatregel inhoudt in zo begrijpelijk mogelijke juridische
+              taal
+            </li>
+            <li>
+              Hoe de maatregel kansen biedt om circulariteit te bevorderen.
+            </li>
+            <li> Eisen en beperkingen aan het inzetten van de maatregel</li>
+            <li>
+              De juridische bron, een inschatting van de juridische houdhaarheid
+              y en mate van circulaire.
+            </li>
+          </ul>
+        </div>
+        <div className="">
+          Gebruik de maatregelen voor nieuw circulair beleid
+        </div>
+
+        <div className="">IMAGE</div>
+
+        <div className="">
+          Vind je als beleidsmaker een maatregel in CircuLaw waarvan je je
+          afvraagt of '"ie interessant of relevant zou kunnen zijn voor jouw
+          gemeente of provincie? Dan daagt CircuLaw je uit:
+          <ul>
+            <li>verken de maatregel</li>
+            <li>
+              bespreek deze met juridische experts in je eigen organisatie
+            </li>
+            <li>gebruik de maatregel in je eigen context</li>
+            <li>
+              deel je ervaring met met CircuLaw, zodat anderen er ook van kunnen
+              leren
+            </li>
+          </ul>
+        </div>
+        <div className="pa-4 bg-[#dbedfb]">
+          <div className="font-bold">Een voorbeeld:</div>
+          <div className="">
+            Je vind in CircuLaw een maatregel 'Houtbouw een plek geven in de
+            omgevingsvisie' Je vraagt je af in jouw gemeente daar al iets mee
+            doet. Je klopt an bij............en bespreekt of en hoe je
+            'Houtbouw' in de omgevingsvisie kan opnemen en wat de impact is.
+            Daarna onderneem je actie om het voor elkaar te krijgen.
+          </div>
+        </div>
+        <div className="font-bold">Let op:</div>
+
+        <div className="italic">
+          De maatregelen kun je in de praktijk nit allemaal één-op-één overnemen
+          of toepassen: sommige maatregelen zijn zelfs nog niet eerder toegepast
+          en vragen om een innovative aanpak!
+        </div>
+
+        <div className=""></div>
+
+        <div className=""></div>
+      </div>
+    </Layout>
+  );
+}
