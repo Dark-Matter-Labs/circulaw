ac428abf2811627cd6eb7fdf33351825256fa2ca
William Felker <gndclouds@icloud.com>
2022-05-30 13:55:33 +0000

    Meeting Changes

--- components/layout.js
+++ components/layout.js
@@ -27,7 +27,7 @@ export default function Layout({ children }) {
                 </span>
               </p>
             </div>
-            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <div className="order-3 mt-2 flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
               <Link href="/faq">
                 <a className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
                   Lees meer
