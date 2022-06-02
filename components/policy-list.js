e6b40ad1828ecc8028ed55f6ccf4d950ad3b67bd
Gurden Batra <gurden.batra@gmail.com>
2022-06-02 15:15:41 +0300

    Update to searchbox layout and search result text

--- components/policy-list.js
+++ components/policy-list.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
 
 import IconWood from "../public/icons/wood.png";
 
@@ -27,31 +28,52 @@ export default function PolicyList(props) {
 
   return (
     <>
-      <div className="py-5 relative max-w-lg">
-        {/* <input
-          aria-label="Search"
-          type="text"
-          onChange={(e) => setSearchValue(e.target.value)}
-          placeholder="Search"
-          className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-800 dark:text-gray-100"
-        /> */}
-        <svg
-          className="py-5 absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+      <div className="relative">
+        <div>
+          <span className="inline-block ">Zoek in houtbouwmaatregelen</span>
+        </div>
+        <div className="w-96 inline-block py-4 mb-10 px-4 border-2 border-black">
+          <div className="flex">
+            <SearchIcon className="h-6 w-6" aria-hidden="true" />
+            <input
+              onChange={(e) => setSearchValue(e.target.value)}
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Zoek op trefwoord"
+              className="block w-full"
+            />
+          </div>
+        </div>
       </div>
+
       <div className="">
-        {lawData.length === 0 && "No Laws found."}
+        {lawData.length === 0 && (
+          <div>
+            <span>
+              <b>0</b> maatregelen gevonden voor <b>{searchValue}</b> in{" "}
+              <b>{props.casus}</b>{" "}
+            </span>
+          </div>
+        )}
+
+        {searchValue !== "" && lawData.length > 1 && (
+          <div>
+            <span>
+              <b>{lawData.length}</b> maatregelen gevonden voor{" "}
+              <b>{searchValue}</b> in <b>{props.casus}</b>{" "}
+            </span>
+          </div>
+        )}
+
+        {searchValue !== "" && lawData.length === 1 && (
+          <div>
+            <span>
+              <b>{lawData.length}</b> maatregel gevonden voor{" "}
+              <b>{searchValue}</b> in <b>{props.casus}</b>{" "}
+            </span>
+          </div>
+        )}
 
         {lawData.map((lawData, lawDataIdx) => {
           const {
