edd2689ee8f0a441706d81307d41a50b48a1bd09
Gurden Batra <gurden.batra@gmail.com>
2022-07-21 14:54:49 +0300

    Adding colours and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'className'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#1F2223",
+        green1: "#019975",
+        green2: "#22532C",
+        green3: "#6D9F6B",
+        green4: "#AABC97",
+        light: "rgba(249, 217, 200, 0.4)",
+      },
+      fontFamily: {
+        publicSans: ["'Public Sans'", "sans-serif"],
+        manrope: ["'Manrope'", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
