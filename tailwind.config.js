0d149a1aeed5fb3b1f8eb738f42b056546786d33
Gurden Batra <gurden.batra@gmail.com>
2022-07-22 19:41:04 +0300

    Adding updated list of colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dark: "#1F2223",
-        green1: "#019975",
-        green2: "#22532C",
-        green3: "#6D9F6B",
-        green4: "#AABC97",
-        light: "rgba(249, 217, 200, 0.4)",
+        blush1: "#F9D9C8",
+        blush2: "#FFF3ED",
+        blush3: "#FFD8C4",
+        black1: "#1F2223",
+        green1: "#22532C",
+        green2: "#6D9F6B",
+        green3: "#AABC97",
+        greenAlpha: "rgba(109, 159, 107, 0.2)",
+        greenLink: "#019975",
+        grey1: "#979797",
+        white1: "#F8FAF8",
       },
       fontFamily: {
         publicSans: ["'Public Sans'", "sans-serif"],
@@ -22,7 +27,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  images: {
-    domains: ["tailwindui.com"],
-  },
 };
