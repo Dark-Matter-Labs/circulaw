9d109ac25c1a0e979c5d944924a8da7df04fc06c
Gurden Batra <gurden.batra@gmail.com>
2022-07-22 19:43:22 +0300

    Get needed font weights

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;800&family=Public+Sans:wght@700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Public+Sans:wght@700&display=swap"
           rel="stylesheet"
         />
       </Head>
