78b2f354f237a58ad89fdea5084e5ccdac8f6472
Gurden Batra <gurden.batra@gmail.com>
2022-07-25 17:04:17 +0300

    fix JIT warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./public/**/*.html"],
   darkMode: false, // or 'media' or 'className'
   theme: {
     extend: {
