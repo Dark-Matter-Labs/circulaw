bdc2dbb23fa3556323cb48e2f9488f52d44329d0
Gurden Batra <gurden.batra@gmail.com>
2022-08-05 14:27:46 +0300

    placeholder pages

---
+++ pages/alpha/index.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+
+export default function Alpha() {
+  return (
+    <Layout>
+      <div className="flex ">Aplha info Holder Page</div>
+    </Layout>
+  );
+}
