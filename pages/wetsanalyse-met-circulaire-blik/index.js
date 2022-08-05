774639936275f8512966f12f30bdf5b4cf598cd2
Gurden Batra <gurden.batra@gmail.com>
2022-08-05 14:27:23 +0300

    add about pages

---
+++ pages/wetsanalyse-met-circulaire-blik/index.js
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+import OverNav from "../../components/over-nav";
+
+export default function WetAnalyse() {
+  return (
+    <Layout>
+      <div className="breadcrumb ml-5 sm:ml-20 pt-8 text-greenLink">
+        <Link href="/">
+          <a>Home &gt; </a>
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3">
+        <div className="mx-5 sm:mx-20 my-20 col-span-2">
+          <h1 className="pb-5 mobile sm:main">
+            Wetsanalyse vanuit circulaire blik
+          </h1>
+          <h2 className="mobile sm:main">
+            Hoe CircuLaw juristen de wet analyseren
+          </h2>
+          <p className="body-text-mobile sm:body-text max-w-4xl">
+            Vernieuwend aan CircuLaw is dat wetsanalyses worden uitgevoerd
+            vanuit transitiedoelen.
+          </p>
+          <br />
+          <p className="body-text-mobile sm:body-text max-w-4xl">
+            Per productgroep en ketenfase worden operationele doelen
+            geformuleerd. Vervolgens wordt met een speciaal ontwikkelde methode
+            het juridische werkgebied afgebakend: van de duizenden bestaande
+            wetten, besluiten en regelingen wordt een selectie gemaakt van
+            wetten die mogelijk relevant zijn.
+          </p>
+          <br />
+          <p className="body-text-mobile sm:body-text max-w-4xl">
+            Deze wetten worden op artikelniveau geanalyseerd vanuit de vraag:
+            kan dit artikel bijdragen aan het gestelde operationele doel; kan
+            hier circulariteit in worden gelezen?
+          </p>
+          <br />
+          <p className="body-text-mobile sm:body-text max-w-4xl">
+            Om de juridische analist hier zo creatief en vanuit zo veel mogelijk
+            verschillende perspectieven naar te laten kijken, is een
+            analysesleutel ontwikkeld. Voor de maatregelen die uit deze analyse
+            rollen is een rankingsysteem ontwikkeld, waarbij maatregelen een
+            score krijgen op juridische houdbaarheid en invloed.
+          </p>
+          <br />
+          <p className="body-text-mobile sm:body-text max-w-4xl pb-10">
+            Ook laten we zien wat het verband is tussen de gevonden maatregelen.
+            Hiermee geven we beleidsmakers een indicatie mee van de
+            toepasbaarheid van een maatregel, en helpen we hen een goede
+            selectie te maken.
+          </p>
+
+          <h2 className="mobile sm:main">
+            Wetsanalyse laten uitvoeren door CircuLaw?
+          </h2>
+          <p className="body-text-mobile sm:body-text max-w-4xl pb-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non
+            faucibus dolor. Phasellus facilisis quis nibh id vehicula.
+            Vestibulum ut dolor metus. Nulla lobortis tortor eu urna elementum,
+            vel cursus nibh ullamcorper. Pellentesque quam odio, tempor eget
+            aliquam sit amet, scelerisque eget eros. Praesent suscipit nisl in
+            nibh gravida, nec condimentum arcu congue.
+          </p>
+
+          <div className="flex bg-green2 bg-opacity-20 p-10 justify-between flex-wrap">
+            <div>
+              <p className="body-text-mobile sm:body-text">
+                Meer weten? Of wil je een wetsanalyse laten uitvoeren op een
+                circulair thema?
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="inline-flex rounded-full items-center px-4 py-2 border border-green1 button text-white bg-green1 hover:bg-greenLink focus:outline-none "
+              >
+                <Link href="/contact">
+                  <a>Neem contact op →</a>
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="mx-5 sm:mx-20 my-20 max-w-sm">
+          <OverNav pagename="wet" />
+        </div>
+      </div>
+    </Layout>
+  );
+}
---
+++ pages/wie-maken-circulaw/index.js
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+import OverNav from "../../components/over-nav";
+
+export default function WieMaken() {
+  return (
+    <Layout>
+      <div className="breadcrumb ml-5 sm:ml-20 pt-8 text-greenLink">
+        <Link href="/">
+          <a>Home &gt; </a>
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3">
+        <div className="mx-5 sm:mx-20 my-20 col-span-2">
+          <h1 className="pb-5 mobile sm:main">Wie maken CircuLaw?</h1>
+          <p className="body-text-mobile sm:body-text max-w-4xl">
+            Eind 2020 startte gemeente Amsterdam in samenwerking met Dark Matter
+            Labsen verschillende universiteiten en mede-overheden het project
+            ‘Activerende regelgeving voor een circulaire samenleving’. De
+            ontwikkeling van de tool ‘CircuLaw’ is een van de speerpunten van
+            dit project. Het doel van CircuLaw: het versnellen van de transitie
+            naar een circulaire economie door beter gebruik te maken van het
+            instrument wet- en regelgeving. In de ontwikkeling van CircuLaw
+            werkt een breed consortium aan publieke en academische
+            kennispartners van verschillende disciplines samen. Zo brengen we de
+            laatste staat van kennis bij elkaar en kunnen we samen innoveren,
+            experimenteren en grenzen verleggen. Zodat we een service maken die
+            optimaal aansluit bij het werkproces van de circulaire beleidsmaker.
+          </p>
+          <br />
+
+          <h2 className="mobile sm:main">
+            Gemeente Amsterdam, Stedelijke Innovatie
+          </h2>
+          <p className="body-text-mobile sm:body-text max-w-4xl pb-10">
+            Gemeente Amsterdam is al jaren koploper in de circulaire economie.
+            Het doel van de stad is om in 2050 volledig circulair te zijn, en
+            het grondstoffengebruik in 2030 gehalveerd te hebben. Het stedelijk
+            innovatieteam van Amsterdam heeft CircuLaw in 2020 geïnitieerd. Dit
+            team werkt samen met partners in en buiten de stad aan innovaties
+            die de transitie naar een circulaire, veilige en bereikbare stad
+            versnellen. Binnen dit publieke innovatieteam is expertise aanwezig
+            op het gebied van sociale en technologische innovatie,
+            transitiemanagement, digitalisering en moderne overheid. Vanuit de
+            gemeente dragen daarnaast ervaren circulaire experts en juristen van
+            beleidsafdelingen bij aan de ontwikkeling van de tool.
+          </p>
+
+          <div className="flex bg-green2 bg-opacity-20 p-10 justify-between flex-wrap">
+            <div>
+              <p className="body-text-mobile sm:body-text">
+                Bijdragen aan CircuLaw?
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="inline-flex rounded-full items-center px-4 py-2 border border-green1 button text-white bg-green1 hover:bg-greenLink focus:outline-none "
+              >
+                <Link href="/contact">
+                  <a>Neem contact op →</a>
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="mx-5 sm:mx-20 my-20 max-w-sm">
+          <OverNav pagename="wie" />
+        </div>
+      </div>
+    </Layout>
+  );
+}
