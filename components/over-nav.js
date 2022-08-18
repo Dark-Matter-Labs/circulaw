102407845a9b270de4460a821e17dfa44120c70c
Gurden Batra <gurden.batra@gmail.com>
2022-08-18 17:35:49 +0300

    formatting update

--- components/over-nav.js
+++ components/over-nav.js
@@ -1,40 +1,40 @@
 const navigation = [
-  { name: "Waarom CircuLaw?", href: "/waarom-circulaw", pagename: "waarom" },
-  { name: "Wat is CircuLaw?", href: "/wat-is-circulaw", pagename: "wat" },
+  { name: 'Waarom CircuLaw?', href: '/waarom-circulaw', pagename: 'waarom' },
+  { name: 'Wat is CircuLaw?', href: '/wat-is-circulaw', pagename: 'wat' },
   {
-    name: "Status en ambities",
-    href: "/status-en-ambities",
-    pagename: "status",
+    name: 'Status en ambities',
+    href: '/status-en-ambities',
+    pagename: 'status',
   },
   {
-    name: "Wetsanalyse met circulaire blik",
-    href: "/wetsanalyse-met-circulaire-blik",
-    pagename: "wet",
+    name: 'Wetsanalyse met circulaire blik',
+    href: '/wetsanalyse-met-circulaire-blik',
+    pagename: 'wet',
   },
-  { name: "Wie maken CircuLaw?", href: "/wie-maken-circulaw", pagename: "wie" },
+  { name: 'Wie maken CircuLaw?', href: '/wie-maken-circulaw', pagename: 'wie' },
 ];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function OverNav(props) {
   return (
-    <nav className="space-y-1" aria-label="Sidebar">
+    <nav className='space-y-1 sticky top-40' aria-label='Sidebar'>
       {navigation.map((item) => (
         <a
           key={item.name}
           href={item.href}
           className={classNames(
             item.pagename === props.pagename
-              ? "text-black1"
-              : "text-greenLink hover:bg-gray-50 hover:text-gray-900",
-            "flex items-center px-3 py-2 breadcrumb bg-white1"
+              ? 'text-black1'
+              : 'text-greenLink hover:bg-gray-50 hover:text-gray-900',
+            'flex items-center px-3 py-2 breadcrumb bg-white1',
           )}
-          aria-current={item.current ? "page" : undefined}
+          aria-current={item.current ? 'page' : undefined}
         >
-          <span className="truncate">
-            {">"}
+          <span className='truncate'>
+            {'>'}
             {item.name}
           </span>
         </a>
--- pages/api/laws/index.js
+++ pages/api/laws/index.js
@@ -1,4 +1,4 @@
-import { laws } from "../../../data";
+import { laws } from '../../../data';
 
 export default function handler(req, res) {
   res.status(200).json(laws);
