8e01913293827aafcf81d07577d264dc82bc3faa
Gurden Batra <gurden.batra@gmail.com>
2022-11-15 14:02:31 +0200

    removed unused imports, fix mob image and Preitter formatting

--- pages/circulaire-windturbines/welke-overheid-heeft.js
+++ pages/circulaire-windturbines/welke-overheid-heeft.js
@@ -1,32 +1,29 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import Layout from '../../components/layout';
 import WelkeLayout from '../../components/welke-layout';
 import WindmillImage from '../../public/Windmill_Measures_Web.png';
 import WindmillImageMob from '../../public/Windmill_Measures_Mob.png';
 
-
 export default function InfoPage() {
   return (
     <Layout>
-     <WelkeLayout 
-      casus = 'circulaire Windturbines'
-      title = 'Welke overheid heeft welke bevoegdheid voor maatregelen circulaire windturbines?'
-      img = {WindmillImage}
-      imgMob = {WindmillImage}
-      p1 = 'Provincies en gemeenten kunnen circulaire windturbines opnemen in hun omgevingsvisie om
+      <WelkeLayout
+        casus='circulaire Windturbines'
+        title='Welke overheid heeft welke bevoegdheid voor maatregelen circulaire windturbines?'
+        img={WindmillImage}
+        imgMob={WindmillImageMob}
+        p1='Provincies en gemeenten kunnen circulaire windturbines opnemen in hun omgevingsvisie om
       dit beleidsdoel kenbaar te maken. Het vergroten van het aandeel publieke grond van
       provincies en gemeenten zorgt ervoor dat circulaire eisen kunnen worden gesteld bij de
       uitgifte van de gronden.'
-      p2 = 'Gemeenten kunnen daarnaast circulariteit opnemen in hun omgevingsplan en zodoende
+        p2='Gemeenten kunnen daarnaast circulariteit opnemen in hun omgevingsplan en zodoende
       aanvullende eisen stellen bij de vergunningverlening. In een anterieure overeenkomst
       worden kosten voor gebiedsontwikkeling vastgelegd en daar kan circulariteit ook bij
       betrokken worden.'
-      p3 = 'Gemeenten, provincies en Rijk hebben ook nog gedeelde bevoegdheden: door circulaire
+        p3='Gemeenten, provincies en Rijk hebben ook nog gedeelde bevoegdheden: door circulaire
       eisen te stellen bij gronduitgifte - door verkoop, erfpacht en huurovereenkomsten -
       wordt het plaatsen van circulaire windturbines aangemoedigd.'
-      p4 = ''
-     />
+        p4=''
+      />
     </Layout>
   );
 }
--- pages/houtbouw/welke-overheid.js
+++ pages/houtbouw/welke-overheid.js
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import Layout from '../../components/layout';
 import TimberImage from '../../public/Timber_Measures_Web.png';
 import TimberImageMob from '../../public/Timber_Measures_Mob.png';
@@ -8,27 +6,27 @@ import WelkeLayout from '../../components/welke-layout';
 export default function InfoPage() {
   return (
     <Layout>
-     <WelkeLayout 
-      casus = 'Houtbouw'
-      title = 'Welke overheid heeft welke bevoegdheid voor houtbouwmaatregelen?'
-      img = {TimberImage}
-      imgMob = {TimberImageMob}
-      p1 = 'Op rijksniveau kunnen regels gesteld worden ten aanzien van hergebruik van producten en
+      <WelkeLayout
+        casus='Houtbouw'
+        title='Welke overheid heeft welke bevoegdheid voor houtbouwmaatregelen?'
+        img={TimberImage}
+        imgMob={TimberImageMob}
+        p1='Op rijksniveau kunnen regels gesteld worden ten aanzien van hergebruik van producten en
       kan het Rijk financieel bijdragen aan doelen die gesteld worden in een omgevingsvisie -
       en/of programma over houtbouw.'
-      p2 = 'Provincies kunnen houtbouw integreren in hun omgevingsverordening door de houtopstanden
+        p2='Provincies kunnen houtbouw integreren in hun omgevingsverordening door de houtopstanden
       in het gebied te vergroten. Het stellen van omgevingswaarden kan hierbij helpen.'
-      p3 = 'Gemeenten kunnen ook veel doen. Zo kunnen gemeenten regels stellen over
+        p3='Gemeenten kunnen ook veel doen. Zo kunnen gemeenten regels stellen over
       sloopwerkzaamheden, afvalstoffen en recycling. Daarnaast kan houtbouw opgenomen worden
       in het omgevingsplan om de vergunningverlening hierop aan te passen. Omgevingswaarden
       kunnen hierbij een rol spelen.'
-      p4 = 'Gemeenten, provincies en Rijk hebben ook nog gedeelde bevoegdheden: aanbestedingen
+        p4='Gemeenten, provincies en Rijk hebben ook nog gedeelde bevoegdheden: aanbestedingen
       spelen hierbij de grootste rol. Het vergroten van het aandeel publieke grond zorgt
       ervoor dat circulaire eisen kunnen worden gesteld bij de uitgifte van die gronden. De
       omgevingsvisie is voor alle overheden belangrijk omdat hier de beleidsdoelen voor
       houtbouw in kunnen worden verankerd. Als laatste kunnen overheden innovatie toestaan
       middels technische gelijkwaardigheid van bouwonderdelen en door experimenten.'
-     />
+      />
     </Layout>
   );
 }
