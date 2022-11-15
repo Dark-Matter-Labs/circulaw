36e431e0c50f398d439e0685cb2df69da89476f3
Gurden Batra <gurden.batra@gmail.com>
2022-11-15 14:02:39 +0200

    Prettier formatting

--- components/samenhang-layout.js
+++ components/samenhang-layout.js
@@ -12,20 +12,18 @@ p3
 */
 
 export default function SamenhangLayout(props) {
-    return(
-        <>
-        <div className='global-margin mt-20'>
+  return (
+    <>
+      <div className='global-margin mt-20'>
         <div className='breadcrumb text-greenLink pb-8'>
           <Link href='/'>
             <a>Home &gt; </a>
           </Link>
-          <Link href= {`/${props.casus.toLowerCase()}`} >
+          <Link href={`/${props.casus.toLowerCase()}`}>
             <a className=' inline-block lowercase first-letter:uppercase'>{props.casus} &gt; </a>
           </Link>
         </div>
-        <h1 className='mobile sm:main text-green1 pb-2 max-w-3xl mx-auto'>
-          {props.title}
-        </h1>
+        <h1 className='mobile sm:main text-green1 pb-2 max-w-3xl mx-auto'>{props.title}</h1>
       </div>
       <div className='hidden sm:block image-margin'>
         <Image src={props.img} layout='responsive' alt='Picture of the case' />
@@ -35,21 +33,15 @@ export default function SamenhangLayout(props) {
       </div>
       <div className='global-margin mb-20'>
         <div className='max-w-3xl mx-auto'>
-          <p className='body-text-mobile sm:body-text text-black1 pb-6'>
-            {props.p1}
-          </p>
-          {props.p2 !== '' &&
-          <p className='body-text-mobile sm:body-text text-black1 pb-6'>
-            {props.p2}
-          </p>
-          }
-          {props.p3 !== '' &&
-          <p className='body-text-mobile sm:body-text text-black1 pb-6'>
-            {props.p3}
-          </p>
-          }
+          <p className='body-text-mobile sm:body-text text-black1 pb-6'>{props.p1}</p>
+          {props.p2 !== '' && (
+            <p className='body-text-mobile sm:body-text text-black1 pb-6'>{props.p2}</p>
+          )}
+          {props.p3 !== '' && (
+            <p className='body-text-mobile sm:body-text text-black1 pb-6'>{props.p3}</p>
+          )}
         </div>
       </div>
-      </>
-    )
+    </>
+  );
 }
--- pages/circulaire-windturbines/samenhang-maatregelen.js
+++ pages/circulaire-windturbines/samenhang-maatregelen.js
@@ -1,22 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../../components/layout';
-import WindmillImage from '../../public/Windmill_Process_Web.png'
+import WindmillImage from '../../public/Windmill_Process_Web.png';
 import WindmillImageMob from '../../public/Windmill_Process_Mob.png';
 import SamenhangLayout from '../../components/samenhang-layout';
 
 export default function InfoPage() {
   return (
     <Layout>
-      <SamenhangLayout 
-        casus = 'circulaire Windturbines'
-        title = 'Samenhang maatregelen circulaire windturbines'
-        img = {WindmillImage}
-        imgMob = {WindmillImageMob}
-        p1 = 'Beleid rondom circulaire windturbines kan worden opgenomen in omgevingsvisies. Door dit te verankeren kunnen subsidies worden vastgesteld. Daarnaast kunnen overheden proberen meer grond te kopen om uit te geven met circulaire eisen. Ook kunnen met het omgevingsplan of een projectbesluit vergunningen worden verleend voor circulaire windturbines. Afspraken over de kosten van circulaire windturbines kunnen voor het omgevingsplan worden vastgelegd in een anterieure overeenkomst.'
-        p2 = ''
-        p3 = ''
-        />
+      <SamenhangLayout
+        casus='circulaire Windturbines'
+        title='Samenhang maatregelen circulaire windturbines'
+        img={WindmillImage}
+        imgMob={WindmillImageMob}
+        p1='Beleid rondom circulaire windturbines kan worden opgenomen in omgevingsvisies. Door dit te verankeren kunnen subsidies worden vastgesteld. Daarnaast kunnen overheden proberen meer grond te kopen om uit te geven met circulaire eisen. Ook kunnen met het omgevingsplan of een projectbesluit vergunningen worden verleend voor circulaire windturbines. Afspraken over de kosten van circulaire windturbines kunnen voor het omgevingsplan worden vastgelegd in een anterieure overeenkomst.'
+        p2=''
+        p3=''
+      />
     </Layout>
   );
 }
