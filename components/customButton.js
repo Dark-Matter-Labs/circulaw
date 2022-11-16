9ef784a9eeb8b0a562695221eec97b280992b696
theocampbell <57679708+theocampbell@users.noreply.github.com>
2022-11-16 15:59:25 +0100

    add custom-button to section types
    added custom button to section types

--- components/customButton.js
+++ components/customButton.js
@@ -3,7 +3,8 @@ import Link from 'next/link'
 
 const buttonColors = {
   home: 'border-green1 bg-blush2 hover:bg-green1 text-green1 hover:text-white1',
-  actionPanel: 'border-green1 bg-green1 hover:bg-transparent text-white1 hover:text-green1'
+  actionPanel: 'border-green1 bg-green1 hover:bg-transparent text-white1 hover:text-green1',
+  sectionTypes: 'border-green1 bg-transparent hover:bg-green1 text-green1 hover:text-white1',
 }
 
 export default function CustomButton({ color, children }) {
