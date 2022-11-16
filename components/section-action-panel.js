b5cdf29cfffef329add6045efd470032705486dc
theocampbell <57679708+theocampbell@users.noreply.github.com>
2022-11-16 16:02:23 +0100

    fixed link in section action panel
    fixed link

--- components/section-action-panel.js
+++ components/section-action-panel.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import CustomButton from './customButton';
+
+
 export default function ActionPanel(props) {
   return (
     <div className='bg-green3 py-10'>
@@ -10,10 +12,12 @@ export default function ActionPanel(props) {
             <div className='mt-2 max-w-xl body-text-mobile sm:body-text '>{props.paragraph}</div>
           </div>
           <div className='mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center'>
-            <Link href={props.buttonLink} passHref>
+            <Link href={props.buttonLink}>
+              <a>
                 <CustomButton color = 'actionPanel'>
                 {props.buttonText} →
                 </CustomButton>
+                </a>
             </Link>
           </div>
         </div>
