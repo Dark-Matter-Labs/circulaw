8587622a21dbf56f89309b9ec6aa16a0e193dfcb
theocampbell <57679708+theocampbell@users.noreply.github.com>
2022-11-21 10:39:08 +0100

    run prettier
    run prettier

--- components/policy-list.js
+++ components/policy-list.js
@@ -99,7 +99,8 @@ export default function PolicyList(props) {
               </div>
               <div className='block font-manrope font-bold text-xs pb-1'>
                 {europees && <span>Europees - </span>}
-                {nationaal && <span>Nationaal {provinciaal && <span> - </span>} </span>} {/*added extra conditional statements to remove the - when there is only one region */}
+                {nationaal && <span>Nationaal {provinciaal && <span> - </span>} </span>}{' '}
+                {/*added extra conditional statements to remove the - when there is only one region */}
                 {provinciaal && <span>Provinciaal {gemeentelijk && <span> - </span>} </span>}
                 {waterschappen && <span>Waterschappen - </span>}
                 {gemeentelijk && <span>Gemeentelijk</span>}
