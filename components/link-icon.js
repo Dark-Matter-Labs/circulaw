b0c9030e0cec092e50e63f9f8a2201ac58648a6f
theocampbell <57679708+theocampbell@users.noreply.github.com>
2022-11-28 18:06:53 +0100

    added size prop
    Have not called the prop in any components yet as we need to have NEXT 12.2 in order for this to work.

--- components/link-icon.js
+++ components/link-icon.js
@@ -1,14 +1,24 @@
 import Image from 'next/image'
 
 
-export default function LinkIcon() {
+// this should work but only after next 12.2
+const sizes = {
+    mob: 'h-2 w-2',
+    desktop: 'h-4 w-4',
+}
+
+
+export default function LinkIcon({size}) {
+    let sizeClasses = sizes[size]
     return(
-        <span className='pl-1'>
-        <Image  
-            className='mt-2' 
-            alt = 'new tab' src = '/icons/VectornewTab.png' 
-            width = {18} 
-            height ={18}/>
+        <span className={`pl-1 ${sizeClasses}`}>
+        <Image
+            className= ''
+            alt = 'new tab' 
+            src = '/icons/VectornewTab.png' 
+            height={16}
+            width={16}
+            />
         </span>
     )
 }
