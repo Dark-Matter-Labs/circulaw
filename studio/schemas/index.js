95aa21b674237b5ff0ae6d9ed3e6bc1e5eb519ff
theocampbell <57679708+theocampbell@users.noreply.github.com>
2022-12-08 18:04:16 +0100

    test schema

--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import test from "./test"
+
+export const schemaTypes = [test]
---
+++ studio/schemas/test.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'test',
+    type: 'document',
+      title: 'Test',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      }
+    ]
+  }
