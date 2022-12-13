0430d422e17269063b87edc2fb507817612d4ee4
theocampbell <57679708+theocampbell@users.noreply.github.com>
2022-12-13 09:44:30 +0100

    component created

--- components/layouts/theme-index-layout.js
+++ components/layouts/theme-index-layout.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import SectionTypes from '../../components/section-types-list';
+import ThemeBottomSection from '../section-bottom-theme-index';
 
 export default function ThemeLayout(props) {
   return (
@@ -45,101 +46,14 @@ export default function ThemeLayout(props) {
         </div>
         {/* using the prop isSpecialMeasures to conditionally render this section
           if there is not special measure set this to '' and the section will not render*/}
-        {props.isSpecialMeasures !== '' && (
+        {props.isSpecialMeasures && (
           <div className='my-20'>
             <h2 className='pb-8 mobile sm:main'>{props.isSpecialMeasures}</h2>
             <SectionTypes type={props.type} />
           </div>
         )}
       </div>
-
-      <div className='bg-green3 bg-opacity-10'>
-        <div className='pt-5 global-margin  '>
-          <div className='border-b border-grey1 pb-10'>
-            <div className='grid grid-cols-1 sm:grid-cols-3 items-center'>
-              <div className='col-span-2'>
-                <h2 className='pt-10 mobile sm:main'>{props.seeMeasuresTitle}</h2>
-                <div className='block py-4 sm:hidden'>
-                  <Image
-                    src={props.seeMeasuresImage}
-                    alt={props.seeMeasuresImageAlt}
-                    layout='responsive'
-                  />
-                </div>
-                <p className='body-text-mobile sm:body-text py-5 max-w-2xl'>
-                  {props.seeMeasuresText}
-                </p>
-                <Link href={props.seeMeasuresLink}>
-                  <a>
-                    <span className='text-greenLink link-mobile sm:link'>
-                      {props.seeMeasuresLinkText}
-                    </span>
-                  </a>
-                </Link>
-              </div>
-              <div className='hidden sm:block'>
-                <Image
-                  src={props.seeMeasuresImage}
-                  alt={props.seeMeasuresImageAlt}
-                  layout='responsive'
-                />
-              </div>
-            </div>
-          </div>
-          <div className='border-b border-grey1 pb-10'>
-            <div className='grid grid-cols-1 sm:grid-cols-3 items-center'>
-              <div className='col-span-2'>
-                <h2 className='pt-10 mobile sm:main'>{props.samenhangTitle}</h2>
-                <div className='block py-4 sm:hidden'>
-                  <Image
-                    src={props.samenhangImage}
-                    alt={props.samenhangImageAlt}
-                    layout='responsive'
-                  />
-                </div>
-                <p className='body-text-mobile sm:body-text py-5 max-w-2xl'>
-                  {props.samenhangText}
-                </p>
-                <Link href={props.samenhangLink}>
-                  <a>
-                    <span className='text-greenLink link-mobile sm:link'>
-                      {props.samenhangLinkText}
-                    </span>
-                  </a>
-                </Link>
-              </div>
-              <div className='hidden sm:block'>
-                <Image
-                  src={props.samenhangImage}
-                  alt={props.samenhangImageAlt}
-                  layout='responsive'
-                />
-              </div>
-            </div>
-          </div>
-          <div className='border-b border-grey1 pb-40 pt-10'>
-            <div className='grid grid-cols-1 sm:grid-cols-3 items-center'>
-              <div className='col-span-2'>
-                <h2 className='pt-10 mobile sm:main'>{props.welkeTitle}</h2>
-                <div className='block py-4 sm:hidden'>
-                  <Image src={props.welkeImage} alt={props.welkeImageAlt} layout='responsive' />
-                </div>
-                <p className='body-text-mobile sm:body-text py-5 max-w-2xl'>{props.welkeText}</p>
-                <Link href={props.welkeLink}>
-                  <a>
-                    <span className='text-greenLink link-mobile sm:link'>
-                      {props.welkeLinkText}
-                    </span>
-                  </a>
-                </Link>
-              </div>
-              <div className='hidden sm:block'>
-                <Image src={props.welkeImage} alt={props.welkeImageAlt} layout='responsive' />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+          <ThemeBottomSection props={props} />
     </div>
   );
 }
