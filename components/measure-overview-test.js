1b8bc27391503134af4d6c0ef0cdf8f67b67aaab
theocampbell <57679708+theocampbell@users.noreply.github.com>
2022-12-20 14:24:31 +0100

    add conditional text to tooltip

--- components/measure-overview-test.js
+++ components/measure-overview-test.js
@@ -74,7 +74,7 @@ export default function MeasureOverviewTest({ viewport, children, data, ...props
             <span className='font-manrope font-semibold text-lg text-black1'>
               Juridisch invloed
             </span>
-            <JITooltip>
+            <JITooltip data={data}>
               <svg className='w-6 h-6 fill-current text-black mx-2' viewBox='0 0 26 26'>
                 <circle cx='12' cy='15' r='10' fill='#979797' />
                 <path
