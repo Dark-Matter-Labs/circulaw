78c98e608e85f60beeccea1f9093c9a2c182a141
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-01-23 16:43:29 +0100

    remove s on matrassen

--- components/nav.js
+++ components/nav.js
@@ -236,7 +236,7 @@ export default function Nav() {
                 </Disclosure.Button>
                 <Disclosure.Button
                   as='a'
-                  href='/matrassens'
+                  href='/matrassen'
                   className='ml-5 border-transparent text-gray-900 border-b border-black-white-300 pl-8 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-5'
                 >
                   Matrassen
