42618745e332f18204592f55bdc7d0864fb8bbc7
Gurden Batra <gurden.batra@gmail.com>
2023-01-24 16:43:05 +0100

    remove space

--- components/custom-button.js
+++ components/custom-button.js
@@ -2,6 +2,7 @@ const buttonColors = {
   home: 'border-black-white-200 bg-black-white-200 shadow hover:bg-green-600 text-green-600 hover:text-black-white-200 transition ease-in-out hover:duration-150 focus:outline-none',
   actionPanel:
     'border-green-600 bg-green-600 hover:bg-transparent text-black-white-200 hover:text-green-600 transition ease-in-out hover:duration-150 focus:outline-none',
+  nav: 'border-green-500 bg-green-500 hover:bg-transparent text-black-white-200 hover:text-green-600 transition ease-in-out hover:duration-150 focus:outline-none',
   whiteBackground:
     'border-green-600 bg-transparent hover:bg-green-600 text-green-600 hover:text-black-white-200 transition ease-in-out hover:duration-150 focus:outline-none',
   toPdf: 'border-green-300 text-black-white-800 bg-green-300',
@@ -13,7 +14,7 @@ export default function CustomButton({ color, children, ...props }) {
     <button
       {...props}
       type='button'
-      className={`inline-flex rounded-full items-center px-4 py-2 border  button ${colorClasses}`}
+      className={`inline-flex rounded-full items-center px-4 py-2 border button ${colorClasses}`}
     >
       {children}
     </button>
