a5ed1cc3222da369db28bacae3525072783ef65c
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-01-25 10:24:44 +0100

    update matress content

--- pages/measures/matrassen.js
+++ pages/measures/matrassen.js
@@ -13,8 +13,8 @@ export default function Measures({ laws }) {
       <MeasuresLayout
         totalNumberOfLaws={laws.length}
         thema='matrassen'
-        heading='Circulaire matrasketen'
-        introPara={`need to add text here for matrassen case ${laws.length}`}
+        heading='Instrumenten om de circulariteit van de matrasketen te bevorderen'
+        introPara='We hebben x kansrijke instrumenten gevonden die je kunt inzetten als het gaat om matrassen. Met sommige van deze instrumenten is al praktijkervaring opgedaan, met andere nog niet. Ga aan de slag! Met jouw ervaringen help je anderen weer verder.'
         icon={MatrassenIcon} // need to create a new icon for matress case
         searchTitle='Zoek in circulaire matrasketen'
       />
