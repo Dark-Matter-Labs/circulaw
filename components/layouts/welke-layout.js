36ef0d6516e43dc2b372fc58e011918a04866134
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-01-30 15:08:00 +0100

    dynamically import all laws in the right cat

--- components/layouts/welke-layout.js
+++ components/layouts/welke-layout.js
@@ -4,7 +4,11 @@ import Image from 'next/image';
 import MeasureLinks from '../measure-links-dropdown';
 
 export default function WelkeLayout(props) {
+  const allRegionLaws = props.allRegionLaws;
+  const provLaws = props.provLaws;
   const gemLaws = props.gemLaws;
+  const natLaws = props.natLaws;
+
   return (
     <>
       {/* Header */}
@@ -36,8 +40,8 @@ export default function WelkeLayout(props) {
       {/* DYNAMIC IMAGE */}
       <div className='global-margin'>
         <div className='flex grid-cols-3 items-start justify-center'>
-          {/* HEADING */}
-          <div className='grid col-span-1 items-start justify-center h-full w-80'>
+          {/* LEFT HAND SIDE */}
+          <div className='grid col-span-1 items-start justify-center h-full w-80 mt-10'>
             <div className='w-80 h-10 border border-green-800 rounded-full text-black-white-800 bg-black-white-200 flex items-center justify-left'>
               <div className='h-10 w-10 border-r border-t border-b border-green-800 rounded-full flex items-center justify-center'>
                 <h5 className='mobile sm:desktop'>A</h5>
@@ -47,8 +51,8 @@ export default function WelkeLayout(props) {
               </div>
             </div>
             <div className='pl-4 pt-6'>
-              {gemLaws &&
-                gemLaws.map((gemLaw) => (
+              {allRegionLaws &&
+                allRegionLaws.map((gemLaw) => (
                   <div key={gemLaw.titel} className='flex items-center justify-start max-w-80'>
                     <span className='pr-2 inline-block'>
                       {' '}
@@ -74,22 +78,136 @@ export default function WelkeLayout(props) {
           </div>
 
           {/* DIAGRAM */}
-          <div className='w-[37.5rem] h-[37.5rem] flex items-center justify-center'>
+          <div className='w-[34rem] h-[34rem] min-w-[34rem] min-h-[34rem] flex items-center justify-center mx-4'>
             <div className='w-full h-full border-1 border-green-800 bg-green-800 rounded-full flex items-end justify-center'>
               <div className='w-5/6 h-5/6 border-1 border-green-500 bg-green-500 rounded-full flex items-end justify-center'>
                 <div className='w-[70%] h-[70%] border-1 border-green-400 bg-green-400 rounded-full flex items-end justify-center'>
-                  <div className='w-[33%] h-[37.5rem] rounded-[50%] overview-radial-gradient'></div>
+                  <div className='w-[33%] h-[34rem] rounded-[50%] overview-radial-gradient'></div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className='col-span-1'>
-            <div className='w-80 h-10'>top button</div>
+          {/* RIGHT HAND SIDE */}
+          <div className='grid col-span-1 items-start justify-center h-full w-80 mt-10'>
+            {/* National */}
+            {natLaws && (
+              <div className='pb-6'>
+                <div className='w-80 h-10 bg-green-800 text-white rounded-full flex items-center justify-left'>
+                  <div className='flex items-center justify-center w-full h-full -ml-10'>
+                    <h4 className='mobile sm:desktop'>National</h4>
+                  </div>
+                  <div className='h-10 w-10 border-l border-t border-b border-white rounded-full flex items-center justify-center'>
+                    <h5 className='mobile sm:desktop'>Nat</h5>
+                  </div>
+                </div>
+                <div className='pl-4 pt-6'>
+                  {natLaws?.map((law) => (
+                    <div key={law.titel} className='flex items-center justify-start max-w-80'>
+                      <span className='pr-2 inline-block'>
+                        {' '}
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          strokeWidth={2}
+                          stroke='#028352'
+                          className='w-4  h-4'
+                        >
+                          <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3'
+                          />
+                        </svg>
+                      </span>
+                      <h5 className='py-2 inline-block mobile sm:desktop'>{law.titel}</h5>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+
+            {/* Provinciaal */}
+            {provLaws && (
+              <div className='pb-6'>
+                <div className='w-80 h-10 rounded-full text-white bg-green-500 flex items-center justify-left'>
+                  <div className='flex items-center justify-center w-full h-full -ml-10'>
+                    <h4 className='mobile sm:desktop'>Provinciaal</h4>
+                  </div>
+                  <div className='h-10 w-10 border-l border-t border-b border-white rounded-full flex items-center justify-center'>
+                    <h5 className='mobile sm:desktop'>Pr</h5>
+                  </div>
+                </div>
+                <div className='pl-4 pt-6'>
+                  {provLaws &&
+                    provLaws.map((law) => (
+                      <div key={law.titel} className='flex items-center justify-start max-w-80'>
+                        <span className='pr-2 inline-block'>
+                          {' '}
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            strokeWidth={2}
+                            stroke='#028352'
+                            className='w-4  h-4'
+                          >
+                            <path
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
+                              d='M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3'
+                            />
+                          </svg>
+                        </span>
+                        <h5 className='py-2 inline-block mobile sm:desktop'>{law.titel}</h5>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            )}
+            {/* Gemeentelijk */}
+            {gemLaws && (
+              <div>
+                <div className='w-80 h-10 rounded-full bg-green-400 text-white flex items-center justify-right'>
+                  <div className='flex items-center justify-center w-full h-full -ml-10'>
+                    <h4 className='mobile sm:desktop'>Gemeentelijk</h4>
+                  </div>
+                  <div className='h-10 w-10 border-l border-t border-b border-white rounded-full flex items-center justify-center'>
+                    <h5 className='mobile sm:desktop'>Gem</h5>
+                  </div>
+                </div>
+                <div className='pl-4 pt-6'>
+                  {gemLaws.map((law) => (
+                    <div key={law.titel} className='flex items-center justify-start max-w-80'>
+                      <span className='pr-2 inline-block'>
+                        {' '}
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          strokeWidth={2}
+                          stroke='#028352'
+                          className='w-4  h-4'
+                        >
+                          <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3'
+                          />
+                        </svg>
+                      </span>
+                      <h5 className='py-2 inline-block mobile sm:desktop'>{law.titel}</h5>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
 
+      {/* TEXT COMPONENT */}
       <div className='global-margin mb-20'>
         <div className='max-w-3xl mx-auto'>
           <p className=' p-lg text-black-white-800 pb-6'>{props.p1}</p>
--- pages/circulaire-windturbines/welke-overheid-heeft.js
+++ pages/circulaire-windturbines/welke-overheid-heeft.js
@@ -4,16 +4,13 @@ import Layout from '../../components/layouts/layout';
 import WelkeLayout from '../../components/layouts/welke-layout';
 import WindmillIcon from '../../public/windmill.svg';
 
-const lawsQuery = `
-*[_type == "measure" && thema == "circulaire-windturbines"]
-`;
-
-export default function InfoPage({ laws }) {
-  // const [laws, setLaws] = useState()
+export default function InfoPage() {
+  const [allRegionLaws, setAllRegionLaws] = useState();
+  const [natLaws, setNatLaws] = useState();
+  const [provLaws, setProvLaws] = useState();
   const [gemLaws, setGemLaws] = useState();
-  // const [natLaws, setNatLaws] = useState()
-  // const [provLaws, setProvLaws] = useState()
-  console.log(laws);
+
+  // fetch all laws with National + Provinciaal + Nationaal
   useEffect(() => {
     client
       .fetch(
@@ -21,8 +18,42 @@ export default function InfoPage({ laws }) {
     *[_type == "measure" && thema == "circulaire-windturbines" && "Gemeentelijk" in overheidslaag && "Provinciaal" in overheidslaag && "Nationaal" in overheidslaag]
     `,
       )
+      .then((data) => setAllRegionLaws(data));
+  }, []);
+
+  // fetch all laws with only National
+  useEffect(() => {
+    client
+      .fetch(
+        `
+        *[_type == "measure" && thema == "circulaire-windturbines" && length(overheidslaag) < 3 && "Nationaal" in overheidslaag]
+    `,
+      )
+      .then((data) => setNatLaws(data));
+  }, []);
+
+  // fetch all laws with only Provincial
+  useEffect(() => {
+    client
+      .fetch(
+        `
+        *[_type == "measure" && thema == "circulaire-windturbines" && length(overheidslaag) < 3  && "Provinciaal" in overheidslaag]
+    `,
+      )
+      .then((data) => setProvLaws(data));
+  }, []);
+
+  // fetch all laws with only Gemeentelijk
+  useEffect(() => {
+    client
+      .fetch(
+        `
+        *[_type == "measure" && thema == "circulaire-windturbines" && length(overheidslaag) < 3 && "Gemeentelijk" in overheidslaag]
+    `,
+      )
       .then((data) => setGemLaws(data));
   }, []);
+
   return (
     <Layout>
       <WelkeLayout
@@ -41,13 +72,11 @@ export default function InfoPage({ laws }) {
       eisen te stellen bij gronduitgifte - door verkoop, erfpacht en huurovereenkomsten -
       wordt het plaatsen van circulaire windturbines aangemoedigd.'
         p4=''
+        allRegionLaws={allRegionLaws}
+        natLaws={natLaws}
+        provLaws={provLaws}
         gemLaws={gemLaws}
       />
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  const laws = await client.fetch(lawsQuery);
-  return { props: { laws: laws } };
 }
