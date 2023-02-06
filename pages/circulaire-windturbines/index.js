6d6e80d3f0c046557b1992a22d9fc056cca5bb79
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-02-06 14:00:59 +0100

    add bg color to bottom and fix hydration error

--- pages/circulaire-windturbines/index.js
+++ pages/circulaire-windturbines/index.js
@@ -42,8 +42,9 @@ export default function Windturbine({ laws }) {
         extendedMeasureHeading='instrumenten om circulariteit van windturbines te bevorderen'
         extendedMeasureSubtitle='Met voorbeelden.'
       />
-      <div className='global-margin pt-10 pb-20'>
-        <p className='p-lg'>
+      <div className='bg-[#F8FAF8]'>
+      <div className='global-margin pt-10 pb-20 '>
+        <div className='p-lg'>
           De informatie over windturbines is mede tot stand gekomen door nauwe samenwerking met de{' '}
           <span className='text-green-500 link-lg inline-block'>
             <a href='https://www.noord-holland.nl/' target='_blank' rel='noopener noreferrer'>
@@ -60,7 +61,8 @@ export default function Windturbine({ laws }) {
             </span>
             .{' '}
           </div>{' '}
-        </p>
+        </div>
+      </div>
       </div>
     </Layout>
   );
