a7bb550b640e7baa6387c24f213f18fbf09678dd
Gurden Batra <gurden.batra@gmail.com>
2023-02-07 12:38:43 +0200

    Prettier

--- studio/components/logo.jsx
+++ studio/components/logo.jsx
@@ -1,7 +1,10 @@
-
-
 export default function Logo() {
-    return (
-    <img src = '/Circulaw_logotype_home.png' alt='circulow logo' width={75} style={{marginTop: '-10px', marginBottom: '-10px' }}/>
-    )
+  return (
+    <img
+      src='/Circulaw_logotype_home.png'
+      alt='circulow logo'
+      width={75}
+      style={{ marginTop: '-10px', marginBottom: '-10px' }}
+    />
+  );
 }
