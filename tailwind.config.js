962ccfa173e386f14b43b5801dd60111fa946343
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-02-22 10:11:13 +0100

    add utils to tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './public/**/*.html',
-    './lib/**/*.{js,ts,jsx,tsx}'
+    './lib/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false,
   theme: {
