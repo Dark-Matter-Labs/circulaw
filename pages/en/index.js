8ca54f9a37f492153c3bac89d822af909eb69373
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-02-27 15:54:49 +0100

    create page

---
+++ pages/en/index.js
@@ -0,0 +1,16 @@
+import Layout from '../../components/layouts/layout';
+;
+
+
+export default function English() {
+    return (
+        <>
+        <Layout>
+            <div className='h-screen'>
+                ENGLISH PAGE
+            </div>
+        </Layout>
+
+        </>
+    )
+}
