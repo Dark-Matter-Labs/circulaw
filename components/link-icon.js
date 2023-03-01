fcb99f4c32c6f4903b8cfb882445ac9116d1bf5a
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-03-01 17:13:03 +0100

    fix link icon component

--- components/link-icon.js
+++ components/link-icon.js
@@ -1,15 +1,10 @@
 import Image from 'next/image';
 import linkIcon from '../public/icons/link-icon.svg';
 
-const sizes = {
-  mob: 'relative',
-  desktop: 'relative',
-};
 
-export default function LinkIcon({ size }) {
-  let sizeClasses = sizes[size];
+export default function LinkIcon() {
   return (
-    <span className={`pl-0.5 inline-block h-6 w-6 -mb-1 relative ${sizeClasses}`}>
+    <span className='pl-0.5 inline-block h-5 w-5 sm:h-6 sm:w-6 -mb-1 relative'>
       <Image className='' alt='new tab' src={linkIcon} width={30} height={10} />
     </span>
   );
