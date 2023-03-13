1d66fe07e76bf3dbebb515f86260c58e9b0a7912
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-03-13 15:55:27 +0100

    delete get over function

--- utils/nav-structure.js
+++ utils/nav-structure.js
@@ -18,40 +18,7 @@ const waardeketens = [
       className: '',
     },
   ];
-  
-  
-  const over = [
-    {
-      name: 'Wat is CircuLaw?',
-      description: 'description',
-      href: '/wat-is-circulaw',
-      className: '',
-    },
-    {
-      name: 'Wat vind je nu op CircuLaw?',
-      description: 'description',
-      href: '/status-en-ambities',
-      className: '',
-    },
-    {
-      name: 'Wetsanalyse vanuit circulaire blik',
-      description: 'description',
-      href: '/wetsanalyse-met-circulaire-blik',
-      className: '',
-    },
-    {
-      name: 'Wie maken CircuLaw?',
-      description: 'description',
-      href: '/wie-maken-circulaw',
-      className: '',
-    },
-  ];
-
 
 export function get_waardeketens() {
     return waardeketens;
-}
-
-export function get_over() {
-    return over;
 }
