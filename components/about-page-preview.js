93dfe8fe29c0f998ce394c91ee40d70c3a6069dd
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-04-11 12:49:45 +0200

    fix image in sanity preview

--- components/about-page-preview.js
+++ components/about-page-preview.js
@@ -7,9 +7,10 @@ import { aboutPagePTComponents } from '../lib/portable-text/pt-components';
 
 export default function AboutPagepreview({query, queryParams}) {
     const data = {aboutPage: usePreview(null, query, queryParams)}
+    console.log(data?.aboutPage?.aboutPageContent)
     return (
       <div>
-      <div className='bg-about-header bg-cover h-40'>
+      <div className='bg-about-header bg-cover bg-center h-40'>
       </div>
         <div className='global-margin pb-8 text-black-white-800'>
         <div className='grid grid-cols-1 w-full md:grid-cols-3'>
@@ -17,14 +18,14 @@ export default function AboutPagepreview({query, queryParams}) {
             <div className='breadcrumb pt-8 text-green-500'>
               <Link href='/'>Home &gt;</Link>
             </div>
-            <div className='max-w-4xl'>
+            <div className='max-w-3xl'>
               <h1 className='lg:block sm:pt-10 py-6 sm:pb-10 mobile sm:desktop text-green-500'>
                 {data?.aboutPage?.pageTitle}
               </h1>
               <PortableText value={data?.aboutPage?.aboutPageContent} components={aboutPagePTComponents} />
             </div>
           </div>
-            <div className='mt-3 md:ml-5 lg:mx-5 lg:mb-20 lg:mt-32 max-w-sm col-span-1'>
+            <div className='mt-3 max-w-3xl lg:ml-12 lg:mb-20 lg:mt-32 col-span-1'>
               <OverNav pagename={data?.aboutPage?.slug.current} />
             </div>
         </div>
--- components/about-page.js
+++ components/about-page.js
@@ -7,22 +7,22 @@ import { aboutPagePTComponents } from '../lib/portable-text/pt-components';
 export default function AboutPageComponent({data}) {
     return (
       <div>
-      <div className='bg-about-header bg-cover h-40'>
+      <div className='bg-about-header bg-cover bg-center h-40'>
       </div>
         <div className='global-margin pb-8 text-black-white-800'>
         <div className='grid grid-cols-1 w-full lg:grid-cols-3'>
-          <div className='col-span-2'>
+          <div className='lg:col-span-2'>
             <div className='breadcrumb pt-8 text-green-500'>
               <Link href='/'>Home &gt;</Link>
             </div>
-            <div className='max-w-4xl'>
+            <div className='max-w-3xl'>
               <h1 className='lg:block sm:pt-10 py-6 sm:pb-10 mobile sm:desktop text-green-500'>
                 {data?.aboutPage?.pageTitle}
               </h1>
               <PortableText value={data?.aboutPage?.aboutPageContent} components={aboutPagePTComponents} />
             </div>
           </div>
-            <div className='mt-3 lg:-mr-[500px] lg:ml-12 lg:mb-20 lg:mt-32 col-span-1'>
+            <div className='mt-3  max-w-3xl lg:ml-12 lg:mb-20 lg:mt-32 col-span-1'>
               <OverNav pagename={data?.aboutPage?.slug.current} />
             </div>
         </div>
