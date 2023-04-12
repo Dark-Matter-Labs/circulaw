63373e791175a062decde74b83fdd0b8f3671ab0
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-04-12 16:31:55 +0200

    clean

--- components/about-page.js
+++ components/about-page.js
@@ -33,10 +33,7 @@ export default function AboutPageComponent({ data }) {
             </div>
           </div>
           <div className='hidden lg:block mt-3 lg:ml-12 lg:mb-20 lg:mt-32 col-span-1'>
-            <OverNav
-              currentSlug={data?.aboutPage?.slug.current}
-              slugs={data?.aboutPageSlugs}
-            />
+            <OverNav currentSlug={data?.aboutPage?.slug.current} slugs={data?.aboutPageSlugs} />
           </div>
           <div className='block lg:hidden'>
             <AboutPageDropdown
