e664687285629d6f17e84af73c374ac6834bf8ff
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-04-18 15:51:36 +0200

    clean

--- components/measure-table.js
+++ components/measure-table.js
@@ -59,9 +59,22 @@ export default function MeasureTable({ data }) {
                   >
                     {data?.measure?.artikel}
                     <span className='pl-0.5 inline-block -mb-0.5 h-5 w-5 relative'>
-                    <svg className="stroke-current" width="20" height="20" viewBox="0 -1 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10 6H6C4.89543 6 4 6.89543 4 8V18C4 19.1046 4.89543 20 6 20H16C17.1046 20 18 19.1046 18 18V14M14 4H20M20 4V10M20 4L10 14" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                     </svg>
+                      <svg
+                        className='stroke-current'
+                        width='20'
+                        height='20'
+                        viewBox='0 -1 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          d='M10 6H6C4.89543 6 4 6.89543 4 8V18C4 19.1046 4.89543 20 6 20H16C17.1046 20 18 19.1046 18 18V14M14 4H20M20 4V10M20 4L10 14'
+                          stroke=''
+                          stroke-width='2'
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                        />
+                      </svg>
                     </span>
                   </a>
                 </span>
