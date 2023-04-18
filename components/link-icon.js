889eb1fbb0edbad9c0c2a6208ae2f18c9f8c61df
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-04-18 15:51:08 +0200

    make link icon hover with link

--- components/link-icon.js
+++ components/link-icon.js
@@ -1,10 +1,10 @@
-import Image from 'next/image';
-import linkIcon from '../public/icons/link-icon.svg';
 
 export default function LinkIcon() {
   return (
-    <span className='pl-0.5 inline-block h-5 w-5 sm:h-6 sm:w-6 -mb-1 relative'>
-      <Image className='' alt='new tab' src={linkIcon} width={30} height={10} />
+    <span className='pl-0.5 inline-block h-5 w-5 sm:h-6 sm:w-6 -mb-1 relative '>
+    <svg className="stroke-current" width="24" height="24" viewBox="0 -1 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 6H6C4.89543 6 4 6.89543 4 8V18C4 19.1046 4.89543 20 6 20H16C17.1046 20 18 19.1046 18 18V14M14 4H20M20 4V10M20 4L10 14" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
     </span>
   );
-}
+}  
--- components/measure-table.js
+++ components/measure-table.js
@@ -1,5 +1,4 @@
 import { PortableText } from '@portabletext/react';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { juridischeToelichtingComponernts } from '../lib/portable-text/pt-components';
 
@@ -59,14 +58,10 @@ export default function MeasureTable({ data }) {
                     rel='noreferrer'
                   >
                     {data?.measure?.artikel}
-                    <span className='pl-0.5 inline-block h-5 w-5 -mb-0.5 relative'>
-                      <Image
-                        className=''
-                        alt='new tab'
-                        src='/icons/link-icon.svg'
-                        width={20}
-                        height={20}
-                      />
+                    <span className='pl-0.5 inline-block -mb-0.5 h-5 w-5 relative'>
+                    <svg className="stroke-current" width="20" height="20" viewBox="0 -1 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 6H6C4.89543 6 4 6.89543 4 8V18C4 19.1046 4.89543 20 6 20H16C17.1046 20 18 19.1046 18 18V14M14 4H20M20 4V10M20 4L10 14" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                     </svg>
                     </span>
                   </a>
                 </span>
