97308456870b7e5cd6af71ecf78b702c7c942fc6
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-04-26 14:11:11 +0200

    add first draft of schema

--- studio/schemas/documents/news.js
+++ studio/schemas/documents/news.js
@@ -6,5 +6,43 @@
 // button
 
 export default {
-    
+    title: 'News Item',
+    name: 'newsItem',
+    type: 'document',
+    fields: [
+        {
+            title:'Title',
+            name: 'title',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+        },
+        {
+            title:'Text Content',
+            name: 'text',
+            type: 'text',
+        },
+        {
+            title:'External Link',
+            name: 'externalLink',
+            type: 'url',
+            // validation
+        },
+        {
+            title:'Internal Link',
+            name: 'internalLink',
+            type: 'ref',
+            // do we want to ref this?
+        },
+        {
+            title:'Image',
+            name: 'image',
+            type: 'image',
+        },
+        {
+            title:'Date',
+            name: 'date',
+            type: 'date',
+            description: 'Date that will be displayed on the card. This will not affect the order of the card'
+        },
+    ]
 }
