5b1b8fa69709dd0517b0408c004fb9725b80be92
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-04-26 17:17:44 +0200

    add extra breakpoint

--- components/news-items.js
+++ components/news-items.js
@@ -10,17 +10,17 @@ export default function NewsItems({ newsItems }) {
 
     const formatDate = (date) => {
         let dateObject = new Date(date);
-        return dateObject.toLocaleDateString('nl', {day:'numeric' , month:'short', year:'numeric'});
+        return dateObject.toLocaleDateString('en', {day:'numeric' , month:'short', year:'numeric'});
       };
   return (
     <>
-      <div className='flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-4 items-center justify-between gap-x-4 gap-y-8 overflow-hidden'>
+      <div className='flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center justify-between gap-x-4 gap-y-8 overflow-hidden'>
         {newsItems?.map((item) => (
           <div
             key={item?._id}
             className={`${
               newsItems[1] === item || newsItems[3] === item ? 'bg-green-600' : 'bg-green-800'
-            } h-[335px] w-full max-w-[310px] rounded-cl text-black-white-100 flex flex-col items-start p-6`}
+            } h-[335px] w-full min-w-[270px] max-w-[310px] rounded-cl text-black-white-100`}
           >
             {/*  
             {item?.image && (
@@ -33,7 +33,7 @@ export default function NewsItems({ newsItems }) {
                 />
               </div>
             )} */}
-
+                <div className='w-full h-full p-6 flex flex-col items-start'>
               {item?.date && <div className='capitilize pb-1'>{formatDate(item?.date)}</div>}
               <div className='pb-4'>
                 <h3 className='mobile sm:desctop'>{item?.title}</h3>
@@ -42,22 +42,23 @@ export default function NewsItems({ newsItems }) {
                 <p className='mobile sm:desktop pb-4'>{item?.text}</p>
               </div>
               {item?.internalLink && 
-              <div>
-                <Link href={item?.internalLink}>
+              <div className='flex grow'>
+                <Link href={item?.internalLink} className='self-end'>
                 <CustomButton color='home'>{item?.internalLinkText}<ArrowRightIcon className='inline-block h-4 w-4 ml-1 place-self-end' aria-hidden='true' />
                 
                 </CustomButton>
                 </Link>
               </div>}
               {item.externalLink && 
-                <div>
-                    <Link href={item.externalLink} target='_blank'>
+                <div className='flex grow'>
+                    <Link href={item.externalLink} target='_blank' className='self-end'>
                         <span className='text-green-500 inline link-base sm:link-lg link-interaction break-words'>{item.externalLinkText}
                         <LinkIcon />
                         </span>
                     </Link>
                 </div>
               }
+            </div>
             </div>
         ))}
       </div>
