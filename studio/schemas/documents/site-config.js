9a13f75b8224b43cf6f6f4b8e014a24858869e80
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-05-09 15:07:49 +0200

    add groups to home page schema

--- studio/schemas/documents/site-config.js
+++ studio/schemas/documents/site-config.js
@@ -1,13 +1,32 @@
+// siteConfig is actually labeled as Home Page in the CMS however in the code it will remain siteConfig. 
+// we could write a patch to update the name of the schema in the code to make it correspond with Home Page. 
+
+
 export default {
   name: 'siteConfig',
   type: 'document',
   title: 'Home Page',
+  groups: [
+    {
+      name: 'navigation',
+      title: 'Navigation'
+    },
+    {
+      name: 'aboutPage',
+      title: 'About Page'
+    },
+    {
+      name: 'siteSettings',
+      title: 'Site Settings'
+    }
+  ],
   fields: [
     {
       title: 'URL',
       name: 'url',
       type: 'url',
       description: 'The main site url. Used to create canonical url',
+      group: 'siteSettings'
     },
     {
       title: 'Over CircuLaw',
@@ -15,6 +34,7 @@ export default {
       description: 'Select menu for about navigation',
       type: 'reference',
       to: { type: 'navigation' },
+      group: 'navigation'
     },
     {
       title: 'Vraag & antwoord',
@@ -22,6 +42,7 @@ export default {
       description: 'Select menu for FAQ navigation',
       type: 'reference',
       to: { type: 'navigation' },
+      group: 'navigation',
     },
     {
       title: "Thema's",
@@ -29,28 +50,33 @@ export default {
       description: 'Select menu for Thema navigation',
       type: 'reference',
       to: { type: 'navigation' },
+      group: 'navigation'
     },
     {
       title: 'About Section Title',
       name: 'aboutSectionTitle',
       type: 'string',
       description: 'The title of the about section on the home page',
+      group: 'aboutPage'
     },
     {
       title: 'About Section Text',
       name: 'aboutSectionText',
       type: 'text',
+      group: 'aboutPage'
     },
     {
       title: 'About Section Mobile Text',
       name: 'aboutSectionMobileText',
       type: 'text',
+      group: 'aboutPage'
     },
     {
       title: 'About Section Page',
       name: 'aboutSectionPage',
       type: 'reference',
       to: [{ type: 'aboutPage' }],
+      group: 'aboutPage'
     },
   ],
 };
