2732efd17ddb32a5e95248a4663ada29ca63f43a
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-06-12 12:33:14 +0200

    partners

--- studio/schemas/documents/partners.js
+++ studio/schemas/documents/partners.js
@@ -6,7 +6,8 @@ export default {
     {
       name: 'partners',
       type: 'array',
-      title: 'Partner Logo',
+      title: 'Partnerlijst',
+      description:'De lijst van partners is in dezelfde volgorde zichtbaar op de website. Klik op toevoegen om nieuwe partner toe te voegen. Max 20 partners mogelijk.',
       of: [{ type: 'partner' }],
     },
   ],
