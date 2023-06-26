020f1f13e5dc274f768f72ba6c1a221a20ce57f0
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-06-26 10:34:21 +0200

    remove transitions from beta banner

--- components/beta-banner.js
+++ components/beta-banner.js
@@ -1,28 +1,13 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { ArrowRightIcon } from '@heroicons/react/outline';
 
-export default function BetaBanner({ scrollEffect }) {
-  const router = useRouter();
+export default function BetaBanner() {
   return (
-    <div
-      className={`${
-        scrollEffect
-          ? 'bg-transparent transition-colors duration-300'
-          : 'bg-[#E6E6E6] transition-colors duration-300'
-      } w-52 rounded-b-[16px] px-2 flex items-center justify-center`}
-    >
+    <div className='w-52 rounded-b-[16px] px-2 flex items-center justify-center bg-[#E6E6E6]'>
       <div
-        className={`${
-          scrollEffect
-            ? `${
-                router.pathname === '/' ? 'text-green-600' : 'text-black-white-200'
-              } transition-colors duration-300`
-            : 'text-black-white-800 transition-colors duration-300'
-        }`}
-      >
+        className='text-black-white-800'>
         <h5 className='inline bold'>Beta</h5> <h5 className='inline mobile'>versie - </h5>
-        <h5 className={`${scrollEffect ? '' : 'link-interaction'} inline underline mobile`}>
+        <h5 className='link-interaction inline underline mobile'>
           <Link href='/beta'>Lees meer </Link>
           <ArrowRightIcon className='h-3 w-3 inline' />
         </h5>
