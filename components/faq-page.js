c861995801aab705cf9db561cde8b87b79e88b44
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-07-04 15:00:45 +0200

    add styles to faq sections

--- components/faq-page.js
+++ components/faq-page.js
@@ -2,27 +2,27 @@ import { PortableText } from '@portabletext/react';
 import Link from 'next/link';
 import { FAQPagePTComponents } from '../lib/portable-text/pt-components';
 import { Disclosure } from '@headlessui/react';
-import { ChevronUpIcon } from '@heroicons/react/outline';
+import { PlusIcon, MinusIcon } from '@heroicons/react/outline';
 
 export default function FAQPageComponent({ data }) {
   return (
     <div className='global-margin pb-8 text-black-white-800'>
-      <div className='grid grid-cols-1 w-full md:grid-cols-3'>
-        <div className='col-span-2'>
+      <div className='grid grid-cols-1 w-full justify-center '>
+        <div className=''>
           <div className='breadcrumb pt-8 text-green-500 link-interaction'>
             <Link href='/'>Home &gt;</Link>
           </div>
-          <div className='max-w-4xl'>
+          <div className='max-w-4xl mx-auto'>
             <h1 className='lg:block sm:pt-10 py-6 sm:pb-10 mobile sm:desktop text-green-500'>
               {data?.pageTitle}
             </h1>
 
-            <div>
+            <div className=''>
               {data.FAQPageContent.map((item) => (
                 <div key={item._key}>
                   {item?.sectionTitle && (
                     <div>
-                      <h2 className='mobile sm:desktop text-green-500 py-6'>
+                      <h2 className='mobile sm:desktop text-green-500 pt-6 pb-10'>
                         {' '}
                         {item?.sectionTitle}
                       </h2>
@@ -34,15 +34,22 @@ export default function FAQPageComponent({ data }) {
                       <Disclosure>
                         {({ open }) => (
                           <>
-                            <Disclosure.Button>
-                              <span>{item?.question}</span>
-                              <ChevronUpIcon
-                                className={`${
-                                  open ? 'rotate-180 transform' : ''
-                                } h-5 w-5 text-purple-500`}
-                              />
+                            <Disclosure.Button className='w-full border-t border-green-500'>
+                              <h3 className='mobile sm:desktop text-green-500 flex justify-between py-4 text-left w-full break-words'>{item?.question}  
+                              {open === true && 
+                              <span className='h-6 w-6 text-green-500 inline-block relative shrink-0	'>
+                              <MinusIcon
+           
+                              /> </span> }
+                              {open === false && 
+                              <span className='h-6 w-6 text-green-500 inline-block relative shrink-0	'>
+                              <PlusIcon
+                     
+                            /> </span>
+                              }
+                              </h3>
                             </Disclosure.Button>
-                            <Disclosure.Panel>
+                            <Disclosure.Panel className='mb-4 -mt-2'>
                               <PortableText
                                 value={item.response}
                                 components={FAQPagePTComponents}
@@ -55,9 +62,7 @@ export default function FAQPageComponent({ data }) {
                   )}
                 </div>
               ))}
-              <div>
-                <PortableText value={data?.faqPdf[0]} components={FAQPagePTComponents} />
-              </div>
+            
             </div>
           </div>
         </div>
