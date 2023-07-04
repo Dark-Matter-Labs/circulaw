cb7c5007df10e5cf71280abf9f512415dc4cfc2b
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-07-04 11:36:54 +0200

    add headless disclosure to questions

--- components/faq-page.js
+++ components/faq-page.js
@@ -1,10 +1,10 @@
 import { PortableText } from '@portabletext/react';
 import Link from 'next/link';
 import { FAQPagePTComponents } from '../lib/portable-text/pt-components';
+import { Disclosure } from '@headlessui/react';
+import { ChevronUpIcon } from '@heroicons/react/outline';
 
 export default function FAQPageComponent({ data }) {
-
-  console.log(data, 'hello')
   
   return (
     <div className='global-margin pb-8 text-black-white-800'>
@@ -22,13 +22,36 @@ export default function FAQPageComponent({ data }) {
               {data.FAQPageContent.map((item) => (
               
                 <div key={item._key}>
-                  {item?.sectionTitle}
-                  <br />
-                  {item.question}
-                  <br />
-                  {console.log(item.response)}
-                  <PortableText value = {item.response} components={FAQPagePTComponents}/>
-                  <br />
+                  {item?.sectionTitle && <div>
+                    <h2 className='mobile sm:desktop text-green-500 py-6'> {item?.sectionTitle}
+                    </h2>
+                    </div>}
+
+
+                    {item?.question  && 
+                    <div>
+                      <Disclosure>
+                        {({open}) => (
+                          <>
+                          <Disclosure.Button>
+                            <span>{item?.question}</span>
+                            <ChevronUpIcon
+                  className={`${
+                    open ? 'rotate-180 transform' : ''
+                  } h-5 w-5 text-purple-500`}
+                />
+
+                          </Disclosure.Button>
+                          <Disclosure.Panel>
+                          <PortableText value = {item.response} components={FAQPagePTComponents}/>
+                          </Disclosure.Panel>
+                          </>
+                        )}
+                      </Disclosure>
+
+                    </div>
+
+                    }
                   </div>  
               ))}
               <div>
