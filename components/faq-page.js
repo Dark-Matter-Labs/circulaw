168517606c37db19547a8bbf9a52e9b63c53bb16
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-07-04 09:47:54 +0200

    render content in the correct order

--- components/faq-page.js
+++ components/faq-page.js
@@ -1,8 +1,11 @@
-import Link from 'next/link';
 import { PortableText } from '@portabletext/react';
+import Link from 'next/link';
 import { FAQPagePTComponents } from '../lib/portable-text/pt-components';
 
 export default function FAQPageComponent({ data }) {
+
+  console.log(data, 'hello')
+  
   return (
     <div className='global-margin pb-8 text-black-white-800'>
       <div className='grid grid-cols-1 w-full md:grid-cols-3'>
@@ -14,7 +17,25 @@ export default function FAQPageComponent({ data }) {
             <h1 className='lg:block sm:pt-10 py-6 sm:pb-10 mobile sm:desktop text-green-500'>
               {data?.pageTitle}
             </h1>
-            <PortableText value={data?.FAQPageContent} components={FAQPagePTComponents} />
+            
+            <div>
+              {data.FAQPageContent.map((item) => (
+              
+                <div key={item._key}>
+                  {item?.sectionTitle}
+                  <br />
+                  {item.question}
+                  <br />
+                  {console.log(item.response)}
+                  <PortableText value = {item.response} components={FAQPagePTComponents}/>
+                  <br />
+                  </div>  
+              ))}
+              <div>
+                <PortableText value = {data?.faqPdf[0]} components={FAQPagePTComponents} />
+              </div>
+            </div>
+            
           </div>
         </div>
       </div>
