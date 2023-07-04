ea47558adafeb261bd37bdd2206a9ebead948165
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-07-04 11:29:08 +0200

    add descriptipon to pdf block in sanity

--- studio/schemas/documents/faq.js
+++ studio/schemas/documents/faq.js
@@ -38,6 +38,7 @@ export default {
       title: 'FAQ Pdf',
       name: 'faqPdf',
       type: 'array',
+      description: 'Only one PDF can be rendered on this page', 
       of: [
         {
           type: 'pdfBlock'
