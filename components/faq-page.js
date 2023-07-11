c2da93027759aeb4e0cac6f4df732251a43c0320
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-07-11 15:25:28 +0200

    add width to text

--- components/faq-page.js
+++ components/faq-page.js
@@ -62,7 +62,7 @@ export default function FAQPageComponent({ data }) {
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography as='div'>
+          <Typography as='div' className='w-4/5'>
           <PortableText
               value={item.response}
               components={FAQPagePTComponents}
