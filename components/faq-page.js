9388e9fd5d57574d9757df88055354bcde8ea36d
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-07-24 16:40:26 +0200

    clean

--- components/faq-page.js
+++ components/faq-page.js
@@ -10,9 +10,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 // import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-
 export default function FAQPageComponent({ data }) {
-
   const [expanded, setExpanded] = useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
@@ -31,50 +29,53 @@ export default function FAQPageComponent({ data }) {
               {data?.pageTitle}
             </h1>
             <div>
+              {data.FAQPageContent.map((item, i) => (
+                <div key={i}>
+                  {item?.sectionTitle && (
+                    <div>
+                      <h2 className='mobile sm:desktop text-green-500 pt-6 pb-10'>
+                        {' '}
+                        {item?.sectionTitle}
+                      </h2>
+                    </div>
+                  )}
 
-
-    {data.FAQPageContent.map((item, i) => ( 
-
-    
-
-      <div key={i}>
-          {item?.sectionTitle && (
-        <div>
-          <h2 className='mobile sm:desktop text-green-500 pt-6 pb-10'>
-            {' '}
-            {item?.sectionTitle}
-          </h2>
-        </div>
-      )}
-
-      {item?.question && 
-      <Accordion  expanded={expanded === `panel_${i}`} onChange={handleChange(`panel_${i}`)} className='border-t pb-12 pt-4 border-green-600 border-0 rounded-0 shadow-none my-0' disableGutters={true} square={true}> 
-        <AccordionSummary
-          expandIcon={expanded === `panel_${i}` ?  <MinusIcon className='h-5 w-6 text-green-600' /> : <PlusIcon className='h-6 w-6 text-green-600'/>}
-          aria-controls={`panel_${i}_bh-content`}
-          id={`panel_${i}_bh-header`}
-        >
-          <Typography as='h3' className='width-1/3 mobile sm:desktop text-green-600 mr-4'>
-       
-            {item.question}
-            
-           
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography as='div' className='w-5/6'>
-          <PortableText
-              value={item.response}
-              components={FAQPagePTComponents}
-            />
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      }
-      </div>
-))}
-
-    </div>
+                  {item?.question && (
+                    <Accordion
+                      expanded={expanded === `panel_${i}`}
+                      onChange={handleChange(`panel_${i}`)}
+                      className='border-t pb-12 pt-4 border-green-600 border-0 rounded-0 shadow-none my-0'
+                      disableGutters={true}
+                      square={true}
+                    >
+                      <AccordionSummary
+                        expandIcon={
+                          expanded === `panel_${i}` ? (
+                            <MinusIcon className='h-5 w-6 text-green-600' />
+                          ) : (
+                            <PlusIcon className='h-6 w-6 text-green-600' />
+                          )
+                        }
+                        aria-controls={`panel_${i}_bh-content`}
+                        id={`panel_${i}_bh-header`}
+                      >
+                        <Typography
+                          as='h3'
+                          className='width-1/3 mobile sm:desktop text-green-600 mr-4'
+                        >
+                          {item.question}
+                        </Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <Typography as='div' className='w-5/6'>
+                          <PortableText value={item.response} components={FAQPagePTComponents} />
+                        </Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
