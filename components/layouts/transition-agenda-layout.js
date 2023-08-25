03b5ab1f166f9076a076edf279ce0601c14a3dd4
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-08-25 11:40:35 +0200

    display pt components on ta page

--- components/layouts/transition-agenda-layout.js
+++ components/layouts/transition-agenda-layout.js
@@ -1,13 +1,15 @@
+import { PortableText } from '@portabletext/react'
 import SectionTypes from '../section-types-list'
-
+import { complexPortableTextComponents } from '../../lib/portable-text/pt-components'
 
 
 export default function TransitionAgendaLayout({themaCardData, transitionAgendaData}) {
+    console.log(transitionAgendaData.pageObjects)
     return (
         <>
         <div className="global-margin">
             <div className=" pt-20 pb-20">
-                <h1 className="mobile sm:desktop">{transitionAgendaData?.transitionAgendaTitle}</h1>
+                <h1 className="mobile sm:desktop pb-6">{transitionAgendaData?.transitionAgendaTitle}</h1>
                 <h2 className='mobile sm:desktop'>{transitionAgendaData?.subtitle}</h2>
             </div>
             <h2 className='mobile sm:desktop py-10'>
@@ -17,12 +19,13 @@ export default function TransitionAgendaLayout({themaCardData, transitionAgendaD
             <SectionTypes themaCards={themaCardData}/>
             </div>
             <div className='py-10'>
-                <h2 className='mobile sm:desktop py-10'>European Law</h2>               
-                <h3 className='mobile sm:desktop'>I think we can put a portable text here which will be able to upload pdfs and control some styling. </h3> 
-
-                <p className='mobile sm:desktop'>  
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                {transitionAgendaData?.pageObjects.map((object) => (
+                    <>
+                    {console.log(object.blockTitle)}
+                    <h2 className='mobile sm:desktop py-10'>{object?.blockTitle}</h2>
+                    <PortableText value={object?.complexPortableTextBloclk} components={complexPortableTextComponents}/>            
+                    </>
+                ))}
             </div>
         </div>
         </>
