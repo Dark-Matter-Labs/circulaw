4b5af76a0724e011572290e392e3165be7933174
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-08-25 10:30:29 +0200

    add transition agenda schema

---
+++ studio/schemas/documents/transition-agenda.js
@@ -0,0 +1,52 @@
+
+
+export default {
+    title: 'Transition Agenda',
+    name: 'transitionAgenda',
+    type: 'document',
+    fields: [
+        {
+            title: 'Transition Agenda',
+            name: 'transitionAgendaTitle',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+          },
+          {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug',
+            validation: (Rule) => Rule.required(),
+            options: {
+              source: 'transitionAgendaTitle',
+              inUnique: 'true',
+              slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+            },
+          },
+          {
+            title: 'Subtitle',
+            name: 'subtitle',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+          },
+          // can add in link/cover image/other things if need be
+          {
+            title: 'Page Objects',
+            name: 'pageObjects',
+            type: 'array',
+            of: [
+                {
+                    type: 'complexPortableText'
+                }
+            ]
+          },
+          {
+            title: 'Talks and Presentations',
+            name: 'talksAndPresentations',
+            type: 'array',
+            of: [
+                {type: 'complexPortableText'} // create talks and presentations block - or this could be just a page object with links. 
+            ]
+          }
+
+    ]
+}
