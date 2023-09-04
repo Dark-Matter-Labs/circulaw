98d6bf5c4b096a2c0e194692df7865c1056052c0
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-09-04 17:08:28 +0200

    remove query and use lenght

--- pages/bouw/houtbouw-stimuleren/instruments/index.js
+++ pages/bouw/houtbouw-stimuleren/instruments/index.js
@@ -2,19 +2,20 @@ import Layout from '/components/layouts/layout';
 import MeasuresLayout from '../../../../components/layouts/measures-layout';
 import IconWood from '../../../../public/icons/woodIcon.svg';
 import { client } from '../../../../lib/sanity';
-import { houtbouwQueries, getListPageData } from '../../../../lib/queries';
+import { getListPageData } from '../../../../lib/queries';
 
 const thema = 'houtbouw-stimuleren';
 
-export default function Measures({ numberOfInstruments, instruments }) {
+export default function Measures({ instruments }) {
+  console.log(instruments.length)
   return (
     <Layout title='CircuLaw - Houtbouw stimuleren'>
       <MeasuresLayout
-        totalNumberOfLaws={numberOfInstruments}
+        totalNumberOfLaws={instruments.length}
         title='Houtbouw stimuleren'
         thema={thema}
         heading='Instrumenten om de inzet van hout in de bouw te bevorderen'
-        introPara={`We hebben ${numberOfInstruments} kansrijke instrumenten gevonden die je kunt inzetten om de houtbouwtransitie te versnellen. Sommige van deze instrumenten zijn al eerder toegepast, andere nog niet. Ga aan de slag! Met jouw ervaringen help je ook anderen weer verder.`}
+        introPara={`We hebben ${instruments.length} kansrijke instrumenten gevonden die je kunt inzetten om de houtbouwtransitie te versnellen. Sommige van deze instrumenten zijn al eerder toegepast, andere nog niet. Ga aan de slag! Met jouw ervaringen help je ook anderen weer verder.`}
         icon={IconWood}
         searchTitle='Zoek in houtbouw stimuleren'
         instruments={instruments}
@@ -24,8 +25,6 @@ export default function Measures({ numberOfInstruments, instruments }) {
 }
 
 export async function getStaticProps() {
-  // can change this to len(instruments)
-  const numberOfInstruments = await client.fetch(houtbouwQueries.houtbouwLength);
   const instruments = await client.fetch(getListPageData(thema));
-  return { props: { numberOfInstruments, instruments }, revalidate: 1 };
+  return { props: { instruments }, revalidate: 1 };
 }
