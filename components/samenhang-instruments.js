cb7d99e70cfba6a1d7734f8ea05ed04c00c8ac35
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-09-04 15:55:38 +0200

    import component and replace old code

--- components/samenhang-instruments.js
+++ components/samenhang-instruments.js
@@ -3,7 +3,8 @@
 // convert that to a singe array that can be filtered.
 // this will break the logic of seperating under subcategory.?
 // this needs a refactor anyway as I feel like we can make a function rather than heaps of conditional rendereing.
-import Link from 'next/link';
+
+import SamenhangSingleInstrument from './samenhang-single-instrument';
 
 export default function SamenhagInstruments({
   selected,
@@ -16,9 +17,6 @@ export default function SamenhagInstruments({
   const varToString = (varObj) => Object.keys(varObj)[0];
   return (
     <div className='grid col-span-3'>
-      {/* Shorter way of rendering the instruments.  */}
-      {/* couuld make this a function that receives the prop. then call the function 5 times */}
-      {/* hesitant to reduce this further though as if we need to introduce filtering it will complicate matters */}
       {selected === varToString({ beleidsinstrumentenEnVergunningen }) && (
         <div>
           {beleidsinstrumentenEnVergunningen.map((arr, i) => (
@@ -29,30 +27,7 @@ export default function SamenhagInstruments({
                   {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
                   {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
                   {arr.map((law, index) => (
-                    <div
-                      key={index}
-                      className='w-full h-12 border flex justify-between items-center'
-                    >
-                      <Link href={`/bouw/houtbouw-stimuleren/instruments/${law.slug}`}>
-                      <h4 className='mobile sm:desktop link-interaction pl-2'>{law.titel}</h4></Link>
-                      <div className='flex items-center justify-center'>
-                        {law.overheidslaag.includes('Nationaal') && (
-                          <div className='rounded-full h-8 w-8 mx-1 bg-green-200 flex items-center justify-center'>
-                            N
-                          </div>
-                        )}
-                        {law.overheidslaag.includes('Provinciaal') && (
-                          <div className='rounded-full h-8 w-8 mx-1 bg-green-300 flex items-center justify-center'>
-                            P
-                          </div>
-                        )}
-                        {law.overheidslaag.includes('Gemeentelijk') && (
-                          <div className='rounded-full h-8 w-8 mx-1 bg-green-400 flex items-center justify-center'>
-                            G
-                          </div>
-                        )}
-                      </div>
-                    </div>
+                    <SamenhangSingleInstrument law={law} index={index} key={index} />
                   ))}
                 </div>
               )}
@@ -66,35 +41,12 @@ export default function SamenhagInstruments({
           {verkoop.map((arr, i) => (
             <div key={i}>
               {arr.length > 0 && (
-                  <div>
+                <div>
                   {i == 0 && <h2 className='mobile sm:desktop py-8'>Beleid</h2>}
                   {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
                   {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
                   {arr.map((law, index) => (
-                    <div
-                      key={index}
-                      className='w-full h-12 border flex justify-between items-center'
-                    >
-                      <Link href={`/bouw/houtbouw-stimuleren/instruments/${law.slug}`}>
-                      <h4 className='mobile sm:desktop link-interaction pl-2'>{law.titel}</h4></Link>
-                      <div className='flex items-center justify-center'>
-                        {law.overheidslaag.includes('Nationaal') && (
-                          <div className='rounded-full h-8 w-8 mx-1 bg-green-200 flex items-center justify-center'>
-                            N
-                          </div>
-                        )}
-                        {law.overheidslaag.includes('Provinciaal') && (
-                          <div className='rounded-full h-8 w-8 mx-1 bg-green-300 flex items-center justify-center'>
-                            P
-                          </div>
-                        )}
-                        {law.overheidslaag.includes('Gemeentelijk') && (
-                          <div className='rounded-full h-8 w-8 mx-1 bg-green-400 flex items-center justify-center'>
-                            G
-                          </div>
-                        )}
-                      </div>
-                    </div>
+                    <SamenhangSingleInstrument law={law} index={index} key={index} />
                   ))}
                 </div>
               )}
@@ -108,37 +60,14 @@ export default function SamenhagInstruments({
           {inkoop.map((arr, i) => (
             <div key={i}>
               {arr.length > 0 && (
-               <div>
-               {i == 0 && <h2 className='mobile sm:desktop py-8'>Beleid</h2>}
-               {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
-               {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
-               {arr.map((law, index) => (
-                 <div
-                   key={index}
-                   className='w-full h-12 border flex justify-between items-center'
-                 >
-                   <Link href={`/bouw/houtbouw-stimuleren/instruments/${law.slug}`}>
-                   <h4 className='mobile sm:desktop link-interaction pl-2'>{law.titel}</h4></Link>
-                   <div className='flex items-center justify-center'>
-                     {law.overheidslaag.includes('Nationaal') && (
-                       <div className='rounded-full h-8 w-8 mx-1 bg-green-200 flex items-center justify-center'>
-                         N
-                       </div>
-                     )}
-                     {law.overheidslaag.includes('Provinciaal') && (
-                       <div className='rounded-full h-8 w-8 mx-1 bg-green-300 flex items-center justify-center'>
-                         P
-                       </div>
-                     )}
-                     {law.overheidslaag.includes('Gemeentelijk') && (
-                       <div className='rounded-full h-8 w-8 mx-1 bg-green-400 flex items-center justify-center'>
-                         G
-                       </div>
-                     )}
-                   </div>
-                 </div>
-               ))}
-             </div>
+                <div>
+                  {i == 0 && <h2 className='mobile sm:desktop py-8'>Beleid</h2>}
+                  {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
+                  {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
+                  {arr.map((law, index) => (
+                    <SamenhangSingleInstrument law={law} index={index} key={index} />
+                  ))}
+                </div>
               )}
             </div>
           ))}
@@ -151,36 +80,13 @@ export default function SamenhagInstruments({
             <div key={i}>
               {arr.length > 0 && (
                 <div>
-                {i == 0 && <h2 className='mobile sm:desktop py-8'>Beleid</h2>}
-                {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
-                {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
-                {arr.map((law, index) => (
-                  <div
-                    key={index}
-                    className='w-full h-12 border flex justify-between items-center'
-                  >
-                    <Link href={`/bouw/houtbouw-stimuleren/instruments/${law.slug}`}>
-                    <h4 className='mobile sm:desktop link-interaction pl-2'>{law.titel}</h4></Link>
-                    <div className='flex items-center justify-center'>
-                      {law.overheidslaag.includes('Nationaal') && (
-                        <div className='rounded-full h-8 w-8 mx-1 bg-green-200 flex items-center justify-center'>
-                          N
-                        </div>
-                      )}
-                      {law.overheidslaag.includes('Provinciaal') && (
-                        <div className='rounded-full h-8 w-8 mx-1 bg-green-300 flex items-center justify-center'>
-                          P
-                        </div>
-                      )}
-                      {law.overheidslaag.includes('Gemeentelijk') && (
-                        <div className='rounded-full h-8 w-8 mx-1 bg-green-400 flex items-center justify-center'>
-                          G
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                ))}
-              </div>
+                  {i == 0 && <h2 className='mobile sm:desktop py-8'>Beleid</h2>}
+                  {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
+                  {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
+                  {arr.map((law, index) => (
+                    <SamenhangSingleInstrument law={law} index={index} key={index} />
+                  ))}
+                </div>
               )}
             </div>
           ))}
@@ -192,37 +98,14 @@ export default function SamenhagInstruments({
           {fiscaal.map((arr, i) => (
             <div key={i}>
               {arr.length > 0 && (
-               <div>
-               {i == 0 && <h2 className='mobile sm:desktop py-8'>Beleid</h2>}
-               {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
-               {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
-               {arr.map((law, index) => (
-                 <div
-                   key={index}
-                   className='w-full h-12 border flex justify-between items-center'
-                 >
-                   <Link href={`/bouw/houtbouw-stimuleren/instruments/${law.slug}`}>
-                   <h4 className='mobile sm:desktop link-interaction pl-2'>{law.titel}</h4></Link>
-                   <div className='flex items-center justify-center'>
-                     {law.overheidslaag.includes('Nationaal') && (
-                       <div className='rounded-full h-8 w-8 mx-1 bg-green-200 flex items-center justify-center'>
-                         N
-                       </div>
-                     )}
-                     {law.overheidslaag.includes('Provinciaal') && (
-                       <div className='rounded-full h-8 w-8 mx-1 bg-green-300 flex items-center justify-center'>
-                         P
-                       </div>
-                     )}
-                     {law.overheidslaag.includes('Gemeentelijk') && (
-                       <div className='rounded-full h-8 w-8 mx-1 bg-green-400 flex items-center justify-center'>
-                         G
-                       </div>
-                     )}
-                   </div>
-                 </div>
-               ))}
-             </div>
+                <div>
+                  {i == 0 && <h2 className='mobile sm:desktop py-8'>Beleid</h2>}
+                  {i == 1 && <h2 className='mobile sm:desktop py-8'>Strategie</h2>}
+                  {i == 2 && <h2 className='mobile sm:desktop py-8'>Contracten</h2>}
+                  {arr.map((law, index) => (
+                    <SamenhangSingleInstrument law={law} index={index} key={index} />
+                  ))}
+                </div>
               )}
             </div>
           ))}
