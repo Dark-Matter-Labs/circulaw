058ca2df6c7ca5e8d870e12ae4b87c7d562e90b6
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-09-28 11:27:13 +0200

    make mobile version of feedback block

--- components/instrument/instrument-feedback-block.js
+++ components/instrument/instrument-feedback-block.js
@@ -6,26 +6,26 @@ import CustomButton from '../custom-button';
 
 export default function InstrumentFeedbackBlock({ data }) {
   return (
-    <Popover className='fixed inset-y-1/3 right-0 z-10 h-72 w-8 sm:h-96 sm:w-16'>
+    <Popover className='absolute inset-y-1/3 right-0 h-72 w-8 sm:h-96 sm:w-16'>
       {({ open }) => (
         <>
           <Popover.Button
             className={`${
-              open ? '-translate-x-56' : ''
+              open ? '-translate-x-52 sm:-translate-x-56' : ''
             } bg-green-500 rounded-l-clSm h-full w-full flex flex-col items-center justify-between text-grey-100`}
           >
             <ArrowLeftIcon
-              className={`${open ? 'rotate-180 transform' : ''} h-4 sm:h-6 w-4 sm:w-6 text-grey-100 mt-3 sm:mt-10`}
+              className={`${open ? 'rotate-180 transform' : ''} h-4 w-4 sm:h-6 sm:w-6 text-grey-100 mt-3 sm:mt-10`}
             />
             <div className='[writing-mode:vertical-lr] rotate-180 pt-3 sm:pt-10 p-base sm:p-lg'>
               Help ons circulaw te verbeteren
             </div>
           </Popover.Button>
           <Popover.Panel className=''>
-            <div className='w-56 h-96 bg-grey-200 -translate-y-96 -translate-x-40 flex flex-col items-center'>
-              <div className='px-8 py-6'>
+            <div className='w-52 h-72 sm:w-56 sm:h-96 bg-grey-200 -translate-y-72 sm:-translate-y-96 -translate-x-44 sm:-translate-x-40 flex flex-col items-center'>
+              <div className='px-6 py-4 sm:px-8 sm:py-6'>
                 <h4 className='mobile sm:desktop'>Deel met ons:</h4>
-                <ul className='pb-10 list-disc	'>
+                <ul className='pb-2 sm:pb-10 sm:pt-4 list-disc p-sm sm:p-base'>
                   <li>voorbeelden uit jouw praktijk</li>
                   <li>je tips om toepassing makkelijker te maken</li>
                   <li>de ervaring van jou of andere organisaties met een soortgelijk instrument</li>
@@ -35,7 +35,7 @@ export default function InstrumentFeedbackBlock({ data }) {
                 className=''
                 href={{ pathname: '/feedback', query: { instrument: data?.measure?.titel } }}
               >
-                <CustomButton color='greenBackground'>Ik deel mijn kennis </CustomButton>
+                <CustomButton color='greenBackground'><span className='p-sm font-bold sm:p-base'>Ik deel mijn kennis</span></CustomButton>
               </Link>
             </div>
           </Popover.Panel>
