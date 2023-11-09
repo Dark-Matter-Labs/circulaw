30c0ff8b7ecfa985391ab961a428befe089bae14
Gurden Batra <gurden.batra@gmail.com>
2023-11-09 17:43:33 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 // next.config.js
 // const { redirects } = require('./utils/redirects')
+const withTM = require('next-transpile-modules')(['@piwikpro/next-piwik-pro']);
 
-
-module.exports = {
+const nextConfig = {
   // redirects,
-    images: {
-      domains: ['cdn.sanity.io'],
-    },
-  }
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
+}
+
+module.exports = withTM(nextConfig);
