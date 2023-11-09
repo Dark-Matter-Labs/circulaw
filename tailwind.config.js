da43b5c5bacb7f888061ffacc1b983165101112d
Gurden Batra <gurden.batra@gmail.com>
2023-11-09 14:50:25 +0100

    remove utopia

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,37 +12,6 @@ module.exports = {
   important: true,
   theme: {
     extend: {
-      utopia: {
-        minWidth: 320,
-        minSize: 25,
-        minScale: 1,
-        maxWidth: 1140,
-        maxSize: 26,
-        maxScale: 1.25,
-        fontSize: {
-          xs: 'inherit',
-          sm: 'inherit',
-          base: 1.4,
-          lg: 1.33,
-          xl: 1.2,
-          '2xl': 1.11,
-          '3xl': 1,
-          '4xl': {
-            lineHeight: 1.1,
-          }
-        },
-        spacing: {
-          '3xs': 0.25,
-          '2xs': 0.5,
-          xs: 0.75,
-          sm: 1,
-          md: 1.5,
-          lg: 2,
-          xl: 3,
-          '2xl': 4,
-          '3xl': 6
-        }
-    },
       colors: {
         'grey-100': '#FDFDFD',
         'grey-200': '#F0F0F0',
@@ -124,7 +93,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@domchristie/tailwind-utopia'),
     require('@tailwindcss/forms')
 ],
   
