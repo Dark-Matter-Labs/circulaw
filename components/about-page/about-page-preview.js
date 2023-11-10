fea0498e82f60227ef9c5eda1e9b3ec7e5042955
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-11-10 15:57:26 +0100

    update about page preview

--- components/about-page/about-page-preview.js
+++ components/about-page/about-page-preview.js
@@ -9,42 +9,44 @@ export default function AboutPagepreview({ query, queryParams }) {
   const data = { aboutPage: usePreview(null, query, queryParams) };
   return (
     <div>
-      <div className='bg-about-header bg-cover bg-center h-40'></div>
-      <div className='pb-8 text-grey-800 global-margin-alternative'>
-        <div className='block lg:hidden w-full'>
-          <AboutPageDropdown
+    <div className='bg-about-header bg-cover bg-center h-40'></div>
+    <div className='pb-8 text-grey-800 global-margin-alternative'>
+      <div className='block lg:hidden w-full'>
+        <AboutPageDropdown
+          currentSlug={data?.aboutPage?.slug.current}
+          slugs={data?.aboutPageSlugs}
+        />
+      </div>
+      <div className='grid grid-cols-1 w-full lg:grid-cols-3 max-w-8xl'>
+        <div className='lg:col-span-2'>
+          <div className='breadcrumb pt-8 text-green-500 '>
+            <Link href='/' className='link-interaction'>
+              Home &gt;
+            </Link>
+          </div>
+          <div className='max-w-3xl'>
+            <h1 className='mobile sm:desktop lg:block sm:pt-10 py-6 sm:pb-10 text-grey-800'>
+              {data?.aboutPage?.pageTitle}
+            </h1>
+            <PortableText
+              value={data?.aboutPage?.aboutPageContent}
+              components={aboutPagePTComponents}
+            />
+          </div>
+        </div>
+        <div className='hidden lg:block mt-3 lg:ml-12 lg:mb-20 lg:mt-32 col-span-1'>
+          <AboutPageNav
             currentSlug={data?.aboutPage?.slug.current}
-            slugs={data?.aboutPage?.aboutPageSlugs}
+            slugs={data?.aboutPageSlugs}
           />
         </div>
-        <div className='grid grid-cols-1 w-full lg:grid-cols-3'>
-          <div className='col-span-2'>
-            <div className='breadcrumb pt-8 text-green-500 link-interaction'>
-              <Link href='/'>Home &gt;</Link>
-            </div>
-            <div className='max-w-3xl'>
-              <h1 className='mobile sm:desktop lg:block sm:pt-10 py-6 sm:pb-10 text-green-500'>
-                {data?.aboutPage?.pageTitle}
-              </h1>
-              <PortableText
-                value={data?.aboutPage?.aboutPageContent}
-                components={aboutPagePTComponents}
-              />
-            </div>
-          </div>
-          <div className='hidden lg:block mt-3 max-w-3xl lg:ml-12 lg:mb-20 lg:mt-32 col-span-1'>
-            <AboutPageNav
-              currentSlug={data?.aboutPage?.slug.current}
-              slugs={data?.aboutPage?.aboutPageSlugs}
-            />
-          </div>
-          <div className='block lg:hidden'>
-            <AboutPageDropdown
-              currentSlug={data?.aboutPage?.slug.current}
-              slugs={data?.aboutPage?.aboutPageSlugs}
-            />
-          </div>
+        <div className='block lg:hidden'>
+          <AboutPageDropdown
+            currentSlug={data?.aboutPage?.slug.current}
+            slugs={data?.aboutPageSlugs}
+          />
         </div>
+      </div>
         <Link
           className='bg-blue-500 p-6 text-white font-bold fixed bottom-0 right-0'
           href='/api/exit-preview'
