2ccd6f9e28c2cc8984f09f0d820582fe21e2d4f8
Gurden Batra <gurden.batra@gmail.com>
2023-11-22 16:43:16 +0100

    fix import typo

--- pages/bouw/houtbouw-stimuleren/categorie.js
+++ pages/bouw/houtbouw-stimuleren/categorie.js
@@ -1,7 +1,7 @@
 import Layout from '@/components/layouts/layout';
 import ExpertiseLayout from '@/components/layouts/expertise-layout';
 import { client } from '@/lib/sanity';
-import { categorieQuery } from '@lib/queries';
+import { categorieQuery } from '@/lib/queries';
 
 export default function InfoPage({ expertiseData }) {
   return (
