159348af30620c3d8c72a98986037321a1d44f27
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-11-30 16:08:27 +0100

    Update instrument-preview.js

--- components/instrument/instrument-preview.js
+++ components/instrument/instrument-preview.js
@@ -7,7 +7,7 @@ import InstrumentTable from './instrument-table';
 import { instrumentPTComponents } from '@/lib/portable-text/pt-components';
 
 import InstrumentFeedbackBlock from './instrument-feedback-block';
-import Instrumentheader from './instrument-header';
+import InstrumentHeader from './instrument-header';
 import MobileFeedback from './instrument-feedback-block-mobile';
 
 export default function InstrumentPreview({ query, queryParams }) {
@@ -38,7 +38,7 @@ export default function InstrumentPreview({ query, queryParams }) {
 
   return (
     <div ref={ref} className='relative bg-grey-100'>
-      <Instrumentheader data={data} />
+      <InstrumentHeader data={data} />
       <div className='bg-grey-100 relative z-0'>
         <InstrumentFeedbackBlock data={data} />
         <div className='global-margin sm:mt-4 z-0'>
