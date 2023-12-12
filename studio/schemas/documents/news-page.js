508274294ed0db2878fa6504ca104b2a3f6b5fb5
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-12-12 12:28:58 +0100

    create news page schema

---
+++ studio/schemas/documents/news-page.js
@@ -0,0 +1,32 @@
+import {  BsNewspaper } from 'react-icons/bs';
+import { NewsItemComponent } from '../../components/news-item';
+
+export default {
+    name: 'newsPage',
+    title: 'News',
+    type: 'document',
+    fields: [
+      {
+        name: 'newsItems',
+        type: 'array',
+        title: 'News Items',
+        description:
+          'these are the remaining news items. News items should not be here if they are in the Featured news section',
+        of: [{ type: 'newsCard' }, {type: 'agendaItem'}],
+      },
+    ],
+    preview: {
+      select: {
+        title: 'newsCard.newsTitle',
+        date: 'newsCard.newsDate',
+        featured: 'newsCard.featured'
+      },
+      prepare: ({title, date, featured}) => ({
+        title: [featured ? '⭐️ ' : '', `${title ?? `No book selected`}`].join(` `),
+        date: `${date}`,
+        media: BsNewspaper,
+      })
+    }
+  };
+  
+// https://www.sanity.io/guides/create-interactive-array-items-for-featured-elements
