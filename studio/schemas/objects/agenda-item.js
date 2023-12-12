72b4e3269e3003ee9d1cdc8b1c3c57f690b5aec2
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-12-12 12:28:37 +0100

    create agenda item schema

---
+++ studio/schemas/objects/agenda-item.js
@@ -0,0 +1,21 @@
+export default {
+    name: 'agendaItem',
+    title: 'Agenda Item',
+    type: 'object',
+    fields: [
+      {
+        name: 'agendaTitle',
+        type: 'string',
+        title: 'Agenda Title',
+        validation: (Rule) => Rule.required()
+      },
+      {
+        name: 'newsDate',
+        type: 'date',
+        title: 'News Date',
+        description: 'this does not effect the order in which news items are displayed',
+      },
+      // add link option - must be a link
+    ],
+  };
+  
