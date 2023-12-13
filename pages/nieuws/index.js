496302ce71949155bfe5c482a60f50d635a3b84d
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-12-13 16:52:42 +0100

    finalise fetch query

--- pages/nieuws/index.js
+++ pages/nieuws/index.js
@@ -1,7 +1,7 @@
 import Layout from '@/components/layouts/layout';
 import Link from 'next/link';
 import { client } from '@/lib/sanity';
-
+import AgendaCard from '@/components/news-page/agenda-card';
 
 const newsItems = `
 *[_type == 'newsPage' || _type == 'agendaItem'][0] {
@@ -12,20 +12,11 @@ const newsItems = `
     },
    
   }
-`
+`;
 
-
-
-
-
-
-
-
-export default function NewsPage({data}) {
-    
-
-    console.log(data.featured, 'featured'  )
-    console.log(data.notFeatured, 'not featured'  )
+export default function NewsPage({ data }) {
+  console.log(data.featured, 'featured');
+  console.log(data.notFeatured, 'not featured');
   return (
     <Layout>
       <div className='h-screen flex flex-col global-margin mt-4'>
@@ -35,27 +26,21 @@ export default function NewsPage({data}) {
           </Link>
         </div>
 
-        <div className='h-96'>
+        <div className=''>
           <h1 className='p-2xl-semibold sm:p-5xl-semibold w-full border-b-2 pb-5 border-green-800'>
             Uitgelichte nieuwsberichten
           </h1>
-          <div className='grid grid-cols-4 grid-rows-1'>
-            {data.featured.map((item, id) => (
-                <div key={id}>
-                    {JSON.stringify(item)}
-                </div>
-            ))}
+          <div className='grid grid-cols-4 grid-rows-1 py-10'>
+            <AgendaCard />
           </div>
         </div>
         <div className='h-96'>
           <h1 className='p-2xl-semibold sm:p-5xl-semibold w-full border-b-2 pb-5 border-green-800'>
             Laatste nieuws{' '}
           </h1>
-            <div className='grid grid-cols-4 grid-rows-1'>
+          <div className='grid grid-cols-4 grid-rows-1'>
             {data.featured.map((item, id) => (
-                <div key={id}>
-                    {JSON.stringify(item)}
-                </div>
+              <div key={id}>{JSON.stringify(item)}</div>
             ))}
           </div>
         </div>
@@ -69,14 +54,12 @@ export default function NewsPage({data}) {
   );
 }
 
-
-
 export async function getStaticProps() {
-    const data = await client.fetch(newsItems);
-    return {
-      props: {
-        data
-      },
-      revalidate: 1,
-    };
-  }
+  const data = await client.fetch(newsItems);
+  return {
+    props: {
+      data,
+    },
+    revalidate: 1,
+  };
+}
