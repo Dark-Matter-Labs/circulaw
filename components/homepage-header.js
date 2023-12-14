103f7fac7efebf32d636d138c3dfa15e1372dd9e
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-12-14 13:59:01 +0100

    fix header font size and width

--- components/homepage-header.js
+++ components/homepage-header.js
@@ -7,9 +7,9 @@ export default function HomepageHeader({ homePageHeader }) {
     <main className='pb-16 pt-56'>
       <div className='global-margin'>
         <div className='text-center max-w-4xl mx-auto'>
-          <div className='text-left sm:text-center max-w-xs m-auto sm:max-w-3xl sm:block text-grey-200'>
-            <h1 className='block mobile sm:desktop'>{homePageHeader.headerText}</h1>{' '}
-            <h2 className='p-lg-semibold sm:p-4xl-semibold text-left m-auto sm:text-center mt-2 max-w-sm sm:max-w-full text-grey-200 pt-10 pb-8'>
+          <div className='text-left sm:text-center max-w-xs m-auto sm:max-w-4xl sm:block text-grey-200'>
+            <h1 className='block header-desktop-mobile sm:header-desktop'>{homePageHeader.headerText}</h1>{' '}
+            <h2 className='p-base sm:p-3xl-semibold text-left m-auto sm:text-center mt-2 max-w-sm sm:max-w-full text-grey-200 pt-10 pb-8'>
               {homePageHeader.subHeader}
             </h2>
           </div>
