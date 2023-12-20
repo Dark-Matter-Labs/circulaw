4d9bd293cdcd3da28a97c20cf2a0698af011d5e1
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-12-20 10:06:34 +0100

    add meubels to code

---
+++ pages/consumptie-goederen/meubels/index.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import Layout from '@/components/layouts/layout';
+import SimpleThemaLayout from '@/components/layouts/simple-thema-layout';
+import { client } from '@/lib/sanity';
+import { meubelsQueries } from '@/lib/queries';
+
+export default function Meubels({ thema, length, instruments }) {
+  useEffect(() => {
+    localStorage.clear();
+  });
+
+  return (
+    <Layout title='CircuLaw - Meubels'>
+      <SimpleThemaLayout thema={thema} numberOfLaws={length} instruments={instruments} />
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const length = await client.fetch(meubelsQueries.meubelsLength);
+  const thema = await client.fetch(meubelsQueries.meubelsThemaQuery);
+  const instruments = await client.fetch(meubelsQueries.meubelsInstrumentsQuery);
+  return {
+    props: {
+      thema,
+      length,
+      instruments,
+    },
+    revalidate: 1,
+  };
+}
