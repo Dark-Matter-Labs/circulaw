b167d60703609551d09d166a590d989b4fa98a18
theocampbell <57679708+theocampbell@users.noreply.github.com>
2023-12-21 09:39:43 +0100

    add right margin in case of two tags

--- components/policy-list.js
+++ components/policy-list.js
@@ -7,7 +7,6 @@ export default function PolicyList(props) {
   if (props.data) {
     lawData = props.data;
   }
-
   return (
     <>
       {lawData.map((law) => (
@@ -15,15 +14,16 @@ export default function PolicyList(props) {
           href={`/${law.transitionAgenda}/${law.thema}/instrumenten/${law.slug.current}`}
           key={law.titel}
         >
+          {console.log(law.expertise)}
           <div className='block sm:ml-0 mb-10 sm:mb-8 md:max-w-[760px]'>
             <div className='flex justify-start items-center -ml-1'>
               {/* Expertise Tag */}
-              {law?.beleid === true && <Tag classes='bg-green-500 text-gray-100'>Beleid</Tag>}
-              {law?.inkoop === true && <Tag classes='bg-green-500 text-gray-100'>Inkoop</Tag>}
+              {law?.beleid === true && <Tag classes='bg-green-500 text-gray-100 mr-2'>Beleid</Tag>}
+              {law?.inkoop === true && <Tag classes='bg-green-500 text-gray-100 mr-2'>Inkoop</Tag>}
               {law?.grondpositie === true && (
-                <Tag classes='bg-green-500 text-gray-100'>Grondpositie</Tag>
+                <Tag classes='bg-green-500 text-gray-100 mr-2'>Grondpositie</Tag>
               )}
-              {law?.subsidie === true && <Tag classes='bg-green-500 text-gray-100'>Subsidie</Tag>}
+              {law?.subsidie === true && <Tag classes='bg-green-500 text-gray-100 mr-2'>Subsidie</Tag>}
               {law?.fiscaal === true && <Tag classes='bg-green-500 text-gray-100'>Fiscaal</Tag>}
             </div>
 
