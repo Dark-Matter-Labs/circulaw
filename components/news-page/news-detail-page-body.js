360b47cb20ba2880a3af777eaaafa6e75a20b419
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-01-17 09:11:14 +0100

    fix layout on mobile detail

--- components/news-page/news-detail-page-body.js
+++ components/news-page/news-detail-page-body.js
@@ -5,7 +5,7 @@ export default function NewsDetailPageBody({ data }) {
   return (
     <>
       <div className='grid grid-cols-12 items-start justify-start global-margin relative mb-20 mt-10'>
-        <div className='col-span-6'>
+        <div className='col-span-12 sm:col-span-6'>
           <PortableText value={data.content} components={aboutPagePTComponents} />
         </div>
       </div>
