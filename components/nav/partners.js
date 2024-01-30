5394438757cc74d7bebcd6219bc02bd1bcd54015
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-01-30 13:26:39 +0100

    build the different partner sections.

--- components/nav/partners.js
+++ components/nav/partners.js
@@ -3,70 +3,27 @@ import Image from 'next/image';
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
 import { groq } from 'next-sanity';
-import { urlFor } from '@/lib/sanity';
 import { partnersQuery } from '@/lib/queries';
 import sanityLogo from '@/public/logo_partners/sanity-logo.svg';
 import { fetcher } from '@/utils/swr-fetcher';
-import { Fragment } from 'react';
+import ParnerSection from './parner-section';
 
-const bordersDesktop =
-  'lg:[&:nth-child(6)]:border-r-0 lg:[&:nth-child(14)]:border-r-0 lg:[&:nth-child(21)]:border-r-0 lg:[&:nth-child(29)]:border-r-0 lg:[&:nth-child(3)]:border-r lg:[&:nth-child(8)]:border-r lg:[&:nth-child(12)]:border-r lg:[&:nth-child(17)]:border-r lg:[&:nth-child(26)]:border-r';
-const bordersSmall =
-  '[&:nth-child(3)]:border-r-0 [&:nth-child(8)]:border-r-0 [&:nth-child(12)]:border-r-0 [&:nth-child(17)]:border-r-0 [&:nth-child(21)]:border-r-0 [&:nth-child(26)]:border-r-0';
-// fix query to receive the array and not an object
+
 export default function Partners({ footerText }) {
   const { data } = useSWR(groq`${partnersQuery}`, fetcher);
-  const partners = data?.partners;
-
   const router = useRouter();
+
   return (
     <div className='bg-green-800 pt-10 pb-2'>
+      {router.pathname === '/' &&  
       <div className='global-margin pb-12 px-4 lg:pb-16'>
-        
-        <div className='grid grid-cols-3 gap-x-0.5 gap-y-4 lg:grid-cols-5 '>
-          {partners?.map((partner, index) => {
-            return (
-              <Fragment key={partner.partnerName}>
-                <div
-                  key={partner.partnerName}
-                  className={`col-span-1 flex justify-center py-4 px-4 lg:py-8 lg:px-8 border-r border-grey-100  ${bordersDesktop} ${bordersSmall}`}
-                >
-                  <Link href={partner.partnerLink} target='_blank' rel='noopener noreferrer'>
-                    <Image
-                      src={urlFor(partner.logo).url()}
-                      alt={partner.partnerName}
-                      width={190}
-                      height={190}
-                    />
-                  </Link>
-                </div>
-
-                {index === 2 && <hr className='border-grey-100 col-span-3 block lg:hidden' />}
-
-                {index === 4 && <hr className='border-grey-100 col-span-5 hidden lg:block' />}
-
-                {index === 5 && <hr className='border-grey-100 col-span-3 block lg:hidden' />}
-
-                {index === 8 && <hr className='border-grey-100 col-span-3 block lg:hidden' />}
-
-                {index === 9 && <hr className='border-grey-100 col-span-5 hidden lg:block' />}
-
-                {index === 11 && <hr className='border-grey-100 col-span-3 block lg:hidden' />}
-
-                {partners.length > 15 && index === 14 && (
-                  <hr className='border-grey-100 col-span-3 block lg:hidden' />
-                )}
-                {partners.length > 15 && index === 14 && (
-                  <hr className='border-grey-100 col-span-5 hidden lg:block' />
-                )}
-                {partners.length > 15 && index === 17 && (
-                  <hr className='border-grey-100 col-span-3 block lg:hidden' />
-                )}
-              </Fragment>
-            );
-          })}
+        <div className='p-6xl-semibold text-gray-100 pl-3 sm:pl-6 lg:pl-8'>
+          Partners
         </div>
-      </div>
+        <ParnerSection partners={data?.developingPartners} partnerType='Ontwikkelpartners'/>
+        <ParnerSection partners={data?.knowledge} partnerType='Kennispartners'/>
+        <ParnerSection partners={data?.financingPartners} partnerType='Financieringspartners'/>
+      </div>}
       <div className='global-margin pb-2 text-grey-100 p-md'>
         {router.pathname === '/en' && (
           <p className='pb-8'>
