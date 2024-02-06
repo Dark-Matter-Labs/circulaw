ecd2b9bf79f80debe3e622b35f21b040f33b8d7b
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-02-06 14:19:07 +0100

    add new icons

--- studio/desk-structure.js
+++ studio/desk-structure.js
@@ -1,5 +1,5 @@
 import { VscLaw } from 'react-icons/vsc';
-import { GiDeadWood, GiBed, GiWindmill, GiBanana, GiTable } from 'react-icons/gi';
+import { GiDeadWood, GiBed, GiWindmill, GiBanana, GiTable, GiGrass, GiBeanstalk } from 'react-icons/gi';
 import { GrNavigate } from 'react-icons/gr';
 import { FcAbout } from 'react-icons/fc';
 import { FaLanguage, FaHandshake, FaQuestion } from 'react-icons/fa';
@@ -73,19 +73,19 @@ export const Structure = (S) =>
                     .filter('_type == "measure" && thema == "woningen"'),
                 ),
                 S.listItem()
-                .title('Woningen')
-                .icon(BsHouseDoor)
+                .title('Kunstgrasvelden')
+                .icon(GiGrass)
                 .child(
                   S.documentList()
-                    .title('Woningen')
+                    .title('Kunstgrasvelden')
                     .filter('_type == "measure" && thema == "kunstgrasvelden"'),
                 ),
                 S.listItem()
-                .title('Woningen')
-                .icon(BsHouseDoor)
+                .title('Eiwittransitie')
+                .icon(GiBeanstalk)
                 .child(
                   S.documentList()
-                    .title('Woningen')
+                    .title('Eiwittransitie')
                     .filter('_type == "measure" && thema == "eiwittransitie"'),
                 ),
             ]),
