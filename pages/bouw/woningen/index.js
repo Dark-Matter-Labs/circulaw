601810675c35ff5b6c304f520d1d58471b22609e
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-02-07 12:54:19 +0100

    update other simple themas to use new query function

--- pages/bouw/woningen/index.js
+++ pages/bouw/woningen/index.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import Layout from '@/components/layouts/layout';
 import SimpleThemaLayout from '@/components/layouts/simple-thema-layout';
 import { client } from '@/lib/sanity';
-import { woningenQueries } from '@/lib/queries';
+import { simpleThemaQueryFunction } from '@/lib/queries';
 
 export default function Meubels({ thema, length, instruments }) {
   useEffect(() => {
@@ -22,9 +22,9 @@ export default function Meubels({ thema, length, instruments }) {
 }
 
 export async function getStaticProps() {
-  const length = await client.fetch(woningenQueries.woningenLength);
-  const thema = await client.fetch(woningenQueries.woningenThemaQuery);
-  const instruments = await client.fetch(woningenQueries.woningenInstrumentsQuery);
+  const length = await client.fetch(simpleThemaQueryFunction('woningen' ,'Woningen').length);
+  const thema = await client.fetch(simpleThemaQueryFunction('woningen' ,'Woningen').themaQuery);
+  const instruments = await client.fetch(simpleThemaQueryFunction('woningen' ,'Woningen').instrumentsQuery);
   return {
     props: {
       thema,
