338ad1c4f531e1a114107380ba884e772d49361d
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-02-07 12:24:27 +0100

    add folder structure for new themes

---
+++ pages/biomassa-en-voedsel/eiwittransitie/index.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import Layout from '@/components/layouts/layout';
+import SimpleThemaLayout from '@/components/layouts/simple-thema-layout';
+import { client } from '@/lib/sanity';
+import { meubelsQueries } from '@/lib/queries';
+
+export default function Eiwittransitie({ thema, length, instruments }) {
+  useEffect(() => {
+    localStorage.clear();
+  });
+
+  return (
+    <Layout title='CircuLaw - Eiwittransitie'>
+      <SimpleThemaLayout
+        thema={thema}
+        numberOfLaws={length}
+        instruments={instruments}
+        subheading='instrumenten om circulariteit van eiwittransitie te bevorderen'
+      />
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const length = await client.fetch(meubelsQueries.meubelsLength);
+  const thema = await client.fetch(meubelsQueries.meubelsThemaQuery);
+  const instruments = await client.fetch(meubelsQueries.meubelsInstrumentsQuery);
+  return {
+    props: {
+      thema,
+      length,
+      instruments,
+    },
+    revalidate: 1,
+  };
+}
