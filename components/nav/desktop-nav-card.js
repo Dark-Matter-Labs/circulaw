8d3b0ab8320739cffabf609e1ba449025dedc37e
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-02-08 16:27:41 +0100

    add prod chain page links to nav

--- components/nav/desktop-nav-card.js
+++ components/nav/desktop-nav-card.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-export default function DesktopNavCard({ transitionAgenda, themas }) {
+export default function DesktopNavCard({ transitionAgenda, themas , slug}) {
   const router = useRouter();
   return (
     <div
@@ -11,9 +11,11 @@ export default function DesktopNavCard({ transitionAgenda, themas }) {
         transitionAgenda === 'bouw'
           ? 'pl-4 lgNav:pl-10 xl:pl-20 3xl:pl-32 pt-8 pr-2'
           : 'pl-3 lg:pl-6 pt-8 pr-2'
-      }  p-lg-semibold first-letter:uppercase`}
+      }  p-lg-semibold first-letter:uppercase `}
     >
-      {transitionAgenda}
+      <Link href={`/${slug}`} className='hover:underline'>
+      {transitionAgenda} {'>'}
+      </Link>
       {themas.map((thema) => (
         <div className='mt-2' key={thema.name}>
           <Link
--- components/nav/mobile-disclosure.js
+++ components/nav/mobile-disclosure.js
@@ -2,11 +2,11 @@ import { Disclosure, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
-export default function MobileDisclosure({ transitionAgenda, themas }) {
+export default function MobileDisclosure({ transitionAgenda, themas, slug }) {
   return (
     <li className='p-base-semibold text-green-800 border-b py-6'>
       <Disclosure>
-        {({ open }) => (
+        {({ open, close }) => (
           <>
             <Disclosure.Button className={`${open ? '' : ''} flex flex-row items-center `}>
               {transitionAgenda}
@@ -24,6 +24,11 @@ export default function MobileDisclosure({ transitionAgenda, themas }) {
             >
               <Disclosure.Panel className='p-base text-green-600'>
                 <ul>
+                  <Link href={`/${slug}`} onClick={close}>
+                  <li className='pt-4 w-full flex items-center ml-4'>
+                    {transitionAgenda} - overzicht
+                  </li>
+                  </Link>
                   {themas.map((thema) => (
                     <li key={thema.name} className=' pt-4 w-full flex items-center ml-4'>
                       <Link href={thema.url} className='h-10 flex items-center'>
