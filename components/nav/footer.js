19c443f0a7a1a8a0a9c27f89633929c79b0f2ae6
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-03-04 10:08:40 +0100

    fix css error

--- components/nav/footer.js
+++ components/nav/footer.js
@@ -159,7 +159,7 @@ export default function Footer(props) {
                           buttonText='Neem contact op'
                           buttonLink='/contact'
                         />
-                        <div className='hidden sm:block py-6 p-md text-grey-100 flex items-center justify-center z-0 relative'>
+                        <div className='flex sm:block py-6 p-md text-grey-100 items-center justify-center z-0 relative'>
                           <h3 className='inline-block mobile sm:desktop'>Volg ons op:</h3>
                           <span data-text='Volg ons op LinkedIn' className='tooltip p-md z-40'>
                             <Link href='https://www.linkedin.com/company/circulaw/' target='_blank'>
