599db5afac03c13ed969a12b9daf3559e68fb80a
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-03-06 16:07:03 +0100

    add correct options to ce block content

--- studio/schemas/documents/eu-law.js
+++ studio/schemas/documents/eu-law.js
@@ -224,7 +224,57 @@ export default {
       type: 'array',
       title: 'Circular Economy Content',
       group: 'ce',
-      of: [{ type: 'block' }],
+      of: [
+        {
+          type: 'block',
+          of: [
+            {
+              type: 'dropDown',
+            },
+          ],
+          lists: [
+            { title: 'Bullet', value: 'bullet' },
+            { title: 'Number', value: 'number' },
+          ],
+          styles: [
+            { title: 'H3', value: 'h3' },
+            { title: 'normal', value: 'normal' },
+            // { title: 'subheading', value: 'subheading' },
+          ],
+          marks: {
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+            annotations: [
+              {
+                title: 'URL',
+                name: 'link',
+                type: 'object',
+                fields: [
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url',
+
+                    validation: (Rule) =>
+                      Rule.required()
+                        .uri({ scheme: ['http', 'https'] })
+                        .warning('Url is incorrect'),
+                  },
+                  {
+                    title: 'Open in new window',
+                    name: 'blank',
+                    type: 'boolean',
+                    validation: (Rule) => Rule.required(),
+                    initialValue: false,
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
     },
   ],
 };
