cba10d3be027858963aba4cad926627e3a57bbcb
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-03-07 10:45:21 +0100

    add margin

--- components/home-page-eu-section.js
+++ components/home-page-eu-section.js
@@ -8,15 +8,15 @@ export default function HomePageEUSection({ euData }) {
     <Link href='/eu-wetgeving'>
       <div className='flex flex-col sm:flex-row items-center justify-between'>
         <div className='w-full sm:w-7/12 flex flex-col'>
-          <div className='border-b border-green-800'>
+          <div className='border-b border-green-800 mr-6'>
             <h2 className='p-5xl-semibold sm:p-7xl-semibold text-green-600 pb-6'>EU wetgeving</h2>
           </div>
-          <div className='hidden sm:block py-6 p-base'>
+          <div className='hidden sm:block py-6 p-base mr-6'>
             <p>{euData.euLaw}</p>
           </div>
           <div className='w-full flex justify-center sm:hidden py-6'>
             <div className='flex relative h-[200px] w-full justify-center'>
-              <Image src={euData?.image} fill alt='image for wat circulaw' className='rounded-cl ml-6' />{' '}
+              <Image src={euData?.image} fill alt='image for wat circulaw' className='rounded-cl' />{' '}
             </div>
           </div>
           <div className='block sm:hidden pb-6 p-base'>
