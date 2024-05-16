25c2114f15afabd7638012197c37b27b211abc73
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-05-16 10:48:52 +0200

    add eu law drop down menu on desktop

--- components/nav/index.js
+++ components/nav/index.js
@@ -93,6 +93,46 @@ export default function Nav(props) {
   const { getReferenceProps: mainMenuReferencProps, getFloatingProps: mainMenuFloatingProps } =
     useInteractions([mainMenuClick, mainMenuDismiss, mainMenuRole]);
 
+  // eu menu
+  const [euMenuIsOpen, setEuMenuIsOpen] = useState(false);
+  const {
+    refs: euRef,
+    floatingStyles: euStyles,
+    context: euContext,
+  } = useFloating({
+    placement: 'bottom-start',
+    open: euMenuIsOpen,
+    onOpenChange: setEuMenuIsOpen,
+    middleware: [offset(28), flip(), shift()],
+  });
+
+  const { isMounted: euMenuIsMounted, styles: euMenuTransitionStyles } = useTransitionStyles(
+    euContext,
+    {
+      duration: 300,
+      initial: {
+        opacity: 0,
+        transform: 'translateY(-100%)',
+      },
+      open: {
+        opacity: 100,
+        transform: 'translateY(0)',
+      },
+      close: {
+        opacity: 0,
+        transform: 'translateY(-100%)',
+      },
+    },
+  );
+
+  const euMenuClick = useClick(euContext);
+  const euMenuDismiss = useDismiss(euContext);
+  const euMenuRole = useRole(euContext);
+
+  const { getReferenceProps: euReferenceProps, getFloatingProps: euFloatingProps } =
+    useInteractions([euMenuClick, euMenuDismiss, euMenuRole]);
+
+
   // over menu
   const [overMenuIsOpen, setOverMenuIsOpen] = useState(false);
   const {
@@ -202,7 +242,7 @@ export default function Nav(props) {
             router.pathname === '/'
               ? [
                   `${
-                    mainMenuIsMounted || (overMenuIsMounted && scrollEffect === false)
+                    mainMenuIsMounted || (overMenuIsMounted && scrollEffect === false) || (euMenuIsMounted && scrollEffect === false)
                       ? ['bg-green-600 bg-opacity-100 shadow-lg transition-all duration-75']
                       : [
                           `${
@@ -498,7 +538,126 @@ export default function Nav(props) {
                     </FloatingFocusManager>
                   )}
                 </div>
-                <DesktopSimpleButton name='EU wetgeving' url='/eu-wetgeving' />
+
+
+                  {/* EU MENU */}
+                <div className=''>
+                  <button
+                    ref={euRef.setReference}
+                    {...euReferenceProps()}
+                    className='h-full relative p-sm group z-100 mr-6 lg:mr-8 flex flex-row items-center'
+                  >
+                    <span
+                      className={`${
+                        euMenuIsOpen === true
+                          ? [`${router.pathname === '/' ? 'text-green-200' : 'text-green-500'}`]
+                          : [
+                              `${
+                                router.pathname === '/'
+                                  ? 'text-white group-hover:decoration-green-200'
+                                  : 'text-green-800 group-hover:decoration-green-500'
+                              }`,
+                            ]
+                      } group-hover:underline`}
+                    >
+                      EU wetgeving
+                    </span>
+                    <ChevronDownIcon
+                      className={`${
+                        euMenuIsOpen
+                          ? [
+                              `${
+                                router.pathname === '/'
+                                  ? 'text-green-200 rotate-180'
+                                  : 'rotate-180 text-green-500'
+                              }`,
+                            ]
+                          : [
+                              `${
+                                router.pathname === '/'
+                                  ? 'text-white group-hover:text-green-200'
+                                  : 'group-hover:text-green-500'
+                              }`,
+                            ]
+                      } h-5 w-5 ml-2`}
+                    />
+                  </button>
+                  {euMenuIsMounted && (
+                    <FloatingFocusManager context={euContext} modal={false} disabled>
+                      <div
+                        ref={euRef.setFloating}
+                        style={euStyles}
+                        {...euFloatingProps()}
+                        className='h-auto w-72 -z-10 '
+                      >
+                        <div
+                          className={`${
+                            router.pathname === '/' ? 'bg-green-600' : 'bg-green-100'
+                          } h-full pb-10 shadow-lg pl-6 pt-8 pr-8`}
+                          style={{ ...euMenuTransitionStyles }}
+                          onMouseLeave={() => setEuMenuIsOpen(false)}
+                        >
+                          {/* Remove slice once news section is separate */}
+                          <div
+                              className={`${
+                                router.pathname === '/'
+                                  ? 'text-white'
+                                  : 'text-green-600 hover:text-green-500'
+                              } p-xs mb-2  hover:underline active:p-xs-semibold active:no-underline cursor-pointer`}
+                            >
+                              <Link
+                                href='/eu-wetgeving'
+                                id='navClick'
+                                onClick={() => {
+                                  setEuMenuIsOpen(false);
+                                  CustomEvent.trackEvent(
+                                    'Nav click',
+                                    router.asPath,
+                                  );
+                                }}
+                              >
+                                Overzicht
+                              </Link>
+                            </div>
+                          {props?.euSlugs?.map((euLaw) => (
+                            <div
+                              key={euLaw?.slug}
+                              className={`${
+                                router.pathname === '/'
+                                  ? 'text-white'
+                                  : 'text-green-600 hover:text-green-500'
+                              } p-xs mb-2  hover:underline active:p-xs-semibold active:no-underline cursor-pointer`}
+                            >
+                              <Link
+                                href={`/eu-wetgeving/${euLaw?.slug}`}
+                                id='navClick'
+                                onClick={() => {
+                                  setEuMenuIsOpen(false);
+                                  CustomEvent.trackEvent(
+                                    'Nav click',
+                                    router.asPath,
+                                    euLaw.title,
+                                  );
+                                }}
+                              >
+                                {euLaw.title}
+                              </Link>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </FloatingFocusManager>
+                  )}
+                </div>
+
+
+
+
+
+
+
+
+                  {/* ABOUT MENU */}
                 <div className=''>
                   <button
                     ref={overRef.setReference}
