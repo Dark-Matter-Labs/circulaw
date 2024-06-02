23ed0820c111da83d6c2be749a8e25215d3108fa
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-06-02 19:06:00 +0200

    add pagination

--- components/search/search.js
+++ components/search/search.js
@@ -1,30 +1,40 @@
 import algoliasearch from 'algoliasearch/lite';
 import 'instantsearch.css/themes/satellite.css';
-import { Hits, InstantSearch, SearchBox, Configure, RefinementList } from 'react-instantsearch';
+import {
+  Hits,
+  InstantSearch,
+  SearchBox,
+  Configure,
+  RefinementList,
+  Pagination,
+} from 'react-instantsearch';
 
 import { Hit } from '@/components/search/hit';
 
 const searchClient = algoliasearch('0L6RUN37T0', '5287d2668bdeebcbff12a4a06353266a');
 
+// create condition here to change indexName
 export const Search = () => {
   return (
     <>
       <InstantSearch searchClient={searchClient} indexName='instruments' className=''>
         <div className='flex flex-col'>
-        <div className='flex flex-col'>
-        <h3 className='mb-2'>Categorie</h3>
-        <RefinementList attribute="categorie" className='mb-12'/>
-        </div>
-        <div className='flex flex-col'>
-        <h3 className='mb-2'>Categorie</h3>
-        <RefinementList attribute="thema" className='mb-12'/>
-        </div>
+          <div className='flex flex-col'>
+            <h3 className='mb-2'>Categorie</h3>
+            <RefinementList attribute='categorie' className='mb-12' />
+          </div>
+          <div className='flex flex-col'>
+            <h3 className='mb-2'>Categorie</h3>
+            <RefinementList attribute='thema' className='mb-12' />
+          </div>
         </div>
         <Configure hitsPerPage={5} />
         <div className='ais-InstantSearch'>
-      
           <SearchBox />
           <Hits hitComponent={Hit} />
+          <Pagination
+          // Optional props
+          />
         </div>
       </InstantSearch>
     </>
