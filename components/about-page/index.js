8dfe91bd922e046f58234708f479a03799dcf708
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-06-07 15:32:42 +0200

    clean

--- components/about-page/index.js
+++ components/about-page/index.js
@@ -5,7 +5,6 @@ import { PortableText } from '@portabletext/react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
 export default function AboutPageComponent({ data }) {
   return (
     <div>
@@ -33,15 +32,11 @@ export default function AboutPageComponent({ data }) {
                 components={portableTextComponents}
               />
             </div>
-            {data?.aboutPage?.slug.current === 'Wetsanalyse-vanuit-circulaire-blik' &&
-            <div>
-                <Image 
-                  src='/method.png'
-                  alt= 'image of methodology'
-                  width={768}
-                  height={1806}
-                />
-            </div>}
+            {data?.aboutPage?.slug.current === 'Wetsanalyse-vanuit-circulaire-blik' && (
+              <div>
+                <Image src='/method.png' alt='image of methodology' width={768} height={1806} />
+              </div>
+            )}
           </div>
           <div className='hidden lg:block mt-3 lg:ml-12 lg:mb-20 lg:mt-32 col-span-1'>
             <AboutPageNav
