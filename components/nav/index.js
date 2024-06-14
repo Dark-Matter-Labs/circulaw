942c3fd3c4daf4de558e2c974ebe571bec957580
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-06-14 10:57:55 +0200

    finish search bar design

--- components/nav/index.js
+++ components/nav/index.js
@@ -59,7 +59,6 @@ export default function Nav(props) {
 
   const [searchIndex, setSearchIndex] = useState('instruments');
   const [searchQuery, setSearchQuery] = useState('');
-  console.log(searchIndex, searchQuery)
 
   const createQueryString = useCallback(
     (name, value) => {
@@ -73,7 +72,7 @@ export default function Nav(props) {
 
   const onChange = () => (e) => {
    const value = e.target.value
-  setSearchQuery(value)
+   setSearchQuery(value)
   }
 
   const onSubmit = () => (e) => {
@@ -937,7 +936,8 @@ export default function Nav(props) {
                               <div className='h-16 w-[600px] bg-green-600'>
                                 <form className='bg-green-600 w-[600px] h-[66px] rounded-cl flex-row items-center justify-between relative flex' onSubmit={onSubmit()}>
                                   <input className='w-[600px] h-[66px] focus:bg-[url("/search-icon.png")] bg-no-repeat bg-left [background-position-x:10px] pl-12 rounded-cl border-none bg-white/50 caret-white p-base text-white focus:ring-1 focus:ring-white placeholder:text-white placeholder:p-base-semibold' placeholder={searchIndex === 'instruments' ? 'Zoek naar instrumenten...' : 'Zoek naar over CircuLaw...'} onChange={onChange()}/>
-                                  <button className='ml-2 border h-[42px] w-24 border-white p-2 absolute top-3 right-3 shadow-card p-base-semibold text-green-600 bg-white rounded-cl'>Zoeken</button>
+                                  <button type='reset' title="Clear the search query" className={`${searchQuery === '' ? 'hidden' : ''} absolute top-3.5 right-28 rounded-full p-2 hover:bg-white/50 group`} onClick={() => setSearchQuery('')}><XIcon className='h-6 w-6 text-white group-hover:text-green-900'/></button>
+                                 <button type = 'submit' className='ml-2 border h-[42px] w-24 border-white p-2 absolute top-3 right-3 shadow-card p-base-semibold text-green-600 bg-white rounded-cl'>Zoeken</button>
                                 </form>
                               </div>
                             </div>
