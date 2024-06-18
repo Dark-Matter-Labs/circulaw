50ea9d6f679d061fa095ad1e18de4c317c3fd964
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-06-18 15:34:43 +0200

    fix search term display

--- components/search/stats.js
+++ components/search/stats.js
@@ -77,7 +77,7 @@ export default function CustomStats() {
         rLadder !== undefined) &&
       query !== '' ? (
         <div className='heading-2xl sm:heading-3xl'>
-          {nbHits} resultaten gevonden voor: <span className='font-semibold'>{query}</span>{' '}
+          {nbHits} resultaten gevonden voor: <span className='font-semibold'>&apos;{query}&apos; <span className='font-normal'>en</span></span>{' '}
           {categorie &&
             categorie.map((item) => (
               <span className='font-semibold capitalize' key={item}>
