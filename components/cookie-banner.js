b9bd5ac79402dac2deb131a948bb1db68526b003
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-06-19 15:02:49 +0200

    revert to window.location.reload

--- components/cookie-banner.js
+++ components/cookie-banner.js
@@ -1,11 +1,11 @@
 import { setCookie, hasCookie } from 'cookies-next';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 
 const CookieConsent = () => {
   const [consent, setConsent] = useState(true);
-  const router = useRouter();
+  // const router = useRouter();
   useEffect(() => {
     setConsent(hasCookie('localConsent'));
   }, []);
@@ -13,8 +13,8 @@ const CookieConsent = () => {
   const acceptCookie = () => {
     setConsent(true);
     setCookie('localConsent', 'true', { maxAge: 60 * 60 * 24 * 365 });
-    router.refresh(); // extra refresh to enable Hotjar
-
+    // router.refresh(); // extra refresh to enable Hotjar
+    window.location.reload()
     // eslint-disable-next-line
     ppms.cm.api(
       'setComplianceSettings',
