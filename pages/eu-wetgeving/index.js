73450f59547dcdecf8e5e197dcc06f16c42c9c0e
Gurden Batra <gurden.batra@gmail.com>
2024-07-10 16:13:34 +0200

    add PDF and update links

--- pages/eu-wetgeving/index.js
+++ pages/eu-wetgeving/index.js
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Link as ScrollLink } from 'react-scroll';
 import EULawCard from '@/components/eu-law/eu-law-card';
 import Layout from '@/components/layouts/layout';
 import { client } from '@/lib/sanity';
 // import bannerImage from '@/public/banner.png';
 import CustomButton from '@/components/custom-button';
 import { ArrowDownIcon } from '@heroicons/react/outline';
-import { Link as ScrollLink } from 'react-scroll';
-
 
 // refactor
 const euLawsQuery = `
@@ -27,66 +26,73 @@ export default function EULaw({ laws }) {
   return (
     <Layout title='EU wetgeving' pageUrl={router.asPath}>
       <div className='bg-[url("/bg-eu.png")] pt-3 overflow-hidden bg-center	bg-no-repeat bg-cover'>
-          <div className='flex flex-col justify-between global-margin h-full relative z-10 mt-10'>
-            <div className='pt-6 mb-20'>
-              <Link
-                href='/'
-                type='button'
-                className='rounded-clSm bg-gray-100 pl-2 pr-3 py-1.5 p-2xs-bold text-green-600'
-              >
-                <span className='link-interaction '>
-                  Home <span className='ml-2'>{'>'}</span>
-                </span>
-              </Link>
-            </div>
-            <div className='mb-8 flex flex-col'>
-              <h1 className='mb-10 heading-4xl-semibold sm:heading-5xl-semibold text-gray-100'>
-                EU wet- en regelgeving voor een circulaire economie
-              </h1>
-              <p className='p-base text-white max-w-xl mb-4'>
-                Er komt een lawine van Europese wet- en regelgeving op ons af, gericht op de
-                verduurzaming van onze maatschappij. Veel van deze nieuwe wet- en regelgeving valt
-                onder de Green Deal, met als tussendoel Fit for 55.
-              </p>
-              <p className='p-base text-white max-w-xl'>
-                CircuLaw laat je zien hoe je deze verordeningen en richtlijnen kunt toepassen op de
-                circulaire doelen van jouw gemeente of provincie.
-              </p>
-              <div className='mt-8'>
-                <ScrollLink to='laws' smooth={true} offset={-140}>
-                <CustomButton color='euPage'>Bekijk de {laws?.length} wetten <ArrowDownIcon className='h-5 w-5 ml-3 text-green-50'/></CustomButton>
-                </ScrollLink>
-              </div>
+        <div className='flex flex-col justify-between global-margin h-full relative z-10 mt-10'>
+          <div className='pt-6 mb-20'>
+            <Link
+              href='/'
+              type='button'
+              className='rounded-clSm bg-gray-100 pl-2 pr-3 py-1.5 p-2xs-bold text-green-600'
+            >
+              <span className='link-interaction '>
+                Home <span className='ml-2'>{'>'}</span>
+              </span>
+            </Link>
+          </div>
+          <div className='mb-8 flex flex-col'>
+            <h1 className='mb-10 heading-4xl-semibold sm:heading-5xl-semibold text-gray-100'>
+              EU wet- en regelgeving voor een circulaire economie
+            </h1>
+            <p className='p-base text-white max-w-xl mb-4'>
+              Er komt een lawine van Europese wet- en regelgeving op ons af, gericht op de
+              verduurzaming van onze maatschappij. Veel van deze nieuwe wet- en regelgeving valt
+              onder de Green Deal, met als tussendoel Fit for 55.
+            </p>
+            <p className='p-base text-white max-w-xl'>
+              CircuLaw laat je zien hoe je deze verordeningen en richtlijnen kunt toepassen op de
+              circulaire doelen van jouw gemeente of provincie.
+            </p>
+            <div className='mt-8'>
+              <ScrollLink to='laws' smooth={true} offset={-140}>
+                <CustomButton color='euPage'>
+                  Bekijk de {laws?.length} wetten{' '}
+                  <ArrowDownIcon className='h-5 w-5 ml-3 text-green-50' />
+                </CustomButton>
+              </ScrollLink>
             </div>
           </div>
+        </div>
       </div>
       <div className='global-margin'>
         <div className='flex sm:flex-row mt-10 flex-col'>
           <div className='flex flex-col justify-start p-base basis-1/2 mr-8'>
             <h2 className='mb-8 heading-2xl-semibold'>Wat is Green Deal?</h2>
             <div className='mb-8'>
-            <p className='mb-2'>
-              De Europese Unie wil in 2050 klimaatneutraal zijn: een ambitie die bekend staat als de
-              &apos;Green Deal&apos;. In het kader van de Green Deal worden continu nieuwe wetten
-              voorgesteld, behandeld en aangenomen, of worden bestaande herzien.
-            </p>
-            <p>
-              Met al deze richtlijnen en verordeningen wil de EU vanuit verschillende perspectieven
-              bijdragen aan klimaatneutraliteit. Hiernaast zie je een overzicht, inclusief de
-              huidige status van elk voorstel en het toepassingsgebied.
-            </p>
+              <p className='mb-2'>
+                De Europese Unie wil in 2050 klimaatneutraal zijn: een ambitie die bekend staat als
+                de &apos;Green Deal&apos;. In het kader van de Green Deal worden continu nieuwe
+                wetten voorgesteld, behandeld en aangenomen, of worden bestaande herzien.
+              </p>
+              <p>
+                Met al deze richtlijnen en verordeningen wil de EU vanuit verschillende
+                perspectieven bijdragen aan klimaatneutraliteit. Hiernaast zie je een overzicht,
+                inclusief de huidige status van elk voorstel en het toepassingsgebied.
+              </p>
             </div>
             <div className='sm:w-2/5 sm:mt-0'>
-              <CustomButton color='whiteBackground'>Download PDF</CustomButton>
+              <a href='/European_green_deal.pdf' target='_blank' rel='noreferrer'>
+                <CustomButton color='whiteBackground'>Download PDF</CustomButton>
+              </a>
             </div>
           </div>
           <div className='basis-1/2 w-full h-full mt-8 sm:mt-0'>
-            <Image
-              src='/green_deal.png'
-              alt='Screenshot of the PDF green deal'
-              width={588.5}
-              height={328}
-            />
+            <a href='/European_green_deal.pdf' target='_blank' rel='noreferrer'>
+              <Image
+                src='/green_deal.png'
+                alt='Screenshot of the PDF green deal'
+                width={588.5}
+                height={328}
+              />
+            </a>
           </div>
         </div>
         <div className='flex flex-col max-w-2xl p-base mb-4'>
@@ -97,7 +103,8 @@ export default function EULaw({ laws }) {
             Lokale overheden zijn vaak niet op de hoogte van de verplichtingen en kansen die
             voortvloeien uit Europese wet- en regelgeving. CircuLaw brengt daar verandering in. Wij
             brengen de toepasbaarheid voor de circulaire economie in kaart van richtlijnen en
-            verordeningen. Op dit moment hebben we {laws?.length} analyses gereed; je vindt ze hieronder.
+            verordeningen. Op dit moment hebben we {laws?.length} analyses gereed; je vindt ze
+            hieronder.
           </p>
           <p>
             We blijven nieuwe analyses toevoegen, en de status van de verschillende wet- en
@@ -106,7 +113,10 @@ export default function EULaw({ laws }) {
           </p>
         </div>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lgNav:grid-cols-3 gap-y-8 gap-x-6 global-margin my-12 relative' id='laws'> 
+      <div
+        className='grid grid-cols-1 md:grid-cols-2 lgNav:grid-cols-3 gap-y-8 gap-x-6 global-margin my-12 relative'
+        id='laws'
+      >
         {laws?.map((law, id) => (
           <div key={id} className=''>
             <EULawCard law={law} />
