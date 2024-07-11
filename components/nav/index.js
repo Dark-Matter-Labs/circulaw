77d8d8eb666c45e2a43574941bb572dc50cf3311
Gurden Batra <gurden.batra@gmail.com>
2024-07-11 11:36:55 +0200

    fix mobile nav bug

--- components/nav/index.js
+++ components/nav/index.js
@@ -482,7 +482,12 @@ export default function Nav(props) {
                                     <Disclosure.Panel className='ml-4'>
                                       <ul>
                                         <li className='p-base h-10 my-2 last:mb-2 text-green-600 cursor-pointer flex items-center'>
+                                        <Link
+                                              href={'/eu-wetgeving'}
+                                              onClick={() => setMobileMenuIsOpen(false)}
+                                            >
                                           Overzicht
+                                          </Link>
                                         </li>
                                         {props?.euSlugs?.map((euPage) => (
                                           <li
