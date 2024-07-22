305e77de36c7328ee8e0cfd5fb78b80585fa4be5
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-07-22 15:35:39 +0200

    add homepage and root layout

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,21 @@
 import '../global.css';
 import 'tailwindcss/tailwind.css';
 
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+
 import Layout from '@/components/layouts/layout'
 
-export const metadata = {
-  title: 'Next.js',
-}
+const plus_Jakarta_Sans = Plus_Jakarta_Sans({
+  weight: ['200','300','400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-plus-jakarta-sans',
+})
 
-export default function RootLayout({
-    children,
-  }) {
-    return (
-      <html lang="nl">
-        
+export default function RootLayout({children}) {
+  return (
+      <html lang="nl" className={plus_Jakarta_Sans.variable}>
         <body>
           <Layout>
           {children}
