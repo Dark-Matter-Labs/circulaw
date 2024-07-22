0ea04a210436c78e7c708ab5c6cab1b75c01689d
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-07-22 15:48:38 +0200

    add comments to meta

--- utils/global-meta.js
+++ utils/global-meta.js
@@ -8,12 +8,12 @@ if (process.env.NEXT_PUBLIC_SANITY_DATASET === 'staging') {
 
 
 const globalMeta = {
-	siteName: 'Regelgeving voor een circulaire economie',
+	siteName: 'Regelgeving voor een circulaire economie', // added to rootLayout
+	description: 'CircuLaw helpt decentrale overheden met het versnellen van de circulaire transitie. Door te laten zien hoe je daarvoor bestaande wet-en regelgeving kunt inzetten.', // added to rootLayout
+	keywords: ['CircuLaw, circulaire economie, juridische instrumenten'], // added to rootLayout
 	siteUrl: url,
 	email: 'info@circulaw.nl',
     siteLogo: url + '/api/og',
-	description: 'CircuLaw helpt decentrale overheden met het versnellen van de circulaire transitie. Door te laten zien hoe je daarvoor bestaande wet-en regelgeving kunt inzetten.',
-	keywords: 'CircuLaw, circulaire economie, juridische instrumenten'
 }
 
 export default globalMeta;
