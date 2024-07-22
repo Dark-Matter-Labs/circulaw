4d1543190dc433bd289c9314d4ef170091ecff82
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-07-22 15:33:11 +0200

    make header text static

--- components/homepage-header.js
+++ components/homepage-header.js
@@ -2,17 +2,17 @@ import CustomButton from './custom-button';
 import { ArrowDownIcon } from '@heroicons/react/outline';
 import { Link as ScrollLink } from 'react-scroll';
 
-export default function HomepageHeader({ homePageHeader }) {
+export default function HomepageHeader() {
   return (
     <main className='pb-16 pt-56'>
       <div className='global-margin'>
         <div className='text-center max-w-4xl mx-auto'>
           <div className='text-left sm:text-center max-w-xs m-auto sm:max-w-4xl sm:block text-gray-200'>
             <h1 className='block header-desktop-mobile sm:header-desktop'>
-              {homePageHeader.headerText}
-            </h1>{' '}
+            Regelgeving voor een circulaire economie
+                        </h1>{' '}
             <h2 className='p-base sm:heading-2xl-semibold text-left m-auto sm:text-center mt-2 max-w-sm sm:max-w-full text-gray-200 pt-10 pb-8'>
-              {homePageHeader.subHeader}
+            CircuLaw laat decentrale overheden zien hoe je de circulaire transitie kunt versnellen. Per productketen bieden we juridische instrumenten op basis van de Nederlandse wetgeving. Ook geven we inzicht in Europese wetgeving.
             </h2>
           </div>
           <div className='mt-2 sm:flex sm:justify-center md:mt-2'>
