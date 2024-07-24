f57deffb9698dcfe5f9297c44e71df1751513baa
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-07-24 11:55:04 +0200

    add comments

--- app/layout.js
+++ app/layout.js
@@ -49,6 +49,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang='nl' className={plus_Jakarta_Sans.variable}>
       <body>
+        {/* TODO - at the moment the entire layout for every page is wrapped in suspense. This is because the Nav uses useSearchParams and this needs to be wrapped */}
+        {/* We could either: remove useSearchParams from nav, e.g. make a component just for the search part of the nav which uses it, seperate the nav from the layout component */}
         <Suspense>
         <Layout>{children}</Layout>
         </Suspense>
