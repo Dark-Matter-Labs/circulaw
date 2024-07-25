3fb584cdef21997fcb50a59f537ef20d8f1b3593
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-07-25 07:35:26 +0200

    fetch about page data

--- app/over/[slug]/page.js
+++ app/over/[slug]/page.js
@@ -1,9 +1,25 @@
+import { client } from '@/lib/sanity';
+import { ABOUT_PAGE_PATHS_QUERY, ABOUT_PAGE_QUERY } from '@/lib/queries';
 
+export async function generateStaticParams() {
+  const slugs = await client.fetch(ABOUT_PAGE_PATHS_QUERY);
 
+  return slugs.map((slug) => ({ slug }));
+}
 
-export default async function Page() {
-    return (
-        <>
-        </>
-    )
+async function getPageData(params) {
+  const slug = params;
+  const aboutPageContent = await client.fetch(ABOUT_PAGE_QUERY, { slug });
+
+  if (!aboutPageContent) {
+    throw new Error('could not fetch content');
+  }
+
+  return aboutPageContent;
+}
+
+export default async function Page({ params }) {
+  const data = await getPageData(params.slug);
+    console.log(data)
+  return <></>;
 }
