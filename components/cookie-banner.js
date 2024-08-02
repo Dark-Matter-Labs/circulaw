f18bc9fa9fa4da4c40ac0cb0c3a8d19535e161d8
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-02 17:22:06 +0200

    Update cookie-banner.js

--- components/cookie-banner.js
+++ components/cookie-banner.js
@@ -35,7 +35,7 @@ const CookieConsent = ({ hasLocalConsentCookie }) => {
       () => console.log('consent fail'),
     );
   };
-  if (consent === true) {
+  if (consent === true || window.location.host.includes('staging') || window.location.host.includes('-git-')) {
     return null;
   }
 
