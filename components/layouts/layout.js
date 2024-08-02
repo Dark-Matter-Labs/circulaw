e7a05fe183d6315e1f65a9151a64b8e7af1ca6bc
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-02 12:26:43 +0200

    add hotjar to layout comp

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -3,12 +3,24 @@ import CookieConsent from '@/components/cookie-banner';
 import Nav from '@/components/nav';
 import Footer from '@/components/nav/footer';
 import PiwikProProvider from '@piwikpro/next-piwik-pro';
+import { useEffect } from 'react';
+import { getCookie } from 'cookies-next';
+import { hotjar } from 'react-hotjar';
 
 // TODO: what parts of this can we move directly to layout.js
 // moving these parts mean they will render on the server and not client side.
 // at the moment most of the layout is being rendered client side which defeats the purporse of the app router.
 export default function Layout({ children, homePageHeader, navData, partnerLogos }) {
   const vraagSlug = '/vraag-en-antwoord';
+
+  const consent = getCookie('localConsent');
+
+  useEffect(() => {
+    if (consent === true) {
+      hotjar.initialize(Number(process.env.NEXT_PUBLIC_HJID), Number(process.env.NEXT_PUBLIC_HJSV));
+    }
+  }, [consent]);
+
 
   return (
     <>
