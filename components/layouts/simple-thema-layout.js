92e4a6c8226de41b87c387b260d3dfd3f1804205
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-04 14:19:17 +0200

    add sponsor section to front end

--- components/layouts/simple-thema-layout.js
+++ components/layouts/simple-thema-layout.js
@@ -1,10 +1,13 @@
 import InstrumentCard from '../instrument/instrument-card';
 import ThemePageHeaderMobile from '../theme-page/theme-page-header-mobile';
 import ThemePageHeader from '@/components/theme-page/theme-page-header';
+import ImageComponent from '../image-component';
+import Link from 'next/link';
 
 // TODO: See what we could move to a layout.js file instead of having everything in components.
 export default function SimpleThemaLayout({ instruments, numberOfLaws, ...props }) {
   const themaData = props.thema;
+  console.log()
   return (
     <>
       {/* HEADER DESKTOP */}
@@ -29,6 +32,29 @@ export default function SimpleThemaLayout({ instruments, numberOfLaws, ...props 
           </div>
         </div>
       </div>
+      {props?.thema?.themeSponsors?.length > 0 && 
+          <div className='bg-gray-100 h-auto'>
+            <div className='global-margin flex flex-col items-center justify-center'>
+              <div className='mt-20 mb-10 heading-xl-semibold text-green-500 flex flex-row items-center'>
+                  <div className='h-11 w-1.5 bg-green-500'>
+                    
+                  </div>
+                  <div className='ml-4'>
+                  Het thema ‘Voedselverspilling’ is mede mogelijk gemaakt door:
+                  </div>
+                
+              </div>
+              <ul className='mb-16 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 sm:gap-x-8'>
+            {props?.thema?.themeSponsors?.map((sponsor, id) => (
+              <li key={id} className='relative h-28 w-52'>
+                <Link href={sponsor.partnerLink}>
+                  <ImageComponent image={sponsor.logo}/>       
+                </Link>
+              </li>
+            ))}
+            </ul>
+            </div>
+          </div>}
     </>
   );
 }
