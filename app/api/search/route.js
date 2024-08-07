abb7252f4fe1c791a09d15b08886a084f466c399
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-07 15:41:42 +0200

    add projection

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -52,6 +52,34 @@ export async function POST(req) {
                           }
                     `,
         },
+
+        euLaw: {
+         index: agoliaInstance.initIndex('EuLaw'),
+         projection: `
+          {
+          "objectID": _id,
+            'lawTitle': coalesce(euLawReference->title, title),
+            'slug': coalesce(euLawReference->slug.current, slug.current),
+            defined(introText) => introText,
+            'eu1Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[0].content)), "")[0..6000], "")}.eu1Content,
+            'eu2Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[1].content)), "")[0..6000], "")}.eu2Content,
+            'eu3Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[2].content)), "")[0..6000], "")}.eu3Content,
+            'eu4Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[3].content)), "")[0..6000], "")}.eu4Content,
+            'eu5Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[4].content)), "")[0..6000], "")}.eu5Content,
+            'eu6Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[5].content)), "")[0..6000], "")}.eu6Content,
+            'eu7Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[6].content)), "")[0..6000], "")}.eu7Content,
+            'eu8Content': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {'eu1Content': array::join(string::split((pt::text(europeContent[7].content)), "")[0..6000], "")}.eu8Content,
+            'eu1Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu1Title": europeContent[0].title,}.eu1Title,
+            'eu2Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu2Title": europeContent[1].title,}.eu2Title,
+            'eu3Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu3Title": europeContent[2].title,}.eu3Title,
+            'eu4Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu4Title": europeContent[3].title,}.eu4Title,
+            'eu5Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu5Title": europeContent[4].title,}.eu5Title,
+            'eu6Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu6Title": europeContent[5].title,}.eu6Title,
+            'eu7Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu7Title": europeContent[6].title,}.eu7Title,
+            'eu8Title': *[_type == 'euEuropeTab' && euLawReference._ref == ^._id][0] {"eu8Title": europeContent[7].title,}.eu8Title,
+                  }
+         `
+        }
       },
 
       (document) => {
