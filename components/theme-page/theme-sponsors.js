9d9f79becd69165f792142a321ec7fbc870b8710
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-13 10:16:45 +0200

    add initial animation to logos

--- components/theme-page/theme-sponsors.js
+++ components/theme-page/theme-sponsors.js
@@ -1,7 +1,35 @@
+'use client';
 import ImageComponent from '../image-component';
 import Link from 'next/link';
+import { motion, stagger, useAnimate } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+const staggerMenuItems = stagger(0.2, { startDelay: 0.15 });
+
+function useMenuAnimation(isInView) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate(
+      'li',
+      isInView
+        ? { opacity: 1, y: 0, filter: 'blur(0px)' }
+        : { opacity: 0, y: 100, filter: 'blur(20px)' },
+      {
+        duration: 0.3,
+        delay: isInView ? staggerMenuItems : 0,
+      },
+    );
+  }, [isInView, animate]);
+
+  return scope;
+}
 
 export default function ThemeSponsors({ thema, sponsors }) {
+  const [isInView, setIsInView] = useState(false);
+
+  const scope = useMenuAnimation(isInView);
+
   return (
     <div className='bg-gray-100 h-auto'>
       <div className='global-margin flex flex-col items-center justify-center'>
@@ -9,7 +37,11 @@ export default function ThemeSponsors({ thema, sponsors }) {
           <div className='h-11 w-1.5 bg-green-500'></div>
           <div className='ml-4'>Het thema &apos;{thema}&apos; is mede mogelijk gemaakt door:</div>
         </div>
-        <ul className='mb-16 flex flex-row flex-wrap sm:gap-x-8 items-center justify-center'>
+        <motion.ul
+          ref={scope}
+          whileInView={() => setIsInView(true)}
+          className='mb-16 flex flex-row flex-wrap sm:gap-x-8 items-center justify-center'
+        >
           {sponsors?.map((sponsor, id) => (
             <li key={id} className='relative h-28 w-52'>
               <Link href={sponsor.partnerLink}>
@@ -17,7 +49,7 @@ export default function ThemeSponsors({ thema, sponsors }) {
               </Link>
             </li>
           ))}
-        </ul>
+        </motion.ul>
       </div>
     </div>
   );
