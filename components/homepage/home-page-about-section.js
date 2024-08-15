ced188ba78ae60b04ed4a24426b96d1ac6dbfcc0
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-15 17:44:58 +0200

    make youtube component and wrap in suspense

--- components/homepage/home-page-about-section.js
+++ components/homepage/home-page-about-section.js
@@ -3,6 +3,8 @@ import aboutImage from '@/public/home-page/homepageAboutDeco.png';
 import { ArrowRightIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Suspense } from 'react';
+import YouTubeComponent from './youtube-component';
 
 export default function HomePageAboutSection({ aboutSection }) {
   return (
@@ -33,16 +35,9 @@ export default function HomePageAboutSection({ aboutSection }) {
       </div>
       <div className='w-full sm:w-1/2 sm:flex justify-center h-400px mt-6 sm:mt-0'>
         <div className='w-full relative overflow-hidden pt-[56%]'>
-          <iframe
-            className='absolute bottom-0 left-0 sm:left-24 right-0 top-0 sm:top-12 h-full w-full sm:h-3/4 sm:w-3/4 rounded-cl'
-            width='450'
-            height='253'
-            src='https://www.youtube.com/embed/9PTTCyLhzLo?si=uqv63SA7T44lQLnO'
-            title='YouTube video player'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            referrerPolicy='strict-origin-when-cross-origin'
-            allowFullScreen
-          ></iframe>
+          <Suspense>
+           <YouTubeComponent />
+          </Suspense>
         </div>
       </div>
     </div>
