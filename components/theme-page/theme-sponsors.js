9727d2351c4fb6bd3ccff82347ce9bc6716e86b1
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-19 10:42:08 +0200

    add in view effect

--- components/theme-page/theme-sponsors.js
+++ components/theme-page/theme-sponsors.js
@@ -1,9 +1,36 @@
+'use client'
+import { useRef, useState, useEffect } from 'react';
 import ImageComponent from '../image-component';
 import Link from 'next/link';
 
 export default function ThemeSponsors({ thema, sponsors }) {
+  const ref = useRef()  
+  const [inView, setInView] = useState()
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        const isVisible = (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+        setInView(isVisible);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    // Initial check on component mount
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='bg-gray-100 h-auto'>
+    <div className={`${inView ? 'bg-gray-100' : 'bg-red-400'} h-auto`} ref={ref}>
       <div className='global-margin flex flex-col items-center justify-center'>
         <div className='mt-20 mb-10 heading-xl-semibold text-green-500 flex flex-row items-center'>
           <div className='h-11 w-1.5 bg-green-500'></div>
