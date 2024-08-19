4931158b3867b50c26a2bf84f34f2116bc333025
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-19 15:02:21 +0200

    fix eu law sizes

--- components/eu-law/status/status-two-step.js
+++ components/eu-law/status/status-two-step.js
@@ -8,6 +8,8 @@ import mobileStep1 from '@/public/eu-status/mobile-two-step/mobile-two-step-1.sv
 import mobileStep2 from '@/public/eu-status/mobile-two-step/mobile-two-step-2.svg';
 import mobileStep3 from '@/public/eu-status/mobile-two-step/mobile-two-step-3.svg';
 import mobileStep4 from '@/public/eu-status/mobile-two-step/mobile-two-step-4.svg';
+
+
 import Image from 'next/image';
 
 export default function StatusTwoStep({ status }) {
