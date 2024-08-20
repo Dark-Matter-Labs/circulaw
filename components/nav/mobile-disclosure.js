ecb638be04f518f5015c6562bde0a1977b7efb08
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-08-20 16:01:21 +0200

    clean

--- components/nav/mobile-disclosure.js
+++ components/nav/mobile-disclosure.js
@@ -36,8 +36,12 @@ export default function MobileDisclosure({ navData, closeMenu }) {
                         className='h-10 flex items-center'
                         onClick={() => closeMenu(false)}
                       >
-                        {thema?.themaName} 
-                        {thema.new &&  <span className='bg-green-800 text-green-50 font-jakarta text-[10px] font-semibold px-1.5 h-5 flex items-center justify-center rounded-clSm ml-2'>Nieuw</span>}
+                        {thema?.themaName}
+                        {thema.new && (
+                          <span className='bg-green-800 text-green-50 font-jakarta text-[10px] font-semibold px-1.5 h-5 flex items-center justify-center rounded-clSm ml-2'>
+                            Nieuw
+                          </span>
+                        )}
                       </Link>
                     </li>
                   ))}
