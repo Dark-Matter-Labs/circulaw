c979b6f485ec8d79592505ad9cd79748bc4e8d74
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-09-03 13:10:47 +0200

    add options

--- studio/schemas/objects/faq-item.js
+++ studio/schemas/objects/faq-item.js
@@ -17,17 +17,39 @@ export default {
       type: 'array',
       of: [
         {
+          type: 'greenBox',
+        },
+        {
           type: 'pdfBlock',
         },
         {
+          type: 'smallPara',
+        },
+        {
+          type: 'imageBlock',
+        },
+        {
           type: 'block',
-          styles: [{ title: 'normal', value: 'normal' }],
+          of: [
+            {
+              type: 'dropDown',
+            },
+          ],
           lists: [
             { title: 'Bullet', value: 'bullet' },
             { title: 'Number', value: 'number' },
           ],
+          styles: [
+            { title: 'H2', value: 'h2' },
+            { title: 'H3', value: 'h3' },
+            { title: 'normal', value: 'normal' },
+            // { title: 'subheading', value: 'subheading' },
+          ],
           marks: {
-            decorators: [],
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
             annotations: [
               {
                 title: 'URL',
@@ -38,11 +60,18 @@ export default {
                     title: 'URL',
                     name: 'href',
                     type: 'url',
+
+                    validation: (Rule) =>
+                      Rule.required()
+                        .uri({ scheme: ['http', 'https'] })
+                        .warning('Url is incorrect'),
                   },
                   {
                     title: 'Open in new window',
                     name: 'blank',
                     type: 'boolean',
+                    validation: (Rule) => Rule.required(),
+                    initialValue: false,
                   },
                 ],
               },
