b4664e128f9394338034208827146c11b95d72fb
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-09-25 11:11:39 +0200

    fix width and title

--- components/eu-law/scroll-tab-content.js
+++ components/eu-law/scroll-tab-content.js
@@ -9,7 +9,7 @@ import { XIcon } from '@heroicons/react/outline';
 
 export default function ScrollPagesTabContent({ content }) {
   const [isOpen, setIsOpen] = useState(false);
-  
+
   function closeModal() {
     setIsOpen(false);
   }
@@ -71,13 +71,13 @@ export default function ScrollPagesTabContent({ content }) {
           >
             <DialogPanel
               transition
-              className='w-full max-w-md ml-10 min-h-[101vh] bg-white p-8 pt-10 z-130 duration-500 ease-out data-[closed]:translate-x-[28rem] '
+              className='w-full max-w-sm sm:max-w-md ml-10 min-h-[101vh] bg-white p-8 pt-10 z-130 duration-500 ease-out data-[closed]:translate-x-[28rem] '
             >
               <CloseButton className='absolute float-right top-0 right-0 flex pt-4 pr-8 sm:-ml-10'>
                 <span className='sr-only'>Close panel</span>
                 <XIcon className='h-6 w-6' aria-hidden='true' />
               </CloseButton>
-              <DialogTitle className='heading-3xl-semibold'>TITLE</DialogTitle>
+              <DialogTitle className='heading-3xl-semibold'>In deze pagina</DialogTitle>
               <div className='relative mt-6 flex-1'>
                 <ul className=''>
                   {content.map((section, id) => (
