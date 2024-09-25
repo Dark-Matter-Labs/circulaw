1f5f02f72ce82ec08fb80267e4978ee9d59518ea
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-09-25 15:15:00 +0200

    clean and remove old comp

--- components/modeltext/pillars.js
+++ components/modeltext/pillars.js
@@ -1,8 +1,11 @@
 'use client';
 import { useState, useEffect, useRef, useCallback } from 'react';
-import ModelTextDetail from './modeltext-detail';
+import { IconX, IconCopy } from '@tabler/icons-react';
 import ModelTextCard from './modeltext-card';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { PortableText } from '@portabletext/react';
+import { reducedPortableTextComponents } from '@/lib/portable-text/pt-components';
+import Link from 'next/link';
 
 export default function Pillars({ pillars, modelTexts }) {
   const searchParams = useSearchParams();
@@ -29,13 +32,15 @@ export default function Pillars({ pillars, modelTexts }) {
   const [modelTextSlug, setModelTextSlug] = useState();
   const [filteredWithSelected, setFilteredWithSelected] = useState();
   const [selectedModelText, setSelectedModelText] = useState(null);
-  console.log(selectedModelText);
+  const [showLinkCopied, setShowLinkCopied] = useState(false);
   // use effect to set the pillar and filer the modeltexts
   useEffect(() => {
     // initialise from search params
     let modelTextSlug = searchParams.get('modeltext');
-    if (modelTextSlug) {
+    if (modelTextSlug !== '') {
       setModelTextSlug(modelTextSlug);
+    } else if (modelTextSlug === '') {
+      setModelTextSlug(undefined);
     }
 
     let pillar = searchParams.get('pillar');
@@ -56,7 +61,7 @@ export default function Pillars({ pillars, modelTexts }) {
       setSelectedPillar(pillar);
       let filered = modelTexts.filter((t) => t.pillar === pillar);
       let refiltered = filered.filter((t) => t.slug !== modelTextSlug);
-      setSelectedModelText(modelTexts.filter((t) => t.slug === modelTextSlug));
+      setSelectedModelText(modelTexts.filter((t) => t.slug === modelTextSlug)[0]);
       setFilteredWithSelected(refiltered);
     }
   }, [searchParams, modelTexts, modelTextSlug]);
@@ -72,6 +77,7 @@ export default function Pillars({ pillars, modelTexts }) {
     <>
       <div className='max-w-[1280px]'>
         <ul
+          ref={modelTextRef}
           id='pillars'
           className='bg-green-50 rounded-cl flex flex-row p-4 mt-14 gap-x-2.5 justify-between'
         >
@@ -79,10 +85,6 @@ export default function Pillars({ pillars, modelTexts }) {
             <li key={p.title}>
               <button
                 onClick={() => {
-                  //  setUrl(`/modeltext?${createQueryString('pillar', p.slug)}`)
-                  //  window.history.pushState(null, undefined, url)
-                  // router.push(`/modeltext?${createQueryString('pillar', p.slug)}`, { scroll: false })
-                  // setSelectedModelText(null);
                   setModelTextSlug(undefined);
                   router.push(pathname + '?' + createQueryString('pillar', p.slug), {
                     scroll: false,
@@ -120,8 +122,6 @@ export default function Pillars({ pillars, modelTexts }) {
             <button
               key={id}
               onClick={() => {
-                // setUrl(`/modeltext?${createQueryString('pillar', text.pillar)}&${createQueryString('modeltext', text.slug)}`)
-                // window.history.pushState(null, undefined, url)
                 router.push(pathname + '?' + createQueryString('modeltext', text.slug), {
                   scroll: false,
                 });
@@ -132,14 +132,12 @@ export default function Pillars({ pillars, modelTexts }) {
           ))}
         </div>
       ) : (
-        <div className='flex items-start justify-center mt-12' ref={modelTextRef}>
+        <div className='flex items-start justify-center mt-12'>
           <div className='basis-1/3 h-screen overflow-y-scroll flex flex-col gap-y-8 pl-4 py-4 no-scrollbar'>
             {filteredWithSelected?.map((text, id) => (
               <button
                 key={id}
                 onClick={() => {
-                  // setUrl(`/modeltext?${createQueryString('pillar', text.pillar)}&${createQueryString('modeltext', text.slug)}`)
-                  // window.history.pushState(null, undefined, url)
                   router.push(pathname + '?' + createQueryString('modeltext', text.slug), {
                     scroll: false,
                   });
@@ -150,35 +148,103 @@ export default function Pillars({ pillars, modelTexts }) {
             ))}
           </div>
           <div className='basis-2/3 pl-12 flex items-start justify-start h-full py-4'>
-            {selectedModelText && <ModelTextDetail selectedModelText={selectedModelText[0]} />}
+            {selectedModelText && (
+              <div className='rounded-cl bg-gray-100 border w-[635px] py-6 px-10'>
+                <div className='flex flex-row w-full justify-between items-center'>
+                  <h4 className='heading-2xl-semibold mb-2.5'>{selectedModelText.title}</h4>
+                  <button
+                    onClick={() => {
+                      setModelTextSlug(undefined);
+                      router.push(
+                        pathname + '?' + createQueryString('pillar', selectedModelText.pillar),
+                        {
+                          scroll: false,
+                        },
+                      );
+                    }}
+                  >
+                    <IconX className='h-6 w-6 text-green-800' />
+                  </button>
+                </div>
+
+                <h5 className='heading-xl-semibold mb-10'>Modeltekst omgevingsplan</h5>
+                <div className='w-full border border-green-800 flex flex-col p-6 rounded-cl mb-10'>
+                  <div className='self-end relative'>
+                    <button
+                      onClick={() => {
+                        navigator.clipboard.writeText(selectedModelText.modelTextPT);
+                        setShowLinkCopied(true);
+                        setTimeout(() => {
+                          setShowLinkCopied(false);
+                        }, 1800);
+                      }}
+                      className={`${
+                        showLinkCopied ? 'hidden' : 'block'
+                      } p-xs-semibold flex flex-row text-gray-600 mb-8`}
+                    >
+                      Kopieer tekst <IconCopy className='w-6 h-6 text-green-800 ml-2.5' />
+                    </button>
+                    {showLinkCopied && (
+                      <p className='p-xs-semibold text-green-500 text-nowrap flex flex-row mb-8'>
+                        Tekst gekopieerd!
+                        <IconCopy className='w-6 h-6 text-green-500 ml-2.5' />
+                      </p>
+                    )}
+                  </div>
+                  <PortableText
+                    value={selectedModelText.modelText}
+                    components={reducedPortableTextComponents}
+                  />
+                </div>
+                <div className='flex flex-col max-w-[425px] mb-6'>
+                  <h6 className='heading-xl-semibold'>Toelichting</h6>
+                  <PortableText
+                    value={selectedModelText.description}
+                    components={reducedPortableTextComponents}
+                  />
+                </div>
+                <div className='flex flex-col mb-10 max-w-[425px]'>
+                  <h6 className='heading-xl-semibold mb-6'>Gelinkte instrumenten</h6>
+                  <ul className='list-disc list-inside ml-2'>
+                    {selectedModelText?.linkedInstruments?.map((instrument) => (
+                      <li className='p-base underline' key={instrument.slug}>
+                        <Link
+                          className='link-interaction text-green-600'
+                          href={`/${instrument.transitionAgenda}/${instrument.thema}/instrumenten/${instrument.slug}`}
+                        >
+                          {instrument.titel}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className='flex flex-row justify-between '>
+                  <div className='flex flex-row gap-4'>
+                    <div className='flex flex-col'>
+                      <div className='p-xs-semibold mb-2'>Schaalniveau</div>
+                      <div className='text-xs text-gray-600 bg-white border border-gray-200 rounded-cl px-2 py-1'>
+                        {selectedModelText.scale}
+                      </div>
+                    </div>
+                    <div className='flex flex-col'>
+                      <div className='p-xs-semibold mb-2'>Houdbaarheid</div>
+                      <div className='text-xs text-gray-600 bg-white border border-gray-200 rounded-cl px-2 py-1'>
+                        {selectedModelText.impactLevel}
+                      </div>
+                    </div>
+                    <div className='flex flex-col'>
+                      <div className='p-xs-semibold mb-2'>Type regel</div>
+                      <div className='text-xs text-gray-600 bg-white border border-gray-200 rounded-cl px-2 py-1'>
+                        {selectedModelText.type}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       )}
     </>
   );
-}
-
-{
-  /*
-
-// scroll to somehwer
- 
-
-     {selectedModelText && (
-         <ModelTextDetail selectedModelText = {selectedModelText}/>
-        )}
-  
-  useEffect(() => {
-    if (selectedModelText === null) {
-      const filtered = modelTexts.filter((t) => t.pillar === selectedPillar);
-      setFilteredModelTexts(filtered);
-    } else {
-      const filtered = modelTexts.filter((t) => t.pillar === selectedPillar);
-      const reFiltered = filtered.sort(function (x, y) {
-        return x === selectedModelText ? -1 : y === selectedModelText ? 1 : 0;
-      });
-      setFilteredModelTexts(reFiltered);
-    }
-  }, [modelTexts, selectedPillar, selectedModelText]);
-  */
 }
