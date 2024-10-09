60bd620bbc6d891fac0002ceec1adb478d7d9bd1
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-10-09 11:41:48 +0200

    add eutab

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -63,6 +63,32 @@ const NEWS_ITEM_PROJECTION = `{
     linkUrl,
 }`
 
+const EU_LAW_EU_TAB_PROJECTION =`{
+      "objectID": _id,
+      'lawTitle': coalesce(euLawReference->title, title),
+      'searchTitle': coalesce(euLawReference->title, title) + ' - ' + title,
+      'slug': coalesce(euLawReference->slug.current, slug.current),
+      'eu1Content': array::join(string::split((pt::text(europeContent[0].content)), "")[0..5500], ""), 
+      'eu2Content': array::join(string::split((pt::text(europeContent[2].content)), "")[0..5500], ""),
+      'eu3Content': array::join(string::split((pt::text(europeContent[3].content)), "")[0..5500], ""), 
+      'eu4Content': array::join(string::split((pt::text(europeContent[4].content)), "")[0..5500], ""), 
+      'eu5Content': array::join(string::split((pt::text(europeContent[5].content)), "")[0..5500], ""), 
+      'eu6Content': array::join(string::split((pt::text(europeContent[6].content)), "")[0..5500], ""), 
+      'eu7Content': array::join(string::split((pt::text(europeContent[7].content)), "")[0..5500], ""), 
+      'eu8Content': array::join(string::split((pt::text(europeContent[8].content)), "")[0..5500], ""), 
+      'eu1Title': europeContent[0].title,
+      'eu2Title': europeContent[1].title,
+      'eu3Title': europeContent[2].title,
+      'eu4Title': europeContent[3].title,
+      'eu5Title': europeContent[4].title,
+      'eu6Title': europeContent[5].title,
+      'eu7Title': europeContent[6].title,
+      'eu8Title': europeContent[7].title,
+}`
+
+// clTab
+// localTab
+
 export async function POST(req) {
   try {
     const sanityAgolia = indexer(
@@ -83,6 +109,10 @@ export async function POST(req) {
           index: agoliaInstance.initIndex('newsItems'),
           projection: NEWS_ITEM_PROJECTION,
         },
+        euEuropeTab: {
+          index: agoliaInstance.initIndex('euLaw'),
+          projection: EU_LAW_EU_TAB_PROJECTION
+        }
       },
 
       (document) => {
@@ -138,6 +168,30 @@ export async function POST(req) {
               link: document.link,
               category: document.category,
               linkUrl: document.linkUrl,
+            }
+          }
+          case 'euEuropeTab': {
+            return {
+              objectID: document.objectID,
+              lawTitle: document.lawTitle,
+              slug: document.slug,
+              searchTitle: document.searchTitle,
+              eu1Content: document.eu1Content,
+              eu2Content: document.eu2Content,
+              eu3Content: document.eu3Content,
+              eu4Content: document.eu4Content,
+              eu5Content: document.eu5Content,
+              eu6Content: document.eu6Content,
+              eu7Content: document.eu7Content,
+              eu8Content: document.eu8Content,
+              eu1Title: document.eu1Title,
+              eu2Title: document.eu2Title,
+              eu3Title: document.eu3Title,
+              eu4Title: document.eu4Title,
+              eu5Title: document.eu5Title,
+              eu6Title: document.eu6Title,
+              eu7Title: document.eu7Title,
+              eu8Title: document.eu8Title,
             }
           }
           default:
