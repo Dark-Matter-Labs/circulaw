e8e24802f823394f6b30cefe8311acf58f797527
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-10-10 17:14:52 +0200

    remove comments and clean

--- components/faq-page.js
+++ components/faq-page.js
@@ -6,7 +6,6 @@ import * as Accordion from '@radix-ui/react-accordion';
 import React from 'react';
 import { IconChevronDown } from '@tabler/icons-react';
 
-
 export default function FAQPageComponent({ data }) {
   return (
     <div className='global-margin pb-8 text-gray-800'>
@@ -18,63 +17,35 @@ export default function FAQPageComponent({ data }) {
             </Link>
           </div>
 
-        
           <div className='max-w-4xl mx-auto'>
-          <h1 className='heading-2xl-semibold sm:heading-5xl-semibold lg:block sm:pt-10 py-6 sm:pb-10 text-gray-800'>
+            <h1 className='heading-2xl-semibold sm:heading-5xl-semibold lg:block sm:pt-10 py-6 sm:pb-10 text-gray-800'>
               {data?.pageTitle}
             </h1>
-          <Accordion.Root
-            className="flex flex-col gap-y-5"
-            type="single"
-            defaultValue="item-1"
-            collapsible
-	>
-    {data.FAQPageContent.map((item, i) => (
-          <Accordion.Item key={i} className='border-t border-green-600 pb-12 pt-4' value={`item-${i + 1}`}>
-            <Accordion.Trigger className='heading-xl-semibold sm:heading-3xl-semibold text-green-600 mr-4 flex justify-between w-full group'><span className='text-left'>{item.question}</span> <IconChevronDown className='h-8 w-8 group-data-[state=open]:rotate-180 transition-transform duration-300 ease-in-out' /></Accordion.Trigger>
-            <Accordion.Content className='AccordionContent overflow-hidden'>
-            <PortableText value={item.response} components={portableTextComponents} />
-            </Accordion.Content>
-                </Accordion.Item>))}
-              </Accordion.Root> 
+            <Accordion.Root
+              className='flex flex-col gap-y-5'
+              type='single'
+              defaultValue='item-1'
+              collapsible
+            >
+              {data.FAQPageContent.map((item, i) => (
+                <Accordion.Item
+                  key={i}
+                  className='border-t border-green-600 pb-12 pt-4'
+                  value={`item-${i + 1}`}
+                >
+                  <Accordion.Trigger className='heading-xl-semibold sm:heading-3xl-semibold text-green-600 mr-4 flex justify-between w-full group'>
+                    <span className='text-left'>{item.question}</span>{' '}
+                    <IconChevronDown className='h-8 w-8 group-data-[state=open]:rotate-180 transition-transform duration-300 ease-in-out' />
+                  </Accordion.Trigger>
+                  <Accordion.Content className='AccordionContent overflow-hidden'>
+                    <PortableText value={item.response} components={portableTextComponents} />
+                  </Accordion.Content>
+                </Accordion.Item>
+              ))}
+            </Accordion.Root>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-
-
-
-{/**
-  
-  
-            <div>
-              {data.FAQPageContent.map((item, i) => (
-                <div key={i}>
-                  {item?.sectionTitle && (
-                    <div>
-                      <h2 className='heading-xl-semibold sm:heading-3xl-semibold text-green-500 pt-6 pb-10'>
-                        {' '}
-                        {item?.sectionTitle}
-                      </h2>
-                    </div>
-                  )}
-                  {item?.question && (
-                    <Accordion.Root type='single' defaultValue='item-1' collapsible className=''>
-                      <Accordion.Item className='text-left' value={`item-${i + 1}`}>
-                        <AccordionTrigger className='heading-xl-semibold sm:heading-3xl-semibold text-green-600 mr-4'>
-                          {item.question}
-                        </AccordionTrigger>
-                        <AccordionContent>
-                          <PortableText value={item.response} components={portableTextComponents} />
-                        </AccordionContent>
-                      </Accordion.Item>
-                    </Accordion.Root>
-                  )}
-                </div>
-              ))}
-            </div>
-  
-  */}
