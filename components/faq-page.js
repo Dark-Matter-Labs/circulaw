f3ffd904a5f221ff8f5fe1a0b295f2e29921d195
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-10-10 15:16:55 +0200

    remove MUI from front end

--- components/faq-page.js
+++ components/faq-page.js
@@ -1,23 +1,10 @@
 'use client';
 import { portableTextComponents } from '@/lib/portable-text/pt-components';
-import { PlusIcon, MinusIcon } from '@heroicons/react/outline';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
 import { PortableText } from '@portabletext/react';
 import Link from 'next/link';
-import { useState } from 'react';
 
-// TODO: we are using materlia ui here but only for this page.
-// perhaps a re-design with a different functinality so we can remove MUI
-// the hidden content is also not good for SEO
 export default function FAQPageComponent({ data }) {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
+ 
 
   return (
     <div className='global-margin pb-8 text-gray-800'>
@@ -45,37 +32,22 @@ export default function FAQPageComponent({ data }) {
                   )}
 
                   {item?.question && (
-                    <Accordion
-                      expanded={expanded === `panel_${i}`}
-                      onChange={handleChange(`panel_${i}`)}
-                      className='border-t pb-12 pt-4 border-green-600 border-0 rounded-0 shadow-none my-0'
-                      disableGutters={true}
-                      square={true}
-                    >
-                      <AccordionSummary
-                        expandIcon={
-                          expanded === `panel_${i}` ? (
-                            <MinusIcon className='h-5 w-6 text-green-600' />
-                          ) : (
-                            <PlusIcon className='h-6 w-6 text-green-600' />
-                          )
-                        }
-                        aria-controls={`panel_${i}_bh-content`}
-                        id={`panel_${i}_bh-header`}
-                      >
-                        <Typography
-                          as='h3'
+                    <>
+                    
+                    
+            
+                        <div
+                         
                           className='width-1/3 heading-xl-semibold sm:heading-3xl-semibold text-green-600 mr-4'
                         >
                           {item.question}
-                        </Typography>
-                      </AccordionSummary>
-                      <AccordionDetails>
-                        <Typography as='div' className='w-5/6'>
+                        </div>
+                   
+                     
+                        <div as='div' className='w-5/6'>
                           <PortableText value={item.response} components={portableTextComponents} />
-                        </Typography>
-                      </AccordionDetails>
-                    </Accordion>
+                        </div>
+                        </>
                   )}
                 </div>
               ))}
