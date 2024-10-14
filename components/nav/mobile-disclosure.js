3e8e161605e7bbe0a208fadb8af9f1300021a7d7
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-10-14 14:25:25 +0200

    update sub menus

--- components/nav/mobile-disclosure.js
+++ components/nav/mobile-disclosure.js
@@ -1,28 +1,17 @@
-import { Disclosure, Transition } from '@headlessui/react';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
 export default function MobileDisclosure({ navData, closeMenu }) {
   return (
-    <li className='p-base-semibold text-green-800 border-b py-6'>
+    <li className='p-base-semibold text-green-800 border-b last:border-b-0 py-6'>
       <Disclosure>
-        {({ open }) => (
           <>
-            <Disclosure.Button className={`${open ? '' : ''} flex flex-row items-center `}>
+            <DisclosureButton className='flex flex-row items-center group data-[open]:text-green-500'>
               {navData?.title}
-              <ChevronDownIcon className={`${open ? 'rotate-180' : ''} h-4 w-4 mt-1 ml-2`} />
-            </Disclosure.Button>
-            <Transition
-              show={open}
-              enter='transition duration-300 ease-out'
-              enterFrom='transform opacity-0'
-              enterTo='transform opacity-100'
-              leave='transition duration-75 ease-out'
-              leaveFrom='transform opacity-300'
-              leaveTo='transform opacity-0'
-              className='w-full'
-            >
-              <Disclosure.Panel className='p-base text-green-600'>
+              <ChevronDownIcon className='h-4 w-4 mt-1 ml-2 group-data-[open]:rotate-180' />
+            </DisclosureButton>
+              <DisclosurePanel className='p-base text-green-600'>
                 <ul>
                   <Link href={`/${navData?.slug}`} onClick={() => closeMenu(false)}>
                     <li className='pt-4 w-full flex items-center ml-4'>
@@ -46,10 +35,8 @@ export default function MobileDisclosure({ navData, closeMenu }) {
                     </li>
                   ))}
                 </ul>
-              </Disclosure.Panel>
-            </Transition>
+              </DisclosurePanel>
           </>
-        )}
       </Disclosure>
     </li>
   );
