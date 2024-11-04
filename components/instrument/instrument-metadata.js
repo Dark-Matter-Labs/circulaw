cef0b2b60140e2c781db01ee6bf040e2fb0a29a3
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-11-04 12:03:42 +0100

    fix mobile metadata

--- components/instrument/instrument-metadata.js
+++ components/instrument/instrument-metadata.js
@@ -11,10 +11,174 @@ export default function InstrumentMetaData({ data, borders }) {
       <div className='hidden sm:block'>
         <div
           className={`${
-            borders === true ? 'border-y border-gray-300' : 'pt-5 pb-3'
-          } h-auto flex flex-row gap-x-4 grow-0 items-center justify-self-center max-w-[870px]`}
+            borders === true ? 'sm:border-y sm:border-gray-300' : 'pt-5 pb-3'
+          } h-auto flex flex-row gap-x-4 grow-0 items-center justify-self-start w-full max-w-[870px]`}
         >
-          <div className='flex flex-col basis-houdbaarheid'>
+          <div className='h-auto flex flex-row gap-x-3 grow-0 items-st justify-self-start w-full mt-4'>
+            <div className='flex flex-col basis-houdbaarheid'>
+              <div className='flex items-center'>
+                <div className='p-2xs-semibold text-gray-600 mb-1'>Juridische houdbaarheid</div>
+                <Modal Button={<InstrumentTooltipButton />}>
+                  <ModalContent title='Geschatte juridische houdbaarheid'>
+                    <TooltipJuridischeHoudbaarheidContent JHTooltipText={data.JHTooltipText} />
+                  </ModalContent>
+                </Modal>
+              </div>
+
+              <div className='flex items-center'>
+                <div className='p-xs-semibold text-green-500 bg-green-50 p-1 rounded-cl'>
+                  {data?.juridischeHaalbaarheid}
+                </div>
+              </div>
+            </div>
+            <div className='flex flex-col basis-invloed'>
+              <div className='flex items-center'>
+                <div className='p-2xs-semibold text-gray-600 mb-1'>Invloed</div>
+                <Modal Button={<InstrumentTooltipButton />}>
+                  <ModalContent title='Geschatte invloed'>
+                    <TooltipJuridischeInvloedContent invloedTooltipText={data.invloedTooltipText} />
+                  </ModalContent>
+                </Modal>
+              </div>
+              <div className='flex items-center'>
+                <div className='p-xs-semibold text-green-500 bg-green-50 p-1 rounded-cl'>
+                  {data?.juridischInvloed}
+                </div>
+              </div>
+            </div>
+            <div className='flex flex-col basis-overheidslaag'>
+              <div className='flex items-center h-6'>
+                <div className='p-2xs-semibold text-gray-600 mb-1'>Overheidslaag</div>
+              </div>
+
+              <div className='p-xs-semibold text-green-500 flex'>
+                <div className='shrink bg-green-50 p-1 rounded-cl'>
+                  {data?.overheidslaag
+                    ?.filter((x) => x !== null)
+                    .map((level) => (
+                      <span key={level} className=''>
+                        {level} {data?.overheidslaag?.slice(-1)[0] !== level && <span>-</span>}
+                        &nbsp;
+                      </span>
+                    ))}
+                </div>
+              </div>
+            </div>
+            <div className='flex flex-col basis-rladder'>
+              <div className='flex items-center'>
+                <div className='p-2xs-semibold text-gray-600 mb-1'>R-ladder</div>
+                <Modal Button={<InstrumentTooltipButton />}>
+                  <ModalContent title='R-ladder: strategieën van circulariteit'>
+                    <RladderTooltipContent />
+                  </ModalContent>
+                </Modal>
+              </div>
+
+              <div className='flex items-center'>
+                <div className='flex flex-row items-center'>
+                  <div className='shrink bg-green-50 p-1 rounded-cl'>
+                    {data?.rLadder?.map((rValue) => (
+                      <span key={rValue} className='p-xs-semibold text-green-500 '>
+                        {rValue} {data?.rLadder.slice(-1)[0] !== rValue && <span>-</span>}
+                        &nbsp;
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* MOBILE */}
+      <div className='flex flex-col sm:hidden justify-center mb-4'>
+        <div className='flex flex-row justify-between h-auto py-2'>
+          <div className='flex flex-col justify-center'>
+            <div className='flex items-center'>
+              <div className='p-xs-semibold sm:py-1 text-gray-600'>Juridische houdbaarheid</div>
+              <Modal Button={<InstrumentTooltipButton />}>
+                <ModalContent title='Geschatte juridische houdbaarheid'>
+                  <TooltipJuridischeHoudbaarheidContent JHTooltipText={data.JHTooltipText} />
+                </ModalContent>
+              </Modal>
+            </div>
+            <div className='flex items-center'>
+              <div className='shrink bg-green-100 p-1 rounded-cl p-xs-semibold text-green-500'>
+                {data?.juridischeHaalbaarheid}
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col justify-center'>
+            <div className='flex items-center'>
+              <div className='p-xs-semibold py-1 text-gray-600'>Invloed</div>
+              <Modal Button={<InstrumentTooltipButton />}>
+                <ModalContent title='Geschatte invloed'>
+                  <TooltipJuridischeInvloedContent invloedTooltipText={data.invloedTooltipText} />
+                </ModalContent>
+              </Modal>
+            </div>
+
+            <div className='flex items-center'>
+              <div className='p-xs-semibold text-green-500 shrink bg-green-100 p-1 rounded-cl'>
+                {data?.juridischInvloed}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='flex h-auto py-2'>
+          <div className='flex flex-col justify-center'>
+            <div className='flex items-center'>
+              <div className='p-xs-semibold py-1 text-gray-600'>Overheidslaag</div>
+            </div>
+
+            <div className='p-xs-semibold text-green-500 shrink bg-green-100 p-1 rounded-cl'>
+              {data?.overheidslaag
+                ?.filter((x) => x !== null)
+                .map((level) => (
+                  <span key={level} className=''>
+                    {level} {data?.overheidslaag.slice(-1)[0] !== level && <span>-</span>}
+                    &nbsp;
+                  </span>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div className='flex py-2'>
+          <div className='flex flex-col justify-center'>
+            <div className='flex items-center'>
+              <div className='p-xs-semibold py-1 text-gray-600'>R-ladder</div>
+              <Modal Button={<InstrumentTooltipButton />}>
+                <ModalContent title='R-ladder: strategieën van circulariteit'>
+                  <RladderTooltipContent />
+                </ModalContent>
+              </Modal>
+            </div>
+
+            <div className='flex items-center'>
+              <div className='flex flex-row items-center'>
+                {data?.rLadder?.map((rValue) => (
+                  <div
+                    key={rValue}
+                    className='p-xs-semibold text-green-500 shrink bg-green-100 p-1 rounded-cl'
+                  >
+                    {rValue} {data?.rLadder.slice(-1)[0] !== rValue && <span>-</span>}
+                    &nbsp;
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+{
+  /*
+
+<div className='flex flex-col basis-houdbaarheid'>
             <div className='p-xs-semibold text-gray-600'>Juridische houdbaarheid</div>
             <div className='flex items-center'>
               <div className='p-xs-semibold text-green-500 pr-2'>
@@ -75,78 +239,5 @@ export default function InstrumentMetaData({ data, borders }) {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      {/* MOBILE */}
-      <div className='flex flex-col sm:hidden justify-center mb-4'>
-        <div className='flex flex-row justify-between h-auto py-2 border-b border-t-2 border-gray-400'>
-          <div className='flex flex-col justify-center'>
-            <div className='p-xs-semibold sm:py-1 text-gray-600'>Juridische houdbaarheid</div>
-            <div className='flex items-center'>
-              <div className='p-xs-semibold text-green-500 pr-2'>
-                {data?.juridischeHaalbaarheid}
-              </div>
-              {data?.slug && (
-                <Modal Button={<InstrumentTooltipButton />}>
-                  <ModalContent title='Geschatte juridische houdbaarheid'>
-                    <TooltipJuridischeHoudbaarheidContent JHTooltipText={data.JHTooltipText} />
-                  </ModalContent>
-                </Modal>
-              )}
-            </div>
-          </div>
-          <div className='flex flex-col justify-center'>
-            <div className='p-xs-semibold py-1 text-gray-600'>Invloed</div>
-            <div className='flex items-center'>
-              <div className='p-xs-semibold text-green-500 pr-2'>{data?.juridischInvloed}</div>
-              {data?.slug && (
-                <Modal Button={<InstrumentTooltipButton />}>
-                  <ModalContent title='Geschatte invloed'>
-                    <TooltipJuridischeInvloedContent invloedTooltipText={data.invloedTooltipText} />
-                  </ModalContent>
-                </Modal>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className='border-b border-gray-400 flex h-auto py-2'>
-          <div className='flex flex-col justify-center'>
-            <div className='p-xs-semibold py-1 text-gray-600'>Overheidslaag</div>
-            <div className='p-xs-semibold text-green-500'>
-              {data?.overheidslaag
-                ?.filter((x) => x !== null)
-                .map((level) => (
-                  <span key={level} className=''>
-                    {level} {data?.overheidslaag.slice(-1)[0] !== level && <span>-</span>}
-                    &nbsp;
-                  </span>
-                ))}
-            </div>
-          </div>
-        </div>
-        <div className='flex py-2 border-b-2 border-gray-400'>
-          <div className='flex flex-col justify-center'>
-            <div className='p-xs-semibold py-1 text-gray-600'>R-ladder</div>
-            <div className='flex items-center'>
-              <div className='flex flex-row items-center'>
-                {data?.rLadder?.map((rValue) => (
-                  <div key={rValue} className='p-xs-semibold text-green-500'>
-                    {rValue} {data?.rLadder.slice(-1)[0] !== rValue && <span>-</span>}
-                    &nbsp;
-                  </div>
-                ))}
-                {data?.slug && (
-                  <Modal Button={<InstrumentTooltipButton />}>
-                    <ModalContent title='R-ladder: strategieën van circulariteit'>
-                      <RladderTooltipContent />
-                    </ModalContent>
-                  </Modal>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </div>*/
 }
