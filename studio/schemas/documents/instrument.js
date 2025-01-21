af4243e2b7cf37055b9aa2efbc4a59db93fd9244
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-01-21 09:44:31 +0100

    update replace function

--- studio/schemas/documents/instrument.js
+++ studio/schemas/documents/instrument.js
@@ -88,7 +88,7 @@ export default {
         slugify: (input) =>
           input
             .toLowerCase()
-            .replace(/[^a-zA-Z0-9]/g, '')
+            .replace(/[^a-zA-Z0-9 ]/g, '')
             .replace(/\s+/g, '-')
             .slice(0, 200),
       },
