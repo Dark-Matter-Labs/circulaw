578a999c29bd8ffd58751e5a13c84d94353a8754
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-01-27 12:03:21 +0100

    update to external registration

--- app/training/page.js
+++ app/training/page.js
@@ -35,7 +35,7 @@ export default function LearningLandingPage() {
             houtbouw in het beleid van jouw overheid? Dat leer je in onze e-learning:{' '}
             <span className='p-base-semibold'>circulaire houtbouw onder de Omgevingswet</span>.
           </p>
-          <Link href='/training/aanmelden' className='block md:hidden'>
+          <Link href='https://training.circulaw.nl/register/a6d26a79-e6b1-42d6-9499-1dbe196fc693' target='_blank' className='block md:hidden'>
             <div className='bg-green-50 mt-6 w-full flex flex-col p-4 text-green-600'>
               <div className='mb-2'>
                 <IconBooks className='size-8' />
@@ -83,7 +83,8 @@ export default function LearningLandingPage() {
             <span className='p-base-semibold'>werk maken</span> van houtbouw.
           </p>
           <Link
-            href='/training/aanmelden'
+            href='https://training.circulaw.nl/register/a6d26a79-e6b1-42d6-9499-1dbe196fc693'
+            target='_blank'
             className='link-base link-interaction flex flex-row items-center font-semibold'
           >
             Schrijf je nu in <IconArrowRight className='ml-1 size-4' />
@@ -120,7 +121,7 @@ export default function LearningLandingPage() {
             </div>
           </div>
         </div>
-        <Link href='/training/aanmelden' className='hidden md:block'>
+        <Link href='https://training.circulaw.nl/register/a6d26a79-e6b1-42d6-9499-1dbe196fc693' target='_blank' className='hidden md:block'>
           <div className='sticky top-32 bg-green-50 ml-16 w-80 flex flex-col p-4 text-green-600'>
             <div className='mb-2'>
               <IconBooks className='size-8' />
