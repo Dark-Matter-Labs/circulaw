011a7c60eff394d95f387971383a2d9d82fc3b54
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-26 16:33:28 +0100

    finish desktop homepage header

--- components/headers/homepage-header.js
+++ components/headers/homepage-header.js
@@ -1,27 +1,28 @@
 import { Link as ScrollLink } from 'react-scroll';
 
-import { IconArrowDown } from '@tabler/icons-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { IconArrowDown, IconArrowRight } from '@tabler/icons-react';
 
 import CustomButton from '../custom-button';
 
 export default function HomepageHeader() {
   return (
-    <main className='relative z-10 pb-16 pt-56'>
-      <div className='global-margin'>
-        <div className='mx-auto max-w-4xl text-center'>
-          <div className='m-auto max-w-xs text-left text-green-100 sm:block sm:max-w-4xl sm:text-center'>
-            <h1 className='header-desktop-mobile sm:header-desktop block'>
-              Regelgeving voor een circulaire economie
-            </h1>{' '}
-            <h2 className='p-base sm:heading-2xl-semibold m-auto mt-2 max-w-sm pb-8 pt-10 text-left text-green-100 sm:max-w-full sm:text-center'>
+    <main className='relative z-10 pt-44'>
+      <div className='global-margin relative'>
+        <div className='max-w-4xl text-left'>
+          <div className='max-w-xs text-left text-green-100 sm:block sm:max-w-4xl'>
+            <h1 className='heading-6xl-semibold block'>Regelgeving voor een circulaire economie</h1>{' '}
+            <h2 className='p-base sm:heading-2xl-semibold mt-2 max-w-2xl pb-8 pt-10 text-left text-green-100'>
               CircuLaw laat decentrale overheden zien hoe je de circulaire transitie kunt
               versnellen. Per productketen bieden we juridische instrumenten op basis van de
               Nederlandse wetgeving. Ook geven we inzicht in Europese wetgeving.
             </h2>
           </div>
-          <div className='mt-2 sm:flex sm:justify-center md:mt-2'>
+          <div className='mt-2 sm:flex sm:justify-start md:mt-2'>
             <div className=''>
-              <CustomButton color='home'>
+              <CustomButton color='greenBackground'>
                 <ScrollLink to='thema' smooth={true} offset={-95}>
                   Bekijk de thema&rsquo;s{' '}
                   <IconArrowDown className='inline-block h-5 w-5' aria-hidden='true' />
@@ -29,7 +30,7 @@ export default function HomepageHeader() {
               </CustomButton>
             </div>
             <div className='mt-3 rounded-md sm:ml-3 sm:mt-0'>
-              <CustomButton color='home'>
+              <CustomButton color='greenBackground'>
                 <ScrollLink to='about' smooth={true} offset={-100}>
                   Meer over CircuLaw?{' '}
                   <IconArrowDown className='inline-block h-5 w-5' aria-hidden='true' />
@@ -37,6 +38,27 @@ export default function HomepageHeader() {
               </CustomButton>
             </div>
           </div>
+        </div>
+        <div className='group absolute bottom-0 right-0 z-30 overflow-visible'>
+          <Link href='/over/wat-is-circulaw' className='h-full w-full'>
+            <Image
+              src='/home-page/homepage-video-cta.png'
+              alt='screenshot of CircuLaw animation'
+              width={760}
+              height={730}
+              className='-rotate-30 size-80'
+            />
+            <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform'>
+              <div className='flex flex-col gap-y-8'>
+                <h3 className='heading-4xl-semibold text-green-100'>Wat is CircuLaw?</h3>
+
+                <div className='heading-xl-semibold text-green-100 group-hover:underline'>
+                  Video bekijken
+                  <IconArrowRight className='inline-block h-5 w-5' aria-hidden='true' />
+                </div>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
     </main>
