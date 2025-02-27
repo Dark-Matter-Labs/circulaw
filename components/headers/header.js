e6f5dd2f5a94c68a8fda2c876de00d6b18e7e5d4
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-27 10:11:11 +0100

    product chain header complete

--- components/headers/header.js
+++ components/headers/header.js
@@ -4,9 +4,10 @@ import Link from 'next/link';
 export default function Header({ imageURL, bgColor, title, pageType, subtitle}) {
     console.log(pageType)
   return (
-    <div className={`${bgColor} global-margin`}>
-      <div className='relative h-full w-full object-cover'>
+    <div className={`${bgColor} global-margin mt-20 `}>
+      <div className='relative h-full w-full object-cover py-10 px-16'>
         {imageURL && (
+          <>
           <Image
             src={imageURL}
             alt='homepage decoration'
@@ -16,11 +17,11 @@ export default function Header({ imageURL, bgColor, title, pageType, subtitle}) 
             priority={true}
             quality={100}
           />
-        )}
-        <div className='absolute z-0 h-full w-full bg-gradient-to-t from-[#035E46] to-[#035E4600]'></div>
+       
+        <div className='absolute top-0 left-0 z-0 h-full w-full bg-gradient-to-t from-[#035E46] to-[#035E4600]'></div> </>)}
         <div className='z-5 relative flex flex-col justify-between'>
         {/* Breadcrumb */}
-          <div className='pt-8 mb-20'>
+          <div className='mb-20'>
             <Link
               className='group inline-flex flex-row items-center justify-center rounded-clSm bg-green-100 py-1.5 pl-2 pr-3 text-green-500'
               href='/'
@@ -31,7 +32,7 @@ export default function Header({ imageURL, bgColor, title, pageType, subtitle}) 
               </span>
             </Link>
           </div>
-          <div className='max-w-3xl pb-8'>
+          <div className='max-w-3xl'>
             <div className=''>
               <div className='p-base sm:heading-2xl-semibold text-green-400'>{subtitle}</div>
               <h1 className='heading-3xl-semibold sm:heading-5xl-semibold inline-block text-green-100'>
