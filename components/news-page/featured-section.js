b13c5db042910a755c39d82c4dbcbcc594ec7d97
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-03-18 15:19:38 +0100

    update news item hasPage

--- components/news-page/featured-section.js
+++ components/news-page/featured-section.js
@@ -1,11 +1,11 @@
-
+import NewNewsCard from './new-news-card'
 
 export default function FeaturedNewsSection({items}) {
-    console.log(items)
+
     return (
   <ul className='grid grid-cols-3 gap-x-3 overflow-hidden'>
             {items.map((item, id) => (
-              <li key={id}>{item.newsTitle}</li>
+              <li key={id}><NewNewsCard data={item} /></li>
             ))}
           </ul>
     )
