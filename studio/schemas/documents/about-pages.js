61eccc538520ba951e7310c10fb45df2afbd0e66
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-05-06 16:30:08 +0200

    begin restructuring about page schema and add highlight component

--- studio/schemas/documents/about-pages.js
+++ studio/schemas/documents/about-pages.js
@@ -1,27 +1,45 @@
+import { AiOutlineHighlight } from 'react-icons/ai';
 import { FcAbout } from 'react-icons/fc';
 
-import { orderRankField } from '@sanity/orderable-document-list';
+import Highlight from '../../components/highlight';
+
+// import { orderRankField } from '@sanity/orderable-document-list';
 
 export default {
   title: 'About Pages',
   name: 'aboutPage',
   type: 'document',
+  groups: [
+    {
+      name: 'editableContent',
+      title: 'Editable Content',
+      default: true,
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+    },
+  ],
   icon: FcAbout,
   fields: [
+    // orderRankField({ type: 'aboutPage', newItemPosition: 'before' }),
     {
       title: 'Titel',
       name: 'pageTitle',
       type: 'string',
+      group: 'editableContent',
+      validation: (Rule) => Rule.required(),
       description:
         'Voer de titel in. De titel komt terug in de slug (het deel van de URL na de domeinnaam - zichtbaar in de navigatie en footer)',
     },
-    orderRankField({ type: 'aboutPage', newItemPosition: 'before' }),
     {
       title: 'Slug',
       name: 'slug',
       type: 'slug',
       description:
         'Klik op ‘aanmaken’ (Slug is het het deel van de URL na de domeinnaam - zichtbaar in de navigatie en footer).',
+      validation: (Rule) => Rule.required(),
+      group: 'editableContent',
       options: {
         source: 'pageTitle',
         inUnique: 'true',
@@ -35,83 +53,60 @@ export default {
       },
     },
     {
+      title: 'Subtitle',
+      name: 'subTitle',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+      group: 'editableContent',
+      description: 'this will display on top of the title in the page',
+    },
+    {
+      title: 'Intro text',
+      name: 'introText',
+      type: 'array',
+      validation: (Rule) => Rule.required(),
+      group: 'editableContent',
+      of: [
+        {
+          type: 'block',
+          styles: [{ title: 'normal', value: 'normal' }],
+          marks: {
+            decorators: [
+              {
+                title: 'Highlight',
+                value: 'highlight',
+                blockEditor: {
+                  render: Highlight,
+                  icon: AiOutlineHighlight,
+                },
+              },
+            ],
+            annotations: [],
+          },
+          lists: [],
+        },
+      ],
+    },
+    {
+      title: 'Content',
+      name: 'content',
+      type: 'array',
+      group: 'editableContent',
+      of: [{ type: 'greenBox' }],
+    },
+    {
       title: 'Meta Page Title',
       name: 'metaTitle',
       type: 'string',
       description: 'SEO',
+      group: 'seo',
     },
     {
       title: 'Meta Description',
       name: 'metaDescribe',
       type: 'string',
       description: 'SEO',
-    },
-    {
-      title: 'Copy',
-      name: 'aboutPageContent',
-      type: 'array',
-      description:
-        'Voer hier de tekst in (maak gebruik van voorgestelde subkoppen, alinea-indeling,  enz.).',
-      of: [
-        {
-          type: 'greenBox',
-        },
-        {
-          type: 'imageBlock',
-        },
-        {
-          type: 'pdfBlock',
-        },
-        {
-          type: 'smallPara',
-        },
-        {
-          type: 'youtube',
-        },
-        {
-          type: 'block',
-          of: [
-            {
-              type: 'dropDown',
-            },
-          ],
-          lists: [
-            { title: 'Bullet', value: 'bullet' },
-            { title: 'Number', value: 'number' },
-          ],
-          styles: [
-            { title: 'H2', value: 'h2' },
-            { title: 'H3', value: 'h3' },
-            { title: 'normal', value: 'normal' },
-            { title: 'subheading', value: 'subheading' },
-          ],
-          marks: {
-            decorators: [
-              { title: 'Strong', value: 'strong' },
-              { title: 'Emphasis', value: 'em' },
-            ],
-            annotations: [
-              {
-                title: 'URL',
-                name: 'link',
-                type: 'object',
-                fields: [
-                  {
-                    title: 'URL',
-                    name: 'href',
-                    type: 'url',
-                  },
-                  {
-                    title: 'Open in new winder',
-                    name: 'blank',
-                    type: 'boolean',
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      ],
+      group: 'seo',
     },
   ],
 };
